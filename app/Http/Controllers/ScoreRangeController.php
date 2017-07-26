7b1d9459db6161a6457ead0d90e074c0f495e756
guoqing <2916191443@qq.com>
2017-07-26 09:08:35 +0800

    ScoreRangeContolller

---
+++ app/Http/Controllers/ScoreRangeController.php
@@ -0,0 +1,121 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ScoreRangeController extends Controller
+{
+    protected $app;
+
+    function __construct(App $app) { $this->app = $app; }
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        if (Request::get('draw')) {
+            return response()->json($this->app->datatable());
+        }
+        return view('app.index', ['js' => 'js/app/index.js','dialog' => true]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('app.create',['js' => 'js/app/create.js']);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     * @return \Illuminate\Http\Response
+     * @internal param \Illuminate\Http\Request|Request $request
+     */
+    public function store(AppRequest $request)
+    {
+        //添加新数据
+        $res = App::create($request->all());
+        if ($res) {
+            return response()->json(['statusCode' => 200, 'message' => '创建成功！']);
+        }else{
+            return response()->json(['statusCode' => 500, 'message' => '创建失败！']);
+        }
+//        // create a new record
+//        $app = new App;
+//        // assign the values to corresponding fields
+//        $app->name = $request->name;
+//        $app->description = $request->description;
+//        // save the record
+//        if ($app->save()) {
+//            return response()->json(['statusCode' => 200, 'message' => '创建成功！']);
+//        }
+//
+//        return response()->json(['statusCode' => 500, 'message' => '创建失败！']);
+    }
+
+    /**
+     * Display the specified resource.
+     * @return \Illuminate\Http\Response
+     * @internal param App $app
+     */
+    public function show($id)
+    {
+        // find the record by $id
+        $app = App::find($id);
+        return view('app.show', ['app' => $app]);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\App  $app
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        // find the record by $id
+        $app = App::find($id);
+        //记录返回给view
+        return view('app.edit',['js' => 'js/app/edit.js', 'app' => $app]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     * @return \Illuminate\Http\Response
+     * @internal param \Illuminate\Http\Request $request
+     * @internal param App $app
+     */
+    public function update(AppRequest $request, $id)
+    {
+        $app = App::findOrFail($id);
+        $res = $app->update($request->all());
+        if ($res) {
+            return response()->json(['statusCode' => 200, 'message' => '编辑成功！']);
+        }else{
+            return response()->json(['statusCode' => 500, 'message' => '编辑失败！']);
+        }
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\App  $app
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $app = App::findOrFail($id);
+        if ($app->delete()){
+            return response()->json(['statusCode' => 200, 'message' => '删除成功！']);
+        }else{
+            return response()->json(['statusCode' => 200, 'message' => '删除失败！']);
+        }
+    }
+}
