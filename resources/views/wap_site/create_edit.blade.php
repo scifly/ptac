7cf41ed93dcc8df2800e121daf16b0e90ff02cfa
TF <1830751348@qq.com>
2017-08-04 09:54:33 +0800

    fixed fileinput

--- resources/views/wap_site/create_edit.blade.php
+++ resources/views/wap_site/create_edit.blade.php
@@ -23,7 +23,8 @@
             <div class="form-group">
                 {!! Form::label('media_ids', '轮播图',['class' => 'col-sm-2 control-label']) !!}
                 <div class="col-sm-6">
-                    <input type="file" id="media_ids" name="image_data[]" multiple>
+                    {{--<input type="file" id="media_ids" name="image_data[]" multiple>--}}
+                    <a href="javascript:" class="btn btn-primary" data-toggle="modal" data-target="#modalPic">上传</a>
                 </div>
             </div>
             <div class="form-group">
@@ -46,4 +47,30 @@
         </div>
     </div>
 </div>
-
+<div class="modal fade" id="modalPic" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal"
+                        aria-hidden="true">×
+                </button>
+                <h4 class="modal-title" id="myModalLabel">
+                    模态框（Modal）标题
+                </h4>
+            </div>
+            <div class="modal-body">
+                <form action="#" class="form-horizontal">
+                    <input type="file" id="uploadFile" multiple>
+                </form>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-default"
+                        data-dismiss="modal">关闭
+                </button>
+                <button type="button" class="btn btn-primary" id="upload">
+                    上传
+                </button>
+            </div>
+        </div>
+    </div>
+</div>
