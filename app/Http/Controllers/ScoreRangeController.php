73d609aeb51f33ad926afab6634feab60e2484a7
guoqing <2916191443@qq.com>
2017-08-07 14:19:17 +0800

    score_range

--- app/Http/Controllers/ScoreRangeController.php
+++ app/Http/Controllers/ScoreRangeController.php
@@ -5,7 +5,9 @@ namespace App\Http\Controllers;
 use App\Http\Requests\ScoreRangeRequest;
 use App\Models\ScoreRange;
 use App\Models\Subject;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Request;
+use Illuminate\Http\Request as HttpRequest;
 
 class ScoreRangeController extends Controller
 {
@@ -136,5 +138,59 @@ class ScoreRangeController extends Controller
         }else{
             return response()->json(['statusCode' => 200, 'message' => '删除失败！']);
         }
+    }
+
+    public function statisticsShow(){
+        return view('score_range.statistics_show');
+    }
+
+    public function statistics(){
+        //获取请求参数
+        $type = 'grade';
+        $id = 1;
+        $exam_id = 1;
+
+        //查询班级
+        if($type == 'grade'){
+            $classes = DB::table('classes')->where('grade_id', $id)->select('id', 'grade_id')->pluck('id')->toArray();
+        }else{
+            $classes = [$id];
+        }
+
+        //查找符合条件的所有成绩
+        $score = DB::table('scores')
+            ->join('students', 'students.id', '=', 'scores.student_id')
+            ->whereIn('students.class_id',$classes)
+            ->where('scores.exam_id', $exam_id)
+            ->select('scores.id', 'scores.student_id', 'scores.subject_id', 'scores.score')
+            ->orderBy('scores.score', 'desc')
+            ->get();
+        //查找所有成绩统计项
+        $score_range = DB::table('score_ranges')->select('id', 'name', 'subject_ids', 'start_score', 'end_score')->get()->toArray();
+        //循环成绩项
+        foreach ($score_range as $v){
+            $v->number = 0;
+            //统计项统计科目
+            $subject_ids = explode(',', $v->subject_ids);
+            //计算学生这些科目总成绩
+            $item = [];
+            foreach($score as $val){
+                //判断该科成绩是否需要统计
+                if(in_array($val->subject_id, $subject_ids)){
+                    if(!isset($item[$val->student_id])){
+                        $item[$val->student_id] = $val->score;
+                    }else{
+                        $item[$val->student_id] += $val->score;
+                    }
+                }
+            }
+            //若成绩在统计项范围内统计数量加一
+            foreach ($item as $val){
+                if($v->end_score>$val && $val>=$v->start_score){
+                    $v->number ++;
+                }
+            }
+        }
+        dump($score_range);
     }
 }
