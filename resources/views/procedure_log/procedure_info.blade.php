136d495496cb0acdd26f6452f88d5e2f3d1f4203
TF <1830751348@qq.com>
2017-08-14 11:33:11 +0800

    fixed procedure_info

--- resources/views/procedure_log/procedure_info.blade.php
+++ resources/views/procedure_log/procedure_info.blade.php
@@ -5,13 +5,18 @@
 @section('content')
     <div class="row">
         <div class="col-xs-12">
-            <div class="stepflex">
+            <div class="stepFlex clearfix">
                 @foreach ($data as $key => $val)
-                    <dl class="@if ($val['status']== 0)success @elseif ($val['status']== 1)fail @else doing @endif">
+                    <dl class="flex-item @if ($val['status']== 0)success @elseif ($val['status']== 1)fail @else doing @endif">
                         <dt class="s-num">{{ $key+1 }}</dt>
                         <dd class="s-text">{{ $val['name'] }}</dd>
                     </dl>
                 @endforeach
+            </div>
+            <div class="stepInfo">
+                <div class="info-item">内容1</div>
+                <div class="info-item">内容2</div>
+                <div class="info-item">内容3</div>
             </div>
         </div>
     </div>
