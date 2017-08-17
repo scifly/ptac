4a25218d621647aa10cd20915fdc0d6359c37d59
guoqing <2916191443@qq.com>
2017-08-17 11:04:37 +0800

    procedure upload file

--- resources/views/procedure_log/create.blade.php
+++ resources/views/procedure_log/create.blade.php
@@ -25,6 +25,14 @@
                         ]) !!}
                     </div>
                 </div>
+                <div class="form-group">
+                    <label for="media_ids" class="col-sm-4 control-label">附件</label>
+                    <div class="col-sm-8">
+                        <div class="preview">
+                        </div>
+                        <a class="btn btn-primary" data-toggle="modal" data-target="#modalPic">上传</a>
+                    </div>
+                </div>
             </div>
         </div>
         <div class="box-footer">
@@ -33,6 +41,29 @@
                 <div class="col-sm-3 col-sm-offset-4">
                     {!! Form::submit('保存', ['class' => 'btn btn-primary pull-left', 'id' => 'save']) !!}
                     {!! Form::reset('取消', ['class' => 'btn btn-default pull-right', 'id' => 'cancel']) !!}
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="modal fade" id="modalPic">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <button type="button" class="close" data-dismiss="modal"
+                            aria-hidden="true">×
+                    </button>
+                    <h4 class="modal-title" id="myModalLabel">
+                        上传附件
+                    </h4>
+                </div>
+                <div class="modal-body">
+                    <input type="file" name="medias[]" id="uploadFile" accept="image/jpeg,image/gif,image/png" multiple>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-default"
+                            data-dismiss="modal">关闭
+                    </button>
                 </div>
             </div>
         </div>
