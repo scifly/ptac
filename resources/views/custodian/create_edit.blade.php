4b85d9e25e402e69761c569d732baf452a11beb1
Guo Xiaolin <352326679@qq.com>
2017-09-18 15:24:05 +0800

    added custodian

--- resources/views/custodian/create_edit.blade.php
+++ resources/views/custodian/create_edit.blade.php
@@ -74,8 +74,8 @@
                             </tr>
                             </thead>
                             <tbody>
-                            @if(isset($mobiles))
-                                @foreach($mobiles as $key => $mobile)
+                            @if(isset($custodian->user->mobiles))
+                                @foreach($custodian->user->mobiles as $key => $mobile)
                                     <tr>
                                         <td><input class="form-control" name="mobile[mobile][e{{$key}}]" type="text"
                                                    placeholder="（请输入手机号码）" value='{{$mobile->mobile}}'>
