a6c03018215f891c5d6a8eb4fdce8f753f1f6835
Guo Xiaolin <352326679@qq.com>
2017-09-22 11:01:38 +0800

    modified mobile

--- app/Rules/Mobiles.php
+++ app/Rules/Mobiles.php
@@ -6,7 +6,7 @@ use App\Models\Mobile;
 use Illuminate\Contracts\Validation\Rule;
 
 class Mobiles implements Rule {
-    const PHONEREG = '/^(13[0-9]|14[579]|15[0-3,5-9]|17[0135678]|18[0-9])\\d{8}$/';
+    const PHONEREG = '/^1[34578][0-9]{9}$/';
     private $value;
     public function passes($attribute, $value)
     {
