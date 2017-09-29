6bbed5d1d6cd78ea771770e72b6f6e8ddece1128
Guo Xiaolin <352326679@qq.com>
2017-09-29 16:13:02 +0800

    test

--- app/Http/Controllers/WapSiteController.php
+++ app/Http/Controllers/WapSiteController.php
@@ -201,7 +201,7 @@ class WapSiteController extends Controller {
         $wapSite = $this->wapSite
             ->where('school_id', 1)
             ->first();
-        dd($wapSite->wapSiteModules->media);
+        // dd($wapSite->wapSiteModules->media);
         return view('wap_site.web_index', [
             'wapsite' => $wapSite,
             'medias'  => $this->media->medias($wapSite->media_ids),
