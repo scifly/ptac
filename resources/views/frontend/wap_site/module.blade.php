5c35f63bcab3d7102a55899cee9f1484e1e49853
Guo Xiaolin <352326679@qq.com>
2017-09-29 16:29:50 +0800

    test

--- resources/views/frontend/wap_site/module.blade.php
+++ resources/views/frontend/wap_site/module.blade.php
@@ -3,7 +3,7 @@
     <!--list-->
     <div class="weui-panel__bd">
 
-        @foreach($modules as $v)
+        @foreach($articles as $v)
             <a href="/wsm_articles/detail/{{$v->id}}" class="weui-media-box weui-media-box_appmsg">
                 <div class="weui-media-box__hd">
                     <img class="weui-media-box__thumb"
