8dec990c9511dc164b28a26b2f55cc9dfdaa1275
Guo Xiaolin <352326679@qq.com>
2017-09-29 15:17:55 +0800

    test

--- public/js/wap_site/edit.js
+++ public/js/wap_site/edit.js
@@ -30,7 +30,7 @@ $(function () {
         $.each(response, function (index, obj) {
             $pre.append(
                 '<div class="img-item">' +
-                    '<img src="/' + obj.path + '" id="' + obj.id + '">' +
+                    '<img src="../' + obj.path + '" id="' + obj.id + '">' +
                     '<div class="del-mask">' +
                         '<i class="delete glyphicon glyphicon-trash"></i>' +
                     '</div>' +
