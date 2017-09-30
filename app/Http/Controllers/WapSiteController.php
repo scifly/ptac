87e517b75b22a6c43e84187e6df7fb232aa2e691
Guo Xiaolin <352326679@qq.com>
2017-09-30 10:31:45 +0800

    test

--- app/Http/Controllers/WapSiteController.php
+++ app/Http/Controllers/WapSiteController.php
@@ -211,7 +211,7 @@ class WapSiteController extends Controller {
         $codeUrl = Wechat::getCodeUrl($corpId, '1000006', 'http://weixin.028lk.com/wap_sites/userInfo');
         
         // $result = Wechat::curlGet($codeUrl);
-        redirect($codeUrl);
+        return redirect($codeUrl);
         $code = Request::query('code');
         $wapSite = $this->wapSite
             ->where('school_id', 1)
