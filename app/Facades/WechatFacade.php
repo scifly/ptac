6eb81e056f3ec88d9686998fda232e91a11f421b
Guo Xiaolin <352326679@qq.com>
2017-09-30 10:02:33 +0800

    test

--- app/Facades/WechatFacade.php
+++ app/Facades/WechatFacade.php
@@ -173,6 +173,7 @@ class Wechat extends Facade {
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
         curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
         curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
+        dd(curl_error($ch));
         $result = curl_exec($ch);
         curl_close($ch);
         return $result;
