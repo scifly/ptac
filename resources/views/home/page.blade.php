ccb65ef92bef570a8885c4c67318d0cca7d49509
Guo Xiaolin <352326679@qq.com>
2017-10-13 09:41:37 +0800

    added test

--- resources/views/home/page.blade.php
+++ resources/views/home/page.blade.php
@@ -30,6 +30,8 @@
     <!-- fileinput-->   
     <link rel="stylesheet" href="{{ URL::asset('js/plugins/fileinput/css/fileinput.min.css') }}">
     <link rel="stylesheet" href="{{ URL::asset('js/plugins/fileinput/themes/explorer/theme.css') }}">
+    <!--微网站应用菜单管理-->
+    <link rel="stylesheet" href="{{ URL::asset('css/showEditMenu.css') }}">
     <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
     <!--[if lt IE 9]>
