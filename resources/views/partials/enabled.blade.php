0f3160bd1af1888b455a842d52e972ed740f99e6
Hao Yuhang <haoyh@yahoo.com>
2017-11-02 17:01:03 +0800

    fixed issue of include file enabled.blade

--- resources/views/partials/enabled.blade.php
+++ resources/views/partials/enabled.blade.php
@@ -3,11 +3,11 @@
     <div class="col-sm-6" style="padding-top: 5px;">
         <label id="{{ $id }}" style="margin-right: 5px;">
             <input id="{{ $id }}" @if($value) checked @endif
-                   type="radio" name="user[gender]" class="minimal" value="1">
+                   type="radio" name="{{ $id }}" class="minimal" value="1">
         </label> 启用
         <label id="{{ $id }}" style="margin: 0 5px 0 10px;">
-            <input id="user[gender]" @if(!$value) checked @endif
-                   type="radio" name="user[gender]" class="minimal" value="0">
+            <input id="{{ $id }}" @if(!$value) checked @endif
+                   type="radio" name="{{ $id }}" class="minimal" value="0">
         </label> 禁用
     </div>
 </div>
