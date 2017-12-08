57b0a70089fa82e305a481d718d0fa35b9c95007
Hao Yuhang <haoyh@yahoo.com>
2017-12-08 23:37:03 +0800

    test pusher

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -10,7 +10,11 @@ class EventServiceProvider extends ServiceProvider {
      *
      * @var array
      */
-    protected $listen = [];
+    protected $listen = [
+        'App\Events\Event' => [
+            'App\Listeners\EventListener'
+        ]
+    ];
     
     protected $subscribe = [
         'App\Listeners\DepartmentEventSubscriber',
