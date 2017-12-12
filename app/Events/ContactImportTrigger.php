4d6dd332da60b517c1def03ef2b0d52227d58f95
Guo Xiaolin <352326679@qq.com>
2017-12-12 17:26:43 +0800

    test

--- app/Events/ContactImportTrigger.php
+++ app/Events/ContactImportTrigger.php
@@ -10,7 +10,7 @@ use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Queue\SerializesModels;
 
-class contactImportTrigger implements ShouldBroadcast
+class ContactImportTrigger implements ShouldBroadcast
 {
     use Dispatchable, InteractsWithSockets, SerializesModels;
     public $data;
