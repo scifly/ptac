d9192b7872b4f69a6e4ed98059a0793122226722
Guo Xiaolin <352326679@qq.com>
2017-12-12 11:04:01 +0800

    test

--- resources/assets/js/bootstrap.js
+++ resources/assets/js/bootstrap.js
@@ -30,6 +30,7 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
  */
 
 let token = document.head.querySelector('meta[name="csrf-token"]');
+alert(token);
 
 if (token) {
     window.axios.defaults.headers.common['X-CSRF-TOKEN'] = token.content;
