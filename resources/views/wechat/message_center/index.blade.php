27a5cc9535220645b0000e322869a7ce738075b8
Wang Dongxi <1756996963@qq.com>
2017-12-22 16:15:12 +0800

    ceshi

--- resources/views/wechat/message_center/index.blade.php
+++ resources/views/wechat/message_center/index.blade.php
@@ -165,26 +165,6 @@
 @endsection
 @section('script')
     <script>
-        $('.selectlist-layout').click(function () {
-            $('.select-container').toggle();
-            $('.select-ul').slideToggle('fast');
-        });
-
-        $('.select-ul li').click(function () {
-            $('.select-container').toggle();
-            $('.select-ul').slideToggle('fast');
-            $('.select-ul li').removeClass('c-green');
-            $(this).addClass('c-green');
-            var html = '' + ($(this).text()) + '<i class="icon iconfont icon-arrLeft-fill"></i>';
-            $('.select-box ').html(html);
-        });
-
-        $('.teacher-list-box').click(function () {
-            alert()
-            window.location.href = '../public/message_show';
-        });
-        //		$('#searchCancel').click(function(){
-        //
-        //		});
+       
     </script>
 @endsection
