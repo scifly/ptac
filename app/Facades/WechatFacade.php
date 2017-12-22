f98819e998affdea23015d02dea61718023f3f67
Yuan Hongbin <2162998418@qq.com>
2017-12-22 09:32:39 +0800

    messagecenter

--- app/Facades/WechatFacade.php
+++ app/Facades/WechatFacade.php
@@ -190,8 +190,8 @@ class Wechat extends Facade {
             self::SCOPE_USERINFO,
             $agentId
         );
-        return $url;
-        // return "window.location = \"{$url}\"";
+
+        return "window.location = \"{$url}\"";
         
     }
     
