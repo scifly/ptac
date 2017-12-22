9b4d6dedb36907fd743f4856f241700a82e260da
Guo Xiaolin <352326679@qq.com>
2017-12-22 14:54:16 +0800

    updated message

--- resources/views/message/index.blade.php
+++ resources/views/message/index.blade.php
@@ -113,7 +113,6 @@
                                             </button>
                                         </div>
                                         <div class="tab-pane" id="content_image">
-<<<<<<< HEAD
                                             <button id="add-image" class="btn btn-box-tool" type="button" style="margin-top: 3px;">
                                                 <i class="fa fa-plus text-blue" style="position: relative;">
                                                 	&nbsp;添加图片
@@ -121,7 +120,6 @@
                                                 	<input type="file" id="file-image" onchange="uploadfile(this)" name="uploadFile" accept="image/*" style="position: absolute;z-index: 1;opacity: 0;width: 100%;height: 100%;top: 0;left: 0;"/>
                                                 </i>
                                             </button>
-=======
                                             <form id="uploadForm" enctype="multipart/form-data">
 
                                                 <button id="add-image" class="btn btn-box-tool" type="button" style="margin-top: 3px;">
@@ -133,7 +131,6 @@
                                                 </button>
                                             </form>
 
->>>>>>> refs/remotes/origin/master
                                         </div>
                                         <div class="tab-pane" id="content_audio">
                                             <button id="add-audio" class="btn btn-box-tool" type="button" style="margin-top: 3px;">
