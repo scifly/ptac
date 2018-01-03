64316213beafc74a3c141be48b13f5873a8fb427
Wang Dongxi <1756996963@qq.com>
2018-01-03 16:04:34 +0800

    modified create.js

--- public/js/wechat/message_center/create.js
+++ public/js/wechat/message_center/create.js
@@ -192,7 +192,7 @@ function show_group() {
         $.ajax({
             type: 'GET',
             data: {},
-            url: '../public/message_dept/' + id,
+            url: '../message_dept/' + id,
             success: function (result) {
                 if (result.statusCode === 200) {
                     choose_box.html(result.message);
@@ -309,7 +309,7 @@ function upload_cover() {
     formData.append('_token', token);
     formData.append('type', msg_type.attr('data-values'));
     $.ajax({
-        url: '../public/message_upload',
+        url: '../message_upload',
         type: 'POST',
         cache: false,
         data: formData,
@@ -396,7 +396,7 @@ $(function () {
         formData.append('file', $('#uploaderInput')[0].files[0]);
         formData.append('_token', token);
         $.ajax({
-            url: '../public/message_upload',
+            url: '../message_upload',
             type: 'POST',
             cache: false,
             data: formData,
@@ -420,7 +420,7 @@ $(function () {
         formData.append('_token', token);
         formData.append('type', msg_type.attr('data-values'));
         $.ajax({
-            url: "../public/message_upload",
+            url: "../message_upload",
             data: formData,
             type: 'POST',
             dataType: 'json',
@@ -448,7 +448,7 @@ $(function () {
         formData.append('_token', token);
         formData.append('type', msg_type.attr('data-values'));
         $.ajax({
-            url: "../public/message_upload",
+            url: "../message_upload",
             data: formData,
             type: 'POST',
             dataType: 'json',
@@ -550,7 +550,7 @@ $(function () {
                 'type': type,
                 'mediaid': wechat_media_id
             },
-            url: '../public/message_store',
+            url: '../message_store',
             success: function (result) {
                 if (result.statusCode === 200) {
                     $.alert('消息发送成功！', function () {
@@ -573,7 +573,7 @@ function getdept() {
         $(this).nextAll().remove();
         $.ajax({
             type: 'GET',
-            url: '../public/message_dept/' + id,
+            url: '../message_dept/' + id,
             success: function (result) {
                 if (result.statusCode === 200) {
                     choose_box.html(result.message);
