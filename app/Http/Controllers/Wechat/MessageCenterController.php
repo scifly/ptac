74b07f239cd69360a728989e85ac20d05031ae5c
Yuan Hongbin <2162998418@qq.com>
2018-01-05 16:39:11 +0800

    test

--- app/Http/Controllers/Wechat/MessageCenterController.php
+++ app/Http/Controllers/Wechat/MessageCenterController.php
@@ -491,7 +491,7 @@ class MessageCenterController extends Controller {
                             'title'           => $input['title'],
                             'comm_type_id'    => CommType::whereName('应用')->first()->id,
                             'app_id'          => App::whereName('信息发送')->first()->id,
-                            'msl_id'          => 22,
+                            'msl_id'          => $input['msl_id'],
                             'content'         => $input['content'],
                             'serviceid'       => 0,
                             'message_id'      => 0,
