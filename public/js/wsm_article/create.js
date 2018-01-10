9fed56e65f0b21be513df6551573f5d1d0eb0f8c
Yuan Hongbin <2162998418@qq.com>
2018-01-10 16:21:46 +0800

    test

--- public/js/wsm_article/create.js
+++ public/js/wsm_article/create.js
@@ -1,56 +1,57 @@
 page.create('formWsmArticle', 'wsm_articles');
-$(function () {
-    $.getMultiScripts(page.plugins.ueditor_all, page.siteRoot());
-    $.getMultiScripts(page.plugins.ueditor_config, page.siteRoot());
-    // var editor = UE.getEditor('container').render('container');//初始化富文本编辑器
-    UE.delEditor('container');
-    var ue = UE.getEditor('container',{
-        initialFrameHeight:500
-    });
-    var $pre = $('.preview');
-    var $uploadFile = $('#uploadFile');
-    // 初始化
-    $uploadFile.fileinput({
-        language: 'zh',
-        theme: 'explorer',
-        uploadUrl: "/wap_sites/uploadImages",
-        uploadAsync: false,
-        maxFileCount: 5,
-        minImageWidth: 50, //图片的最小宽度
-        minImageHeight: 50,//图片的最小高度
-        maxImageWidth: 1000,//图片的最大宽度
-        maxImageHeight: 1000,//图片的最大高度
-        allowedFileExtensions: ['jpg', 'gif', 'png'],//接收的文件后缀
-        fileActionSettings: {
-            showRemove: true,
-            showUpload: false,
-            showDrag: false
-        },
-        uploadExtraData: {
-            '_token': $('#csrf_token').attr('content')
-        }
-    });
-    // 上传成功
-    $uploadFile.on("filebatchuploadsuccess", function (event, data, previewId, index) {
-        // 填充数据
-        var response = data.response.data;
-        $.each(response, function (index, obj) {
-            $pre.append('<div class="img-item"><img src="../' + obj.path + '" id="' + obj.id + '"><div class="del-mask"><i class="delete glyphicon glyphicon-trash"></i></div></div>');
-            $pre.append('<input type="hidden" name="media_ids[]" value="' + obj.id + '">');
-        });
-        // 成功后关闭弹窗
-        setTimeout(function () {
-            $('#modalPic').modal('hide');
-        }, 800)
-    });
+page.loadCss(page.plugins.ueditor_all.css);
+    $.getMultiScripts([page.plugins.ueditor_all.js], page.siteRoot()).done(function () {
+        $.getMultiScripts([page.plugins.ueditor_config.js], page.siteRoot()).done(function () {
+            // var editor = UE.getEditor('container').render('container');//初始化富文本编辑器
+            UE.delEditor('container');
+            var ue = UE.getEditor('container',{
+                initialFrameHeight:500
+            });
+            var $pre = $('.preview');
+            var $uploadFile = $('#uploadFile');
+            // 初始化
+            $uploadFile.fileinput({
+                language: 'zh',
+                theme: 'explorer',
+                uploadUrl: "/wap_sites/uploadImages",
+                uploadAsync: false,
+                maxFileCount: 5,
+                minImageWidth: 50, //图片的最小宽度
+                minImageHeight: 50,//图片的最小高度
+                maxImageWidth: 1000,//图片的最大宽度
+                maxImageHeight: 1000,//图片的最大高度
+                allowedFileExtensions: ['jpg', 'gif', 'png'],//接收的文件后缀
+                fileActionSettings: {
+                    showRemove: true,
+                    showUpload: false,
+                    showDrag: false
+                },
+                uploadExtraData: {
+                    '_token': $('#csrf_token').attr('content')
+                }
+            });
+            // 上传成功
+            $uploadFile.on("filebatchuploadsuccess", function (event, data, previewId, index) {
+                // 填充数据
+                var response = data.response.data;
+                $.each(response, function (index, obj) {
+                    $pre.append('<div class="img-item"><img src="../' + obj.path + '" id="' + obj.id + '"><div class="del-mask"><i class="delete glyphicon glyphicon-trash"></i></div></div>');
+                    $pre.append('<input type="hidden" name="media_ids[]" value="' + obj.id + '">');
+                });
+                // 成功后关闭弹窗
+                setTimeout(function () {
+                    $('#modalPic').modal('hide');
+                }, 800)
+            });
 
-    // modal关闭，内容清空
-    $('#modalPic').on('hide.bs.modal', function () {
-        $uploadFile.fileinput('clear');
-    });
-    // 点击删除按钮
-    $('body').on('click', '.delete', function () {
-        $(this).parent().parent().remove();
-        $pre.append('<input type="hidden" name="del_ids[]" value="' + $(this).parent().siblings().attr('id') + '">');
-    });
+            // modal关闭，内容清空
+            $('#modalPic').on('hide.bs.modal', function () {
+                $uploadFile.fileinput('clear');
+            });
+            // 点击删除按钮
+            $('body').on('click', '.delete', function () {
+                $(this).parent().parent().remove();
+                $pre.append('<input type="hidden" name="del_ids[]" value="' + $(this).parent().siblings().attr('id') + '">');
+            });
+        });
 });
