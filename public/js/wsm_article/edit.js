c82715c7c80d75438d25e4eb584b553852abfffe
Guo Xiaolin <352326679@qq.com>
2018-01-11 13:14:17 +0800

    updated

--- public/js/wsm_article/edit.js
+++ public/js/wsm_article/edit.js
@@ -33,7 +33,7 @@ $(function () {
         // 填充数据
         var response = data.response.data;
         $.each(response, function (index, obj) {
-            $pre.append('<div class="img-item"><img src="../../' + obj.path + '" id="' + obj.id + '"><div class="del-mask"><i class="delete glyphicon glyphicon-trash"></i></div></div>');
+            $pre.append('<div class="img-item"><img src="' + obj.path + '" id="' + obj.id + '"><div class="del-mask"><i class="delete glyphicon glyphicon-trash"></i></div></div>');
             $pre.append('<input type="hidden" name="media_ids[]" value="' + obj.id + '">');
         });
         // 成功后关闭弹窗
