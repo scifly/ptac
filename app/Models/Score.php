1c32c2fc31ce676394630556b3784a4bf40575ca
Yuan Hongbin <2162998418@qq.com>
2018-01-23 10:54:32 +0800

    score

--- app/Models/Score.php
+++ app/Models/Score.php
@@ -572,11 +572,12 @@ class Score extends Model {
         return $result;
         
     }
-
+    
     /**
      * @param UploadedFile $file
      * @param $input
      * @return array
+     * @throws \PHPExcel_Exception
      */
     static function upload(UploadedFile $file, $input) {
         
