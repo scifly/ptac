79a057068fc8d42e0eeffdbb869db76e0d756347
Yuan Hongbin <2162998418@qq.com>
2018-01-26 16:21:46 +0800

    semester edit

--- public/js/semester/edit.js
+++ public/js/semester/edit.js
@@ -1,1 +1,16 @@
 page.edit('formSemester', 'semesters');
+page.loadCss(page.plugins.timepicker.css);
+
+function datetime($initTime) {
+    $initTime.datetimepicker({
+        dateFormat: "yy-mm-dd",
+        showSecond: true,
+        timeFormat: 'hh:mm:ss'
+    })
+}
+
+$.getMultiScripts([page.plugins.timepicker.js, page.plugins.timepicker.jscn], page.siteRoot())
+    .done(function () {
+        datetime($(".start_date"));
+        datetime($(".end_date"));
+    });
