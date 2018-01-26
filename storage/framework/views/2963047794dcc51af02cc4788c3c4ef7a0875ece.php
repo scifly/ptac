5c13096c7d3c1592d2a3759967739d46d51b9672
Yuan Hongbin <2162998418@qq.com>
2018-01-26 14:12:48 +0800

    password reset

---
+++ storage/framework/views/2963047794dcc51af02cc4788c3c4ef7a0875ece.php
@@ -0,0 +1,34 @@
+<div class="box box-default box-solid">
+    <div class="box-header with-border">
+        <?php echo $__env->make('partials.form_header', array_except(get_defined_vars(), array('__data', '__path')))->render(); ?>
+    </div>
+    <div class="box-body">
+        <div class="form-horizontal">
+            <?php if(isset($examType) && !empty($examType['id'])): ?>
+                <?php echo e(Form::hidden('id', $examType['id'], ['id' => 'id'])); ?>
+
+            <?php endif; ?>
+            <div class="form-group">
+                <?php echo Form::label('name', '名称', [
+                    'class' => 'col-sm-3 control-label'
+                ]); ?>
+
+                <div class="col-sm-6">
+                    <?php echo Form::text('name', null, [
+                        'class' => 'form-control',
+                        'placeholder' => '(不超过40个汉字)',
+                        'required' => 'true',
+                        'data-parsley-length' => '[4, 40]'
+                    ]); ?>
+
+                </div>
+            </div>
+            <?php echo $__env->make('partials.remark', array_except(get_defined_vars(), array('__data', '__path')))->render(); ?>
+            <?php echo $__env->make('partials.enabled', [
+                'id' => 'enabled',
+                'value' => $examType['enabled'] ?? null
+            ], array_except(get_defined_vars(), array('__data', '__path')))->render(); ?>
+        </div>
+    </div>
+    <?php echo $__env->make('partials.form_buttons', array_except(get_defined_vars(), array('__data', '__path')))->render(); ?>
+</div>
