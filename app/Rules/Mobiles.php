75981997a4fc7363a5a2f281a0e724f06fd71ac2
Wang Dongxi <1756996963@qq.com>
2018-01-30 13:20:34 +0800

    modfied Mobiles.php

--- app/Rules/Mobiles.php
+++ app/Rules/Mobiles.php
@@ -21,7 +21,7 @@ class Mobiles implements Rule {
         $mobile = Mobile::whereMobile($value['mobile'])
             ->where('id', '!=', $value['id'])
             ->get()->toArray();
- 
+
         if ($mobile || !preg_match(self::PHONEREG, $value['mobile'])) {
             return false;
         }
