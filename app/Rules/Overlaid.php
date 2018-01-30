ec92b9e640c09117cb7a2f3578454dbce8286409
Wang Dongxi <1756996963@qq.com>
2018-01-30 11:52:04 +0800

    modified OverLaid.php

--- app/Rules/Overlaid.php
+++ app/Rules/Overlaid.php
@@ -34,7 +34,7 @@ class Overlaid implements Rule
             case 'educator':
                 if ($value[3]) {
                     $settings = EducatorAttendanceSetting::where('id', '<>', $value[3])
-
+                        ->where('enabled','1')
                         ->pluck('end', 'start')->toArray();
                 } else {
                     $settings = EducatorAttendanceSetting::pluck('end', 'start')->toArray();
