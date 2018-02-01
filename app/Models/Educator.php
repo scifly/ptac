e57dca393d35944b89d9be52663ecae13b2cc380
Yuan Hongbin <2162998418@qq.com>
2018-02-01 14:26:27 +0800

    update phpdoc

--- app/Models/Educator.php
+++ app/Models/Educator.php
@@ -437,7 +437,7 @@ class Educator extends Model {
         return true;
         
     }
-
+    
     /**
      * 删除教职员工
      *
@@ -445,6 +445,8 @@ class Educator extends Model {
      * @param bool $fireEvent
      * @return bool
      * @throws Exception
+     * @throws \ReflectionException
+     * @throws \Throwable
      */
     static function remove($id, $fireEvent = false) {
 
