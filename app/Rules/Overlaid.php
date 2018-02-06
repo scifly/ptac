bf108ab3e8f4af06c29c7eb21f6c52c4935a4435
Wang Dongxi <1756996963@qq.com>
2018-02-06 15:38:38 +0800

    test

--- app/Rules/Overlaid.php
+++ app/Rules/Overlaid.php
@@ -72,9 +72,9 @@ class Overlaid implements Rule
                         ->pluck('end_date','start_date')->toArray();
                 }else{
                     $settings =Semester::whereSchoolId($schoolId)
-                        ->where('enabled',1)
                         ->where('end_date','!=',$start)
                         ->orWhere('start_date','!=',$end)
+                        ->where('enabled',1)
                         ->pluck('end_date','start_date')->toArray();
                 }
                 break;
