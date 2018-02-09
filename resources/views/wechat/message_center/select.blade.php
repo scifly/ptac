56bd3f5461b2c240a20614505c1c4220b7c69a77
Yuan Hongbin <2162998418@qq.com>
2018-02-09 14:49:16 +0800

    messagecenter

--- resources/views/wechat/message_center/select.blade.php
+++ resources/views/wechat/message_center/select.blade.php
@@ -1,3 +1,4 @@
+@if(isset($departments))
 @foreach($departments as $department)
     <div class="air-choose-item" style="position: relative;">
         <label class="weui-cell weui-check__label" id="group-{{ $department->id }}" data-item="{{ $department->id }}" data-uid="{{ $department->id }}" data-type="group">
@@ -13,7 +14,42 @@
         <a class="icon iconfont icon-jiantouyou show-group" style="position:absolute;top: 0;right:0;height: 55px;line-height:55px;z-index: 1;width: 30px;"></a>
     </div>
 @endforeach
-
+@endif
+@if(isset($graLists))
+    @foreach($graLists as $department)
+        <div class="air-choose-item" style="position: relative;">
+            <label class="weui-cell weui-check__label" id="group-{{ $department->id }}" data-item="{{ $department->id }}" data-uid="{{ $department->id }}" data-type="group">
+                <div class="weui-cell__hd">
+                    <input type="checkbox" class="weui-check choose-item-btn" name="checkbox" >
+                    <i class="weui-icon-checked"></i>
+                </div>
+                <div class="weui-cell__bd">
+                    <img src="{{ asset('img/department.png') }}" style="border-radius: 0;" class="js-go-detail lazy" width="75" height="75">
+                    <span class="contacts-text">{{ $department->name }}</span>
+                </div>
+            </label>
+            <a class="icon iconfont icon-jiantouyou show-group" style="position:absolute;top: 0;right:0;height: 55px;line-height:55px;z-index: 1;width: 30px;"></a>
+        </div>
+    @endforeach
+@endif
+@if(isset($claLists))
+    @foreach($claLists as $department)
+        <div class="air-choose-item" style="position: relative;">
+            <label class="weui-cell weui-check__label" id="group-{{ $department->id }}" data-item="{{ $department->id }}" data-uid="{{ $department->id }}" data-type="group">
+                <div class="weui-cell__hd">
+                    <input type="checkbox" class="weui-check choose-item-btn" name="checkbox" >
+                    <i class="weui-icon-checked"></i>
+                </div>
+                <div class="weui-cell__bd">
+                    <img src="{{ asset('img/department.png') }}" style="border-radius: 0;" class="js-go-detail lazy" width="75" height="75">
+                    <span class="contacts-text">{{ $department->name }}</span>
+                </div>
+            </label>
+            <a class="icon iconfont icon-jiantouyou show-group" style="position:absolute;top: 0;right:0;height: 55px;line-height:55px;z-index: 1;width: 30px;"></a>
+        </div>
+    @endforeach
+@endif
+@if($users)
 @foreach($users as $user)
     <div class="air-choose-item" style="position: relative;">
         <label class="weui-cell weui-check__label" id="person-{{ $user->id }}" data-item="{{ $user->id }}" data-uid="{{ $user->id }}" data-type="person">
@@ -28,3 +64,4 @@
         </label>
     </div>
 @endforeach
+@endif
