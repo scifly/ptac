8cbcbd43edb0d9c142520453e213f1074c478446
Hao Yuhang <haoyh@yahoo.com>
2018-03-28 16:23:09 +0800

    debug

--- resources/views/menu/index.blade.php
+++ resources/views/menu/index.blade.php
@@ -1,6 +1,18 @@
 <div class="box box-default box-solid">
     <div class="box-header with-border">
-        @include('partials.list_header')
+        {{--@include('partials.list_header')--}}
+        <span id="breadcrumb" style="color: #999; font-size: 13px;">{!! $breadcrumb !!}</span>
+        <div class="box-tools pull-right">
+            @if(isset($buttons))
+                @foreach($buttons as $button)
+                    @can('act', $uris[$button['id']])
+                        <button id="{{ $button['id'] }}" type="button" class="btn btn-box-tool">
+                            <i class="{{ $button['icon'] }} text-blue"> {{ $button['label'] }}</i>
+                        </button>
+                    @endcan
+                @endforeach
+            @endif
+        </div>
     </div>
     <div class="box-body">
         <div id="tree" class="col-md-12"></div>
