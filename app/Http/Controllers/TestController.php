5c3a6ed84ae126d740aca7cd889a25afc48a14e6
Hao Yuhang <haoyh@yahoo.com>
2018-04-03 16:39:24 +0800

    test

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -38,7 +38,7 @@ class TestController extends Controller {
     
     public function index() {
 
-        $values = User::whereEnabled(1)->get()->groupBy('group_id');
+        $values = User::whereEnabled(1)->groupBy('group_id')->get();
         dd($values);
         try {
             $client = new Client();
