58ab36335f76f37641463bf059a7729bd3ad08ad
Hao Yuhang <haoyh@yahoo.com>
2018-04-08 17:23:52 +0800

    test

--- public/js/home/page.js
+++ public/js/home/page.js
@@ -277,7 +277,7 @@ var page = {
     initDatatable: function (table, options) {
         var selected = [];
         $('#data-table tbody').on('click', 'tr', function () {
-            console.log(this.children.eq(0).text());
+            console.log(this.children);
         });
         var showTable = function () {
             var $datatable = $('#data-table');
