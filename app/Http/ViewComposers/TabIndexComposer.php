f54ad86f575f25f73179b2acc1e423c0e683e839
Hao Yuhang <haoyh@yahoo.com>
2018-04-09 09:59:35 +0800

    test datatable selection

--- app/Http/ViewComposers/TabIndexComposer.php
+++ app/Http/ViewComposers/TabIndexComposer.php
@@ -11,6 +11,18 @@ class TabIndexComposer {
     public function compose(View $view) {
         
         $view->with([
+            'buttons' => [
+                'selectAll' => [
+                    'id' => 'selectAll',
+                    'label' => '全选',
+                    'icon' => 'fa fa-circle',
+                ],
+                'deselectAll' => [
+                    'id' => 'deselectAll',
+                    'label' => '取消全选',
+                    'icon' => 'fa fa-circle-o'
+                ]
+            ],
             'titles' => ['#', '名称', '控制器', '角色', '默认功能', '创建于', '更新于', '状态'],
             'uris'  => $this->uris(),
         ]);
