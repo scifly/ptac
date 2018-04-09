1ee37218158024faa31778b8e0ddf96bcc8cb617
Hao Yuhang <haoyh@yahoo.com>
2018-04-09 10:34:29 +0800

    test datatable selection

--- app/Http/ViewComposers/TabIndexComposer.php
+++ app/Http/ViewComposers/TabIndexComposer.php
@@ -11,6 +11,7 @@ class TabIndexComposer {
     public function compose(View $view) {
         
         $view->with([
+            'batch' => true,
             'titles' => ['#', '名称', '控制器', '角色', '默认功能', '创建于', '更新于', '状态'],
             'uris'  => $this->uris(),
         ]);
