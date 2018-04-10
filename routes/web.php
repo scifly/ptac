edfc67765760c29ffc9d531bb44b835a665de71d
Hao Yuhang <haoyh@yahoo.com>
2018-04-10 09:56:06 +0800

    test datatables selection feature

--- routes/web.php
+++ routes/web.php
@@ -306,7 +306,12 @@ Route::group(['prefix' => 'companies'], routes('CompanyController'));
 Route::group(['prefix' => 'corps'], routes('CorpController'));
 # 菜单管理 - action设置.卡片设置.菜单设置
 Route::group(['prefix' => 'actions'], routes('ActionController'));
-Route::group(['prefix' => 'tabs'], routes('TabController'));
+Route::group(['prefix' => 'tabs'], function () {
+    $c = 'TabController';
+    Route::get('index', $c . '@index');
+    Route::get('edit/{id}', $c . '@edit');
+    Route::put('update/{id?}', $c . '@update');
+});
 Route::group(['prefix' => 'menus'], routeItem('MenuController'));
 Route::group(['prefix' => 'menus'], function () {
     $c = 'MenuController';
