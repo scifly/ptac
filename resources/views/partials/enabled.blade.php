a3e47befd3a3134bc1a72a379d112ebddac1ca20
Hao Yuhang <haoyh@yahoo.com>
2018-04-10 11:39:40 +0800

    test datatables selection feature

--- resources/views/partials/enabled.blade.php
+++ resources/views/partials/enabled.blade.php
@@ -1,7 +1,7 @@
 <?php $lblStyle = "margin: 0 5px; vertical-align: middle; font-weight: normal;"; ?>
 <div class="form-group">
     <label for="{{ $id }}" class="col-sm-3 control-label">
-        @if(isset($label)) {{ $label }} @else 状态 @endif
+        {{ $label ?? '状态' }}
     </label>
     <div class="col-sm-6" style="padding-top: 5px;">
         <input id="{{ $id }}1" @if($value) checked @endif
