0707b176eb7aa44212a7de178bd2953333a01429
Hao Yuhang <haoyh@yahoo.com>
2018-04-12 21:42:50 +0800

    test

--- resources/views/partials/site_header.blade.php
+++ resources/views/partials/site_header.blade.php
@@ -7,23 +7,22 @@
         </a>
         <div class="navbar-custom-menu">
             <ul class="nav navbar-nav">
-                <!--提醒-->
-                {{--<li class="dropdown notifications-menu">--}}
-                    {{--@include('partials.site_notification')--}}
-                {{--</li>--}}
-                {{--<!--任务-->--}}
-                {{--<li class="dropdown tasks-menu">--}}
-                    {{--@include('partials.site_task')--}}
-                {{--</li>--}}
-                <!--用户账号-->
-
                 <li class="dropdown messages-menu open">
                     <a href="#" class="dropdown-toggle" data-toggle="dropdown" aria-expanded="true">
                         <i class="fa fa-send-o" style="margin-right: 15px;"></i>四川盛世华唐
                     </a>
                 </li>
+                <!--用户账号-->
                 <li class="dropdown user user-menu">
                     @include('partials.site_user')
+                </li>
+                <!-- 提醒 -->
+                <li class="dropdown notifications-menu">
+                    @include('partials.site_notification')
+                </li>
+                <!--任务-->
+                <li class="dropdown tasks-menu">
+                    @include('partials.site_task')
                 </li>
             </ul>
         </div>
