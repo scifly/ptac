790b72566ed9501a27e745b1f87f39e5fc7c97db
Hao Yuhang <haoyh@yahoo.com>
2018-04-12 20:51:13 +0800

    test

--- public/js/subject/index.js
+++ public/js/subject/index.js
@@ -1,4 +1,4 @@
 page.index('subjects', [
-    { className: 'text-center', targets: [1, 2, 5, 6]},
+    { className: 'text-center', targets: [2, 5, 6]},
     { className: 'text-right', targets: [3, 4]},
 ]);
