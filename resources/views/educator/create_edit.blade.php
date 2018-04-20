1620042ee44b100a3d5f4eb1b195d3ae9c90f20c
Hao Yuhang <haoyh@yahoo.com>
2018-04-20 14:13:32 +0800

    debug

--- resources/views/educator/create_edit.blade.php
+++ resources/views/educator/create_edit.blade.php
@@ -168,7 +168,7 @@
                     @else
                         <input type="hidden" id="selected-node-ids" value=""/>
                     @endif
-                    <a id="choose" class="btn btn-primary" style="margin-bottom: 5px">修改</a>
+                    <a id="choose" style="margin-bottom: 5px"><i class="fa fa-sitemap"></i>选择</a>
                 </div>
             </div>
             <!-- 所属教职员工组 -->
