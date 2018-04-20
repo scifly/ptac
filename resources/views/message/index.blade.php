c552831464589cd54ffa487c273bc706d9fec963
Hao Yuhang <haoyh@yahoo.com>
2018-04-20 14:59:45 +0800

    debug

--- resources/views/message/index.blade.php
+++ resources/views/message/index.blade.php
@@ -186,33 +186,18 @@
                         {!! Form::close() !!}
                     </div>
                 </div>
-                <div class="tab-pane" id="tab02">
-                    {{--<table id="data-table" style="width: 100%"--}}
-                           {{--class="display nowrap table table-striped table-bordered table-hover table-condensed">--}}
-                        {{--<thead>--}}
-                        {{--<tr class="bg-info">--}}
-                            {{--@foreach ($titles as $title)--}}
-                                {{--<th>{!! $title !!}</th>--}}
-                            {{--@endforeach--}}
-                        {{--</tr>--}}
-                        {{--</thead>--}}
-                        {{--<tbody></tbody>--}}
-                    {{--</table>--}}
-                    <div class="box box-default box-solid">
-                        <div class="box-body">
-                            <table id="data-table" style="width: 100%"
-                                   class="display nowrap table table-striped table-bordered table-hover table-condensed">
-                                <thead>
-                                <tr class="bg-info">
-                                    @foreach ($titles as $title)
-                                        <th>{!! $title !!}</th>
-                                    @endforeach
-                                </tr>
-                                </thead>
-                                <tbody></tbody>
-                            </table>
-                        </div>
-                    </div>
+                <div class="tab-pane" id="tab02" style="width: 100%;">
+                    <table id="data-table" style="width: 100%"
+                           class="display nowrap table table-striped table-bordered table-hover table-condensed">
+                        <thead>
+                        <tr class="bg-info">
+                            @foreach ($titles as $title)
+                                <th>{!! $title !!}</th>
+                            @endforeach
+                        </tr>
+                        </thead>
+                        <tbody></tbody>
+                    </table>
                 </div>
                 <div class="tab-pane" id="tab03"></div>
             </div>
