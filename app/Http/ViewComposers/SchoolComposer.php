73b0675b339b4d38e534c796b186f27091336dd0
Hao Yuhang <haoyh@yahoo.com>
2018-04-21 18:49:03 +0800

    test

--- app/Http/ViewComposers/SchoolComposer.php
+++ app/Http/ViewComposers/SchoolComposer.php
@@ -3,20 +3,48 @@ namespace App\Http\ViewComposers;
 
 use App\Helpers\ModelTrait;
 use App\Models\Corp;
+use App\Models\Menu;
+use App\Models\School;
 use App\Models\SchoolType;
 use Illuminate\Contracts\View\View;
+use Illuminate\Support\Facades\Request;
 
 class SchoolComposer {
     
     use ModelTrait;
     
+    protected $menu;
+    
+    function __construct(Menu $menu) {
+        
+        $this->menu = $menu;
+        
+    }
+    
     public function compose(View $view) {
         
-        $view->with([
-            'schoolTypes' => SchoolType::pluck('name', 'id'),
-            'corps'       => Corp::pluck('name', 'id'),
+        $corps = Corp::whereEnabled(1)->get()->pluck('name', 'id');
+        $schoolTypes = SchoolType::whereEnabled(1)->get()->pluck('name', 'id');
+        $params = [
+            'schoolTypes' => $schoolTypes,
+            'corps'       => $corps,
             'uris'        => $this->uris(),
-        ]);
+            'disabled'    => null   # diaabled - 是否显示'返回列表'和'取消'按钮
+        ];
+        if ($this->menu->menuId(session('menuId'))) {
+            if (Request::route('id')) {
+                $school = School::find(Request::route('id'));
+                $params['corps'] = [
+                    $school->corp_id => $school->corp->name
+                ];
+                $params['schoolTypes'] = [
+                    $school->school_type_id => $school->schoolType->name
+                ];
+                $params['disabled'] = true;
+            }
+        }
+        
+        $view->with($params);
         
     }
     
