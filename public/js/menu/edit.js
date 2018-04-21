9f6bb970ab94659ddd46fc976e28d855290a3be9
Hao Yuhang <haoyh@yahoo.com>
2018-04-21 18:22:49 +0800

    test

--- public/js/menu/edit.js
+++ public/js/menu/edit.js
@@ -1,3 +1,13 @@
-
-;
+var options = [
+    {
+        option: {
+            templateResult: page.formatState,
+            templateSelection: page.formatState
+        },
+        id: 'icon_id'
+    },
+    {
+        id: 'tab_ids'
+    }
+];
 page.edit('formMenu', 'menus', options);
