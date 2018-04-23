588e1f5b915e5d58fc1a6beb7723b1235e848f56
Hao Yuhang <haoyh@yahoo.com>
2018-04-23 14:31:22 +0800

    test

--- resources/views/home/company.blade.php
+++
@@ -1,9 +0,0 @@
-<section class="content-header">
-    <h1>
-        <i class="fa {!! $department['icon'] !!}">
-            <b>{!! $department['name']!!}</b>
-        </i>
-    </h1>
-</section>
-<section class="content">
-</section>
