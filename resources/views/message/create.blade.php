fc5f9d849d42d6c424389ca38de6f32f09cbf27b
Hao Yuhang <haoyh@yahoo.com>
2018-04-23 17:03:29 +0800

    test

--- resources/views/message/create.blade.php
+++
@@ -1,9 +0,0 @@
-{!! Form::open([
-    'url' => '/messages',
-    'method' => 'post',
-    'id' => 'formMessage',
-    'data-parsley-validate' => 'true'
-]) !!}
-@include('message.create_edit')
-{!! Form::close() !!}
-@include('message.department_tree')
