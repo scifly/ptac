45c02c400f28a9add52c088d8869f649e28b51bd
Hao Yuhang <haoyh@yahoo.com>
2018-04-23 15:17:12 +0800

    test

--- resources/views/partials/site_content_header.blade.php
+++ resources/views/partials/site_content_header.blade.php
@@ -1,13 +1,11 @@
 <section class="content-header">
-    @if (isset($enabled))
-        <h1>
-            <i class="fa {!! $department['icon'] !!}">
-                <b>{!! $department['name']!!}</b>
-            </i>
-        </h1>
-        <ol class="breadcrumb">
-            <li><a href="#"><i class="fa fa-dashboard"></i> 首页</a></li>
-            <li class="active">Dashboard</li>
-        </ol>
-    @endif
+    <h1>
+        <i class="fa {!! $department['icon'] !!}">
+            <b>{!! $department['name']!!}</b>
+        </i>
+    </h1>
+    {{--<ol class="breadcrumb">--}}
+        {{--<li><a href="#"><i class="fa fa-dashboard"></i> 首页</a></li>--}}
+        {{--<li class="active">Dashboard</li>--}}
+    {{--</ol>--}}
 </section>
