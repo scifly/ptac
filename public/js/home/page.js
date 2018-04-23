0a7692d523067088f90127d19e515e85edd6adee
Hao Yuhang <haoyh@yahoo.com>
2018-04-23 15:14:37 +0800

    test

--- public/js/home/page.js
+++ public/js/home/page.js
@@ -106,7 +106,7 @@ var page = {
         }
     },
     getWrapperContent: function (menuId, menuUrl, tabId, tabUrl) {
-        var $wrapper = $('.content .clearfix');
+        var $wrapper = $('.content.clearfix');
         $('.overlay').show();
         $.ajax({
             type: 'GET',
