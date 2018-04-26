62a52fb5fe9f432e0711669146af3c6fd27ff9ab
Hao Yuhang <haoyh@yahoo.com>
2018-04-26 17:31:24 +0800

    test

--- public/js/tree.js
+++ public/js/tree.js
@@ -414,13 +414,13 @@
                     // 初始化 根据后台数据节点数组 选中
                     $tree.jstree().select_node(selectedDepartmentIds);
                     if (type === 'contact') {
-                        $($tree.jstree().get_json($tree, {flat: true})).each(function (index, value) {
-                            var node = $("#tree").jstree().get_node(this.id, true);
+                        $($tree.jstree.get_json($tree, {flat: true})).each(function (index, value) {
+                            var node = $("#tree").jstree.get_node(this.id, true);
                             var $node = $('#' + node.id);
-                            if (node.selectable !== 1) {
+                            if (node.type !== 'disabled') {
                                 $node.find('i.jstree-checkbox').removeClass();
-                            // } else {
-                            //     $node.find('i[class=""]').addClass('jstree-icon jstree-checkbox');
+                            } else {
+                                $node.find('i[class=""]').addClass('jstree-icon jstree-checkbox');
                             }
                             // var lvl = node.parents.length;
                             // console.log('node index = ' + index + ' level = ' + lvl + ' id = ' + node.id);
