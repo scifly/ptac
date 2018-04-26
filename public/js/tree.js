e4eade67f1d2913c133a174254dad920bd840f4c
Hao Yuhang <haoyh@yahoo.com>
2018-04-26 16:39:08 +0800

    test

--- public/js/tree.js
+++ public/js/tree.js
@@ -414,7 +414,7 @@
                     // 初始化 根据后台数据节点数组 选中
                     $tree.jstree().select_node(selectedDepartmentIds);
                     if (type === 'contact') {
-                        $($tree.jstree.get_json($tree, {flat: true})).each(function () {
+                        $($tree.jstree().get_json($tree, {flat: true})).each(function () {
                             var node = $("#tree").jstree.get_node(this.id, true);
                             var $node = $('#' + node.id);
                             if (node.selectable !== 1) {
