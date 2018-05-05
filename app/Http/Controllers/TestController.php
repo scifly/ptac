0cd15b8782a6e9e97be1d4129b9947006ce4c5ea
Hao Yuhang <haoyh@yahoo.com>
2018-05-05 19:01:44 +0800

    test

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -50,7 +50,7 @@ class TestController extends Controller {
     
     public function index() {
 
-        dd(User::with('mobiles')->get()->toArray());
+        dd(User::whereIn('id', [1, 2])->with('mobiles')->get()->toArray());
         $corpid = 'wxe75227cead6b8aec';
         $secret = 'uorwAVlN3_EU31CDX0X1oQJk9lB0Or41juMH-cLcIE';
         $token = Wechat::getAccessToken($corpid, $secret, true);
