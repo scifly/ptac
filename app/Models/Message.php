46dedb16524b8a3a4a77dfaf1d6ec6f5e169aec6
Hao Yuhang <haoyh@yahoo.com>
2018-05-10 11:34:22 +0800

    test

--- app/Models/Message.php
+++ app/Models/Message.php
@@ -480,7 +480,7 @@ class Message extends Model {
             Wechat::uploadMedia(
                 $token['access_token'],
                 Request::input('type'),
-                ['media' => '@' . public_path($uploadedFile['path'])]
+                ['media' => '@' . $uploadedFile['path']]
             )
         );
         abort_if(
