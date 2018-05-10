4331918c7ffc0ba3e962c24439568963cba4062e
Hao Yuhang <haoyh@yahoo.com>
2018-05-10 11:16:04 +0800

    test

--- app/Facades/WechatFacade.php
+++ app/Facades/WechatFacade.php
@@ -985,7 +985,7 @@ class Wechat extends Facade {
 
     static function uploadMedia($accessToken, $type, $data) {
 
-        return self::curlPost(sprintf(self::URL_UPLOAD_MEDIA, $accessToken, $type), $data);
+        return self::curlPost(sprintf(self::URL_UPLOAD_MEDIA, $accessToken, $type), json_encode($data));
 
     }
 
