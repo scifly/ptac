0adfb16829a67d16cf1753d8173f359343ca010c
Hao Yuhang <haoyh@yahoo.com>
2018-05-12 18:48:12 +0800

    test

--- public/js/message/index.js
+++ public/js/message/index.js
@@ -90,6 +90,7 @@ $.getMultiScripts([plugins.handlebar.js]).done(function () {
         $.getMultiScripts([plugins.htmleditor.js]).done(function () {
             $.getMultiScripts([plugins.htmleditor.locale]).done(function () {
                 var options = {
+                    toolbar: { html: true },
                     size: 'xs',
                     locale: 'zh-CN'
                 };
