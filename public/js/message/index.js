3d0eced6e076b69892c37edfdc608d3ecd743e92
Hao Yuhang <haoyh@yahoo.com>
2018-05-12 19:01:34 +0800

    test

--- public/js/message/index.js
+++ public/js/message/index.js
@@ -428,6 +428,7 @@ function initEditor() {
                         toolbar: {
                             html: false,
                             size: 'xs',
+                            color: true,
                             font_styles: false
                         },
                         locale: 'zh-CN'
