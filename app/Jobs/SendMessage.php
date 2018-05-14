5e4efbe9c3ad23e3f3bae77cdc9966df86bc0085
Hao Yuhang <haoyh@yahoo.com>
2018-05-14 11:23:29 +0800

    test

--- app/Jobs/SendMessage.php
+++ app/Jobs/SendMessage.php
@@ -54,7 +54,7 @@ class SendMessage implements ShouldQueue {
     public function handle() {
         
         $message = new Message();
-        Log::debug(json_encode($message));
+        Log::debug('message: ' . json_encode($message));
         $response = [
             'userId' => $this->userId,
             'title' => __('messages.message.title'),
