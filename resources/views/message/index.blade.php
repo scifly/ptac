4835119979d4f429bfcdc6a84671aa9f9ca021b9
Hao Yuhang <haoyh@yahoo.com>
2018-05-15 10:58:00 +0800

    test

--- resources/views/message/index.blade.php
+++ resources/views/message/index.blade.php
@@ -27,8 +27,8 @@
             <div class="tab-content">
                 <div class="tab-pane active" id="tab01">
                     @include('message.targets')
-                    <div class="overlay" style="display: none; position: fixed; top: 0;left: 0; width: 100%; height: 100%;">
-                        <i class="fa fa-refresh fa-spin" style=""></i>
+                    <div class="overlay" style="">
+                        <i class="fa fa-refresh fa-spin"></i>
                     </div>
                     <div class="form-horizontal form-main" id="message">
                     {!! Form::open([
