26727c51ce0b8cd4801e3d1947e67f7490e03265
Hao Yuhang <haoyh@yahoo.com>
2018-05-18 11:39:43 +0800

    test

--- resources/views/wechat/score/graph.blade.php
+++ resources/views/wechat/score/graph.blade.php
@@ -4,36 +4,24 @@
 @endsection
 @section('content')
 	<div class="header">
-		<div class="title">
-			学生：{{$student->user->realname}}
-		</div>
-		<div class="myclass">
-			{{$student->squad->name}}
-		</div>
-		<input type="hidden" value="{{$student->id}}" id="student_id">
-		<input type="hidden" value="{{$exam->id}}" id="exam_id">
+		<div class="title">学生：{{ $student->user->realname }}</div>
+		<div class="myclass">{{ $student->squad->name }}</div>
+		{!! Form::hidden('student_id', $student->id, ['id' => 'student_id']) !!}
+		{!! Form::hidden('exam_id', $exam->id, ['id' => 'exam_id']) !!}
 	</div>
 	<div class="tab-bar">
 		<div class="tab-item active">
-			总分
-			<input type="hidden" value="-1" >
+			总分{!! Form::hidden('', -1) !!}
 		</div>
-		@foreach($data as $d)
+		@foreach($subjects as $id => $name)
 			<div class="tab-item">
-				{{$d->name}}
-				<input type="hidden" value="{{$d->id}}" >
+				{{ $name }}
+				{!! Form::hidden('', $id) !!}
 			</div>
 		@endforeach
 	</div>
-
-	<div class="line-table-con class-rank">
-
-	</div>
-
-	<div class="line-table-con grade-rank">
-
-	</div>
-
+	<div class="line-table-con class-rank"></div>
+	<div class="line-table-con grade-rank"></div>
 	<div style="height: 70px;width: 100%;"></div>
 	<div class="footerTab" >
 		<a class="btnItem footer-active">
@@ -46,7 +34,6 @@
 		</a>
 		<div style="clear: both;"></div>
 	</div>
-
 @endsection
 @section('script')
 	<script src="{{asset('/js/wechat/score/show.js')}}"></script>
