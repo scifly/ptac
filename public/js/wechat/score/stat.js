15156fbd81d8a55148ce94a79fd34ddccdcc21b2
Hao Yuhang <haoyh@yahoo.com>
2018-05-18 14:53:24 +0800

    test

--- public/js/wechat/score/stat.js
+++ public/js/wechat/score/stat.js
@@ -17,11 +17,11 @@ function getdata(){
 function showtable(arrayTime, legendData){
     var myChart = echarts.init($('#main')[0]),
         option = {
-        title : { text: '本考次占比', x:'center', top:0 },
+        title : { text: '本考次占比', x: 'center', top: 0 },
         tooltip : { trigger: 'item', formatter: "{a} <br/>{b} : {c} ({d}%)" },
         legend: { show:true, bottom: 10, left: 'center', data: legendData},
         series : [{
-            name: '成绩占比', type: 'pie', radius : '55%', center:['50%','45%'], data:arrayTime,
+            name: '成绩占比', type: 'pie', radius : '55%', center: ['50%','45%'], data: arrayTime,
             itemStyle: {
                 emphasis: { shadowBlur: 10, shadowOffsetX: 0, shadowColor: 'rgba(0, 0, 0, 0.5)' }
             }
