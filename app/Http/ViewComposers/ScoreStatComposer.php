26f9f3153d65ffb9b93f1f8b4f383a6a3658ef83
Hao Yuhang <haoyh@yahoo.com>
2018-05-21 15:57:38 +0800

    test

--- app/Http/ViewComposers/ScoreStatComposer.php
+++ app/Http/ViewComposers/ScoreStatComposer.php
@@ -20,18 +20,15 @@ class ScoreStatComposer {
         
         /** 班级、考试、科目和学生列表 */
         # 对当前用户可见的考试列表
-        $examList = Exam::whereEnabled(1)
-            ->whereIn('id', $this->examIds())
-            ->get()->pluck('name', 'id')
-            ->toArray();
+        $examList = Exam::whereEnabled(1)->whereIn('id', $this->examIds())
+            ->pluck('name', 'id')->toArray();
         reset($examList);
         $exam = Exam::find(key($examList));
         
         # 指定考试对应的班级
-        $classes = Squad::whereEnabled(1)
+        $classList = Squad::whereEnabled(1)
             ->whereIn('id', $exam ? explode(',', $exam->class_ids) : [])
-            ->get();
-        $classList = $classes->pluck('name', 'id')->toArray();
+            ->pluck('name', 'id')->toArray();
         reset($classList);
         $class = Squad::find(key($classList));
         
@@ -43,9 +40,6 @@ class ScoreStatComposer {
         # 指定考试对应的且对当前用户可见的学生列表
         $studentList = [];
         $students = Student::whereClassId($class ? $class->id : 0)->get();
-        // $students = Student::whereEnabled(1)
-        //     ->whereIn('class_id', array_intersect($classes->pluck('id')->toArray(), $this->classIds()))
-        //     ->get();
         foreach ($students as $student) {
             $studentList[$student->id] = $student->student_number . ' - ' . $student->user->realname;
         }
