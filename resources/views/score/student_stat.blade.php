c0880df44f206c0d8a8ae3f04b63d9a0d55f9f98
Hao Yuhang <haoyh@yahoo.com>
2018-05-22 09:44:26 +0800

    test

--- resources/views/score/student_stat.blade.php
+++ resources/views/score/student_stat.blade.php
@@ -6,62 +6,74 @@
                 <h4 class="modal-title">{!! $student ? $student->user->realname : '' !!}同学成绩分析</h4>
             </div>
             <div class="modal-body with-border">
-                <div class="row">
-                    {!! Form::hidden('subject-quantity', count($subjects), [
-                        'id' => 'subject-quantity',
-                        'class' => 'number',
-                        'title' => '科目数量',
-                    ]) !!}
-                    <div style="display: block; overflow-x: auto; clear: both; width: 100%; margin-top: 10px;">
-                        <table id="scores" style="white-space: nowrap; width: 100%;"
-                               class="table-striped table-bordered table-hover table-condensed">
-                            <thead>
-                            <tr class="bg-info">
-                                <th>序号</th>
-                                <th>考试名称</th>
-                                <th>考试时间</th>
-                                @foreach ($subjects as $id => $value)
-                                    <th class="subject-name">{{ $value }}</th>
+                <div class="form-horizontal">
+                    <div class="form-group">
+                        {!! Form::hidden('subject-quantity', count($subjects), [
+                            'id' => 'subject-quantity',
+                            'class' => 'number',
+                            'title' => '科目数量',
+                        ]) !!}
+                        <div style="display: block; overflow-x: auto; clear: both; width: 100%; margin-top: 10px;">
+                            <table id="scores" style="white-space: nowrap; width: 100%;"
+                                   class="table-striped table-bordered table-hover table-condensed">
+                                <thead>
+                                <tr class="bg-info">
+                                    <th>序号</th>
+                                    <th>考试名称</th>
+                                    <th>考试时间</th>
+                                    @foreach ($subjects as $id => $value)
+                                        <th class="subject-name">{{ $value }}</th>
+                                        <th>班排</th>
+                                        <th>年排</th>
+                                    @endforeach
+                                    <th class="subjectName">总分</th>
                                     <th>班排</th>
                                     <th>年排</th>
-                                @endforeach
-                                <th class="subjectName">总分</th>
-                                <th>班排</th>
-                                <th>年排</th>
-                            </tr>
-                            </thead>
-                            <tbody>
-                            @foreach ($examScores as $examScore)
-                                <tr>
-                                    <td>{{ $examScore['examId'] }}</td>
-                                    <td class="exam-name">{{ $examScore['examName'] }}</td>
-                                    <td>{{ $examScore['examTime'] }}</td>
-                                    @foreach ($examScore['scores'] as $score)
-                                        <td>{{ $score['score'] }}</td>
-                                        <td class="class-rank">{{ $score['class_rank'] }}</td>
-                                        <td class="grade-rank">{{ $score['grade_rank'] }}</td>
-                                    @endforeach
-                                    <td>{{ $examScore['examTotal']['score'] }}</td>
-                                    <td class="class-rank">{{ $examScore['examTotal']['class_rank'] }}</td>
-                                    <td class="grade-rank">{{ $examScore['examTotal']['grade_rank'] }}</td>
                                 </tr>
-                            @endforeach
-                            </tbody>
-                        </table>
+                                </thead>
+                                <tbody>
+                                @foreach ($examScores as $examScore)
+                                    <tr>
+                                        <td>{{ $examScore['examId'] }}</td>
+                                        <td class="exam-name">{{ $examScore['examName'] }}</td>
+                                        <td>{{ $examScore['examTime'] }}</td>
+                                        @foreach ($examScore['scores'] as $score)
+                                            <td>{{ $score['score'] }}</td>
+                                            <td class="class-rank">{{ $score['class_rank'] }}</td>
+                                            <td class="grade-rank">{{ $score['grade_rank'] }}</td>
+                                        @endforeach
+                                        <td>{{ $examScore['examTotal']['score'] }}</td>
+                                        <td class="class-rank">{{ $examScore['examTotal']['class_rank'] }}</td>
+                                        <td class="grade-rank">{{ $examScore['examTotal']['grade_rank'] }}</td>
+                                    </tr>
+                                @endforeach
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <div class="subject-title">
+                            {{ $student ? $student->user->realname : ''}}各科班级排名变化
+                        </div>
+                        <div id="class-rank"></div>
+                    </div>
+                    <div class="form-group">
+                        <div class="subject-title">
+                            {{ $student ? $student->user->realname : ''}}各科年级排名变化
+                        </div>
+                        <div id="grade-rank"></div>
                     </div>
                 </div>
-                <div class="row">
-                    <div class="subject-title">
-                        {{ $student ? $student->user->realname : ''}}各科班级排名变化
-                    </div>
-                    <div id="class-rank"></div>
-                </div>
-                <div class="row">
-                    <div class="subject-title">
-                        {{ $student ? $student->user->realname : ''}}各科年级排名变化
-                    </div>
-                    <div id="grade-rank"></div>
-                </div>
+                {{--<div class="row">--}}
+                    {{----}}
+                    {{----}}
+                {{--</div>--}}
+                {{--<div class="row">--}}
+                    {{----}}
+                {{--</div>--}}
+                {{--<div class="row">--}}
+                    {{----}}
+                {{--</div>--}}
             </div>
             <div class="modal-footer">
                 <a href="#" class="btn btn-sm btn-white" data-dismiss="modal">关闭</a>
