b0450b4e0418370fccb4b5d6dde3ec544da07929
Hao Yuhang <haoyh@yahoo.com>
2018-05-22 11:21:38 +0800

    test

--- resources/views/score/class_stat.blade.php
+++ resources/views/score/class_stat.blade.php
@@ -103,7 +103,7 @@
                         </div>
                     </div>
                 @endif
-                <div class="form-group col-sm-12 table-pie" style="width: 550px; height: 550px;">
+                <div class="form-group col-sm-12 table-pie" style="width: 550px; height: 550px; margin: 0 auto">
                 </div>
             </div>
             </div>
