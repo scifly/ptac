ac4b4901403da93b9dd68856a1818ec14af22b0e
Hao Yuhang <haoyh@yahoo.com>
2018-05-22 09:52:02 +0800

    test

--- resources/views/score/student_stat.blade.php
+++ resources/views/score/student_stat.blade.php
@@ -7,7 +7,7 @@
             </div>
             <div class="modal-body with-border">
                 <div class="form-horizontal">
-                    <div class="form-group col-sm-11">
+                    <div class="form-group col-sm-12">
                         {!! Form::hidden('subject-quantity', count($subjects), [
                             'id' => 'subject-quantity',
                             'class' => 'number',
