85fe9d168653a50d2118f18ffd418e7a6957ad0d
Hao Yuhang <haoyh@yahoo.com>
2018-05-24 14:55:29 +0800

    test

--- app/Models/Group.php
+++ app/Models/Group.php
@@ -249,15 +249,7 @@ class Group extends Model {
             $condition .= 'AND School.id = ' . $this->schoolId();
         } else if ($corpMenuId = $menu->menuId($currentMenuId, '企业')) {
             $corpId = Corp::whereMenuId($corpMenuId)->first()->id;
-            $condition .= ' AND Corp.id = ' . $corpId;
-            $joins[] = [
-                'table' => 'corps',
-                'alias' => 'Corp',
-                'type' => 'INNER',
-                'conditions' => [
-                    'Corp.id = School.corp_id'
-                ]
-            ];
+            $condition .= 'AND Corp.id = ' . $corpId;
         }
 
         return Datatable::simple(
