d50cfafb8b2311407ec1ec0cc333fd45f05d0ac4
Hao Yuhang <haoyh@yahoo.com>
2018-05-24 17:23:34 +0800

    test

--- resources/views/partials/enabled.blade.php
+++ resources/views/partials/enabled.blade.php
@@ -4,12 +4,13 @@
         {{ $label ?? '状态' }}
     </label>
     <div class="col-sm-6" style="padding-top: 5px;">
-        <input id="{{ $id }}1" @if($value) checked @endif
+        <input id="{{ $id }}1"
+               @if(!isset($value) || (isset($value) and $value)) checked @endif
                type="radio" name="{{ $id }}" class="minimal" value="1">
         <label for="{{ $id }}1" style="{!! $lblStyle !!}">
             {{ $options[0] ?? '启用' }}
         </label>
-        <input id="{{ $id }}2" @if(!$value) checked @endif
+        <input id="{{ $id }}2" @if (isset($value) && !$value) checked @endif
                type="radio" name="{{ $id }}" class="minimal" value="0">
         <label for="{{ $id }}2" style="{!! $lblStyle !!}">
             {{ $options[1] ?? '禁用' }}
