c91ba6dad8fff01e6c854d492709b68593de8705
Hao Yuhang <haoyh@yahoo.com>
2018-05-24 12:33:50 +0800

    test

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -2,11 +2,19 @@
 namespace App\Http\Controllers;
 
 use App\Facades\Wechat;
+use App\Helpers\Constant;
 use App\Helpers\ModelTrait;
 use App\Models\Corp;
 use App\Models\Department;
 use App\Models\Group;
+use App\Models\Icon;
+use App\Models\Menu;
+use App\Models\MenuTab;
+use App\Models\MenuType;
 use App\Models\Message;
+use App\Models\School;
+use App\Models\Tab;
+use App\Models\WapSite;
 use Exception;
 use GuzzleHttp\Client;
 use GuzzleHttp\Exception\ClientException;
@@ -22,6 +30,205 @@ use ReflectionMethod;
 class TestController extends Controller {
     
     use ModelTrait;
+    
+    protected $menus = [
+        '首页' => [
+            'id' => 0,
+            'parent_id' => null,
+            'uri' => 'home',
+            'icon' => 'fa fa-home',
+            'tabs' => []
+        ],
+        '消息中心' => [
+            'id' => 0,
+            'parent_id' => null,
+            'uri' => 'messages/index',
+            'icon' => 'fa fa-send-o',
+            'tabs' => null
+        ],
+        '通讯录' => [
+            'id' => 0,
+            'parent_id' => null,
+            'uri' => null,
+            'icon' => 'fa fa-book',
+            'tabs' => null
+        ],
+        '学生' => [
+            'id' => 0,
+            'parent_id' => '通讯录',
+            'uri' => null,
+            'icon' => 'fa fa-child',
+            'tabs' => ['学生', '班级']
+        ],
+        '教职员工' => [
+            'id' => 0,
+            'parent_id' => '通讯录',
+            'uri' => null,
+            'icon' => 'fa fa-child',
+            'tabs' => ['教职员工', '教职员工组']
+        ],
+        '监护人' => [
+            'id' => 0,
+            'parent_id' => '通讯录',
+            'uri' => null,
+            'icon' => 'fa fa-child',
+            'tabs' => ['监护人', '学生']
+        ],
+        '考勤管理' => [
+            'id' => 0,
+            'parent_id' => null,
+            'uri' => null,
+            'icon' => 'fa fa-credit-card',
+            'tabs' => null
+        ],
+        '考勤设置' => [
+            'id' => 0,
+            'parent_id' => '考勤管理',
+            'uri' => null,
+            'icon' => 'fa fa-credit-card',
+            'tabs' => ['教职员工考勤设置', '学生考勤设置', '考勤机']
+        ],
+        '学生考勤查询统计' => [
+            'id' => 0,
+            'parent_id' => '考勤管理',
+            'uri' => null,
+            'icon' => 'fa fa-commenting',
+            'tabs' => ['学生考勤记录']
+        ],
+        '教职员工考勤管理' => [
+            'id' => 0,
+            'parent_id' => '考勤管理',
+            'uri' => null,
+            'icon' => 'fa fa-commenting',
+            'tabs' => ['教职员工考勤']
+        ],
+        '成绩中心' => [
+            'id' => 0,
+            'parent_id' => null,
+            'uri' => null,
+            'icon' => 'fa fa-file',
+            'tabs' => null
+        ],
+        '考试设置' => [
+            'id' => 0,
+            'parent_id' => '成绩中心',
+            'uri' => null,
+            'icon' => null,
+            'tabs' => ['考试', '考试类型']
+        ],
+        '成绩管理' => [
+            'id' => 0,
+            'parent_id' => '成绩中心',
+            'uri' => null,
+            'icon' => null,
+            'tabs' => ['成绩', '成绩统计项', '总成绩']
+        ],
+        '自媒体管理' => [
+            'id' => 0,
+            'parent_id' => null,
+            'uri' => null,
+            'icon' => 'fa fa-photo',
+            'tabs' => ['微网站栏目', '微网站文章', '微网站']
+        ],
+        '用户中心' => [
+            'id' => 0,
+            'parent_id' => null,
+            'uri' => null,
+            'icon' => 'fa fa-user',
+            'tabs' => ['微网站栏目', '微网站文章', '微网站']
+        ],
+        '个人信息' => [
+            'id' => 0,
+            'parent_id' => '用户中心',
+            'uri' => 'users/profile',
+            'icon' => 'fa fa-user',
+            'tabs' => null
+        ],
+        '重置密码' => [
+            'id' => 0,
+            'parent_id' => '用户中心',
+            'uri' => 'users/reset',
+            'icon' => 'fa fa-user',
+            'tabs' => null
+        ],
+        '我的消息' => [
+            'id' => 0,
+            'parent_id' => '用户中心',
+            'uri' => 'users/reset',
+            'icon' => 'fa fa-user',
+            'tabs' => null
+        ],
+        '系统设置' => [
+            'id' => 0,
+            'parent_id' => null,
+            'uri' => null,
+            'icon' => 'fa fa-gear',
+            'tabs' => null
+        ],
+        '菜单管理' => [
+            'id' => 0,
+            'parent_id' => '系统设置',
+            'uri' => null,
+            'icon' => 'fa fa-list-ul',
+            'tabs' => ['菜单']
+        ],
+        '部门管理' => [
+            'id' => 0,
+            'parent_id' => '系统设置',
+            'uri' => null,
+            'icon' => 'fa fa-sitemap',
+            'tabs' => ['部门']
+        ],
+        '学校管理员' => [
+            'id' => 0,
+            'parent_id' => '系统设置',
+            'uri' => null,
+            'icon' => 'fa fa-user-plus',
+            'tabs' => ['超级用户']
+        ],
+        '角色/权限' => [
+            'id' => 0,
+            'parent_id' => '系统设置',
+            'uri' => null,
+            'icon' => 'fa fa-meh-o',
+            'tabs' => ['角色']
+        ],
+        '学校设置' => [
+            'id' => 0,
+            'parent_id' => '系统设置',
+            'uri' => 'schools/edit/',
+            'icon' => 'fa fa-university',
+            'tabs' => null
+        ],
+        '年级/班级' => [
+            'id' => 0,
+            'parent_id' => '系统设置',
+            'uri' => null,
+            'icon' => 'fa fa-users',
+            'tabs' => ['年级', '班级']
+        ],
+        '科目设置' => [
+            'id' => 0,
+            'parent_id' => '系统设置',
+            'uri' => null,
+            'icon' => 'fa fa-book',
+            'tabs' => ['科目', '科目此分类']
+        ],
+        '学期设置' => [
+            'id' => 0,
+            'parent_id' => '系统设置',
+            'uri' => null,
+            'icon' => null,
+            'tabs' => ['学期']
+        ],
+        '套餐设置' => [
+            'id' => 0,
+            'parent_id' => '系统设置',
+            'uri' => null,
+            'icon' => null,
+            'tabs' => ['套餐类型']
+        ],
+    ];
     
     const ALLOWED_CORP_ACTIONS = [
         '/corps/edit/%s',
@@ -52,6 +259,54 @@ class TestController extends Controller {
      */
     public function index() {
     
+        $school = School::find(11);
+        $position = Menu::all()->max('position');
+    
+        try {
+            # 创建学校微网站
+            WapSite::create([
+                'school_id' => $school->id,
+                'site_title' => $school->name,
+                'media_ids' => '0',
+                'enabled' => 'enabled'
+            ]);
+            # 创建学校基础菜单
+            $menuTypeId = MenuType::whereName('其他')->first()->id;
+            foreach ($this->menus as &$data) {
+                if ($data['uri'] == 'schools/edit') {
+                    $data['uri'] .= $school->id;
+                }
+                # 创建
+                $menu = Menu::create([
+                    'parent_id' => !$data['parent_id']
+                        ? $school->menu_id
+                        : $this->menus[$data['parent_id']]['id'],
+                    'menu_type_id' => $menuTypeId,
+                    'name' => $data['name'],
+                    'uri' => $data['uri'],
+                    'position' => $position += 1,
+                    'icon_id' => Icon::whereName($data['icon'])->first()->id,
+                    'enabled' => Constant::ENABLED
+                ]);
+                $data['id'] = $menu->id;
+                # 创建菜单卡片绑定关系
+                if ($data['tabs']) {
+                    foreach ($data['tabs'] as $name) {
+                        $tab = Tab::whereName($name)->first();
+                        MenuTab::create([
+                            'menu_id' => $menu->id,
+                            'tab_id' => $tab->id,
+                            'enabled' => Constant::ENABLED
+                        ]);
+                    }
+                }
+            }
+        } catch (Exception $e) {
+            throw $e;
+        }
+        
+        return false;
+        
         dd(Department::find(5)->toArray());
         $user = Auth::user();
         $corp = Corp::find(4);
--- app/Jobs/ManageCreateMenu.php
+++ app/Jobs/ManageCreateMenu.php
@@ -1,15 +1,21 @@
 <?php
 namespace App\Jobs;
 
+use App\Helpers\Constant;
+use App\Models\Icon;
+use App\Models\MenuType;
+use App\Models\School;
+use App\Models\Tab;
+use App\Models\WapSite;
+use Exception;
 use App\Models\Menu;
 use App\Models\MenuTab;
-use Exception;
 use Illuminate\Bus\Queueable;
-use Illuminate\Contracts\Queue\ShouldQueue;
-use Illuminate\Foundation\Bus\Dispatchable;
-use Illuminate\Queue\InteractsWithQueue;
-use Illuminate\Queue\SerializesModels;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Contracts\Queue\ShouldQueue;
 
 class ManageCreateMenu implements ShouldQueue {
     
@@ -17,112 +23,270 @@ class ManageCreateMenu implements ShouldQueue {
     
     protected $parentMenu, $menuNameTab, $menu, $menuTab;
     
+    protected $menus = [
+        '首页' => [
+            'id' => 0,
+            'parent_id' => null,
+            'uri' => 'home',
+            'icon' => 'fa fa-home',
+            'tabs' => []
+        ],
+        '消息中心' => [
+            'id' => 0,
+            'parent_id' => null,
+            'uri' => 'messages/index',
+            'icon' => 'fa fa-send-o',
+            'tabs' => null
+        ],
+        '通讯录' => [
+            'id' => 0,
+            'parent_id' => null,
+            'uri' => null,
+            'icon' => 'fa fa-book',
+            'tabs' => null
+        ],
+        '学生' => [
+            'id' => 0,
+            'parent_id' => '通讯录',
+            'uri' => null,
+            'icon' => 'fa fa-child',
+            'tabs' => ['学生', '班级']
+        ],
+        '教职员工' => [
+            'id' => 0,
+            'parent_id' => '通讯录',
+            'uri' => null,
+            'icon' => 'fa fa-child',
+            'tabs' => ['教职员工', '教职员工组']
+        ],
+        '监护人' => [
+            'id' => 0,
+            'parent_id' => '通讯录',
+            'uri' => null,
+            'icon' => 'fa fa-child',
+            'tabs' => ['监护人', '学生']
+        ],
+        '考勤管理' => [
+            'id' => 0,
+            'parent_id' => null,
+            'uri' => null,
+            'icon' => 'fa fa-credit-card',
+            'tabs' => null
+        ],
+        '考勤设置' => [
+            'id' => 0,
+            'parent_id' => '考勤管理',
+            'uri' => null,
+            'icon' => 'fa fa-credit-card',
+            'tabs' => ['教职员工考勤设置', '学生考勤设置', '考勤机']
+        ],
+        '学生考勤查询统计' => [
+            'id' => 0,
+            'parent_id' => '考勤管理',
+            'uri' => null,
+            'icon' => 'fa fa-commenting',
+            'tabs' => ['学生考勤记录']
+        ],
+        '教职员工考勤管理' => [
+            'id' => 0,
+            'parent_id' => '考勤管理',
+            'uri' => null,
+            'icon' => 'fa fa-commenting',
+            'tabs' => ['教职员工考勤']
+        ],
+        '成绩中心' => [
+            'id' => 0,
+            'parent_id' => null,
+            'uri' => null,
+            'icon' => 'fa fa-file',
+            'tabs' => null
+        ],
+        '考试设置' => [
+            'id' => 0,
+            'parent_id' => '成绩中心',
+            'uri' => null,
+            'icon' => null,
+            'tabs' => ['考试', '考试类型']
+        ],
+        '成绩管理' => [
+            'id' => 0,
+            'parent_id' => '成绩中心',
+            'uri' => null,
+            'icon' => null,
+            'tabs' => ['成绩', '成绩统计项', '总成绩']
+        ],
+        '自媒体管理' => [
+            'id' => 0,
+            'parent_id' => null,
+            'uri' => null,
+            'icon' => 'fa fa-photo',
+            'tabs' => ['微网站栏目', '微网站文章', '微网站']
+        ],
+        '用户中心' => [
+            'id' => 0,
+            'parent_id' => null,
+            'uri' => null,
+            'icon' => 'fa fa-user',
+            'tabs' => ['微网站栏目', '微网站文章', '微网站']
+        ],
+        '个人信息' => [
+            'id' => 0,
+            'parent_id' => '用户中心',
+            'uri' => 'users/profile',
+            'icon' => 'fa fa-user',
+            'tabs' => null
+        ],
+        '重置密码' => [
+            'id' => 0,
+            'parent_id' => '用户中心',
+            'uri' => 'users/reset',
+            'icon' => 'fa fa-user',
+            'tabs' => null
+        ],
+        '我的消息' => [
+            'id' => 0,
+            'parent_id' => '用户中心',
+            'uri' => 'users/reset',
+            'icon' => 'fa fa-user',
+            'tabs' => null
+        ],
+        '系统设置' => [
+            'id' => 0,
+            'parent_id' => null,
+            'uri' => null,
+            'icon' => 'fa fa-gear',
+            'tabs' => null
+        ],
+        '菜单管理' => [
+            'id' => 0,
+            'parent_id' => '系统设置',
+            'uri' => null,
+            'icon' => 'fa fa-list-ul',
+            'tabs' => ['菜单']
+        ],
+        '部门管理' => [
+            'id' => 0,
+            'parent_id' => '系统设置',
+            'uri' => null,
+            'icon' => 'fa fa-sitemap',
+            'tabs' => ['部门']
+        ],
+        '学校管理员' => [
+            'id' => 0,
+            'parent_id' => '系统设置',
+            'uri' => null,
+            'icon' => 'fa fa-user-plus',
+            'tabs' => ['超级用户']
+        ],
+        '角色/权限' => [
+            'id' => 0,
+            'parent_id' => '系统设置',
+            'uri' => null,
+            'icon' => 'fa fa-meh-o',
+            'tabs' => ['角色']
+        ],
+        '学校设置' => [
+            'id' => 0,
+            'parent_id' => '系统设置',
+            'uri' => 'schools/edit/',
+            'icon' => 'fa fa-university',
+            'tabs' => null
+        ],
+        '年级/班级' => [
+            'id' => 0,
+            'parent_id' => '系统设置',
+            'uri' => null,
+            'icon' => 'fa fa-users',
+            'tabs' => ['年级', '班级']
+        ],
+        '科目设置' => [
+            'id' => 0,
+            'parent_id' => '系统设置',
+            'uri' => null,
+            'icon' => 'fa fa-book',
+            'tabs' => ['科目', '科目此分类']
+        ],
+        '学期设置' => [
+            'id' => 0,
+            'parent_id' => '系统设置',
+            'uri' => null,
+            'icon' => null,
+            'tabs' => ['学期']
+        ],
+        '套餐设置' => [
+            'id' => 0,
+            'parent_id' => '系统设置',
+            'uri' => null,
+            'icon' => null,
+            'tabs' => ['套餐类型']
+        ],
+    ];
+    
+    protected $school;
+    
     /**
      * Create ent job instncent   *
-     * @param $parentMenu
+     * @param School $school
      */
-    public function __construct($parentMenu) {
-        $this->parentMenu = $parentMenu;
+    public function __construct(School $school) {
+        
+        $this->school = $school;
+        
     }
     
     /**
      * Exeute the job.
      *
-     * @return bool
-     * @throws \Exception
-     *u@throws \Throwable
+     * @return void
+     * @throws Exception
      */
     public function handle() {
-        // $this->menuNameTab = [
-        //     '首页', '自媒体管理', '课程表管理', '用户中心',
-        //     '通讯录', '成绩管理', '考勤管理', '移动办公', '系统设置'
-        // ];
-        $this->menu = new Menu();
-        $this->menuNameTab = [];
-        $schoolMenus = $this->menu
-            ->where('parent_id',5)
-            ->whereEnabled(1)
-            ->get();
-        foreach ($schoolMenus as $schoolMenu){
-            $this->menuNameTab[] = $schoolMenu->name;
-        }
-        return $this->storeSchoolMenu($this->parentMenu, $this->menuNameTab);
-    }
-    
-    /**
-     * @param $parentMenu
-     * @param $menuNameTabs
-     * @return bool
-     * @throws Exception
-     * @throws \Throwable
-     */
-    private function storeSchoolMenu($parentMenu, $menuNameTabs) {
-        # 创建新的Menu记录及卡片绑定记录
-        try {
-            DB::transaction(
-                function () use ($parentMenu, $menuNameTabs) {
-                    $this->menuTab = new MenuTab();
-                    $this->menu = new Menu();
-                    #查询 同名菜单名下 第一个菜单对应卡片的ids
-                    foreach ($menuNameTabs as $menuNameTab) {
-                        $tabIds = [];
-                        $oldMenu = $this->menu->where('name', $menuNameTab)->first();
-                        $uri = $oldMenu->uri;
-                        $tabs = $oldMenu->tabs;
-                        foreach ($tabs as $tab) {
-                            $tabIds[] = $tab->id;
-                        }
-                        # 菜单下存在卡片
-                        if (!empty($tabIds)) {
-                            $menuData = [
-                                'parent_id'    => $parentMenu->id,
-                                'name'         => $menuNameTab,
-                                'menu_type_id' => 5,
-                                'position'     => $this->menu->all()->count(),
-                                'enabled'      => 1,
-                            ];
-                            $m = $this->menu->create($menuData);
-                            $this->menuTab->storeByMenuId($m->id, $tabIds);
-                        }
-                        #菜单下存在uri
-                        else if (!empty($uri)) {
-                            $menuData = [
-                                'parent_id'    => $parentMenu->id,
-                                'name'         => $menuNameTab,
-                                'menu_type_id' => 5,
-                                'position'     => $this->menu->all()->count(),
-                                'uri'          => $uri,
-                                'enabled'      => 1,
-                            ];
-                            $this->menu->create($menuData);
-                        }
-                        #存在下一级菜单
-                        else {
-                            $menuData = [
-                                'parent_id'    => $parentMenu->id,
-                                'name'         => $menuNameTab,
-                                'menu_type_id' => 5,
-                                'position'     => $this->menu->all()->count(),
-                                'enabled'      => 1,
-                            ];
-                            $m = $this->menu->create($menuData);
-                            $children = $this->menu->where('parent_id', $oldMenu->id)->get();
-                            $childName = [];
-                            foreach ($children as $child) {
-                                $childName[] = $child->name;
-                            }
-                            if (!empty($childName)) {
-                                #若子菜单和父菜单同名 会陷入死循环
-                                $this->storeSchoolMenu($m, $childName);
-                            }
-                        }
-                    }
-                });
         
+        $position = Menu::all()->max('position');
+        
+        try {
+            # 创建学校微网站
+            WapSite::create([
+                'school_id' => $this->school->id,
+                'site_title' => $this->school->name,
+                'media_ids' => '0',
+                'enabled' => 'enabled'
+            ]);
+            # 创建学校基础菜单
+            $menuTypeId = MenuType::whereName('其他')->first()->id;
+            foreach ($this->menus as &$data) {
+                if ($data['uri'] == 'schools/edit') {
+                    $data['uri'] .= $this->school->id;
+                }
+                # 创建
+                $menu = Menu::create([
+                    'parent_id' => !$data['parent_id']
+                        ? $this->school->menu_id
+                        : $this->menus[$data['parent_id']]['id'],
+                    'menu_type_id' => $menuTypeId,
+                    'name' => $data['name'],
+                    'uri' => $data['uri'],
+                    'position' => $position += 1,
+                    'icon_id' => Icon::whereName($data['icon'])->first()->id,
+                    'enabled' => Constant::ENABLED
+                ]);
+                $data['id'] = $menu->id;
+                # 创建菜单卡片绑定关系
+                if ($data['tabs']) {
+                    foreach ($data['tabs'] as $name) {
+                        $tab = Tab::whereName($name)->first();
+                        MenuTab::create([
+                            'menu_id' => $menu->id,
+                            'tab_id' => $tab->id,
+                            'enabled' => Constant::ENABLED
+                        ]);
+                    }
+                }
+            }
         } catch (Exception $e) {
             throw $e;
         }
-    
-        return true;
+        
     }
+    
 }
