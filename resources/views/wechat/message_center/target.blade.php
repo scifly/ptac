e1588f2fd40755a2b1d0863e54654e84ae76e6ba
Hao Yuhang <haoyh@yahoo.com>
2018-05-26 19:48:41 +0800

    test

--- resources/views/wechat/message_center/target.blade.php
+++ resources/views/wechat/message_center/target.blade.php
@@ -10,9 +10,9 @@
             <i class="weui-icon-checked"></i>
         </div>
         <div class="weui-cell__bd">
-            {{--<img src="{{ $type == 'department' ? asset('img/department.png') : asset('img/personal.png') }}"--}}
-                 {{--@if ($type == 'department') style="border-radius: 0;" @endif--}}
-                 {{--class="js-go-detail lazy" width="75" height="75">--}}
+            <img src="{{ $type == 'department' ? asset('img/department.png') : asset('img/personal.png') }}"
+                 @if ($type == 'department') style="border-radius: 0;" @endif
+                 class="js-go-detail lazy" width="75" height="75">
             <span class="contacts-text">
                 {{ $type == 'department' ? $target->name : $target->realname . ' - '. $target->mobiles->first()->mobile }}
             </span>
