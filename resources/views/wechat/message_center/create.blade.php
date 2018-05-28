597d0efb111b54932cd8c5948bab6cdf791cef94
Hao Yuhang <haoyh@yahoo.com>
2018-05-28 10:19:41 +0800

    test

--- resources/views/wechat/message_center/create.blade.php
+++ resources/views/wechat/message_center/create.blade.php
@@ -107,12 +107,14 @@
             </div>
         </div>
         <!-- 图文消息 -->
-        <div id="mpnews-container" class="weui-cell msg-send-conicon msg-send-bg b-top" style="display: none;">
-            <div class="weui-uploader__hd">
-                <p class="weui-uploader__title"> 添加图文</p>
-            </div>
-            <div class="weui-cell__bd">
-                <a href="#" class="open-popup weui-uploader__input-box" data-target="#mpnews"></a>
+        <div id="mpnews-container" class="msg-send-conicon msg-send-bg b-top" style="display: none;">
+            <div class="weui-cell">
+                <div class="weui-uploader__hd">
+                    <p class="weui-uploader__title">添加图文</p>
+                </div>
+                <div class="weui-cell__bd">
+                    <a href="#" class="open-popup weui-uploader__input-box" data-target="#mpnews"></a>
+                </div>
             </div>
         </div>
         <!-- 定时发送 -->
