aca8a063ed7a19939fc38b83d538d6428a04aa20
Hao Yuhang <haoyh@yahoo.com>
2018-06-01 14:57:34 +0800

    test

--- app/Models/Student.php
+++ app/Models/Student.php
@@ -2,6 +2,7 @@
 namespace App\Models;
 
 use Eloquent;
+use Illuminate\Support\Facades\Log;
 use Throwable;
 use Exception;
 use Carbon\Carbon;
@@ -284,6 +285,7 @@ class Student extends Model {
                 $userId = $student->user_id;
                 #删除关联监护人
                 $custodians = $student->custodians;
+                Log::debug(json_encode($custodians));
                 foreach ($custodians as $custodian) {
                     #判断当前监护人下是否只有当前学生，是则删除监护人
                     $cusStuents = $custodian->students;
