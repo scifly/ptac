3e390b3d439fa358828b7b793e7165303f2fdcc2
Hao Yuhang <haoyh@yahoo.com>
2018-06-01 15:18:54 +0800

    test

--- public/js/contact.js
+++ public/js/contact.js
@@ -179,7 +179,7 @@
                         success: function (result) {
                             $next.remove();
                             $studentId.remove();
-                            $prev.after(result['html']['original']['html']);
+                            $prev.after(result['html']['students']['original']['html']);
                             page.initSelect2();
                         },
                         error: function (e) {
