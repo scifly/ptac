f7a8cc17fbcfcba91d5785a44e5af03b279f3cc6
Hao Yuhang <haoyh@yahoo.com>
2018-06-05 09:15:54 +0800

    test

--- public/js/wsm_article/create.js
+++ public/js/wsm_article/create.js
@@ -1,69 +1,71 @@
 //# sourceURL=create.js
-var token = $('#csrf_token').attr('content');
+var $preview = $('.preview'),
+    $uploadFiles = $('#uploadFiles'),
+    token = $('#csrf_token').attr('content'),
+    scripts = [
+        plugins.fileinput.js,
+        plugins.fileinput.language,
+        plugins.ueditor_config.js,
+        plugins.ueditor_all.js
+    ];
+
 page.loadCss('css/wsm_article/wsma.css');
 page.create('formWsmArticle', 'wsm_articles');
 page.loadCss(plugins.fileinput.css);
-$.getMultiScripts([plugins.fileinput.js, plugins.fileinput.language]);
-$.getMultiScripts([plugins.ueditor_config.js, plugins.ueditor_all.js]).done(
-    function () {
-        // var editor = UE.getEditor('container').render('container');//初始化富文本编辑器
-        // $(function () {
-        UE.delEditor('container');
-        UE.getEditor('container', {
-            initialFrameHeight: 300
-        });
-        var $preview = $('.preview'),
-            $uploadFiles = $('#uploadFiles');
-        // 初始化
-        $uploadFiles.fileinput({
-            language: 'zh',
-            theme: 'explorer',
-            uploadUrl: page.siteRoot() + "/wsm_articles/create",
-            uploadAsync: false,
-            maxFileCount: 5,
-            minImageWidth: 50,      // 最小宽度
-            minImageHeight: 50,     // 最小高度
-            maxImageWidth: 1000,    // 最大宽度
-            maxImageHeight: 1000,   // 最大高度
-            allowedFileExtensions: ['jpg', 'gif', 'png'],//接收的文件后缀
-            fileActionSettings: {
-                showRemove: true,
-                showUpload: false,
-                showDrag: false
-            },
-            uploadExtraData: {
-                '_token': token
-            }
-        });
-        // 上传成功
-        $uploadFiles.on("filebatchuploadsuccess", function (event, data/*, previewId, index*/) {
-            // 填充数据
-            var files = data.response;
-            $.each(files, function (index, file) {
-                $preview.append(
-                    '<div class="img-item">' +
-                        '<img src="../../' + file.path + '" id="' + file.id + '">' +
-                        '<div class="del-mask">' +
-                            '<i class="delete glyphicon glyphicon-trash text-red"></i>' +
-                        '</div>' +
-                    '</div>'
-                );
-                $preview.append('<input type="hidden" name="media_ids[]" value="' + file.id + '">');
-            });
-            // 成功后关闭弹窗
-            setTimeout(function () {
-                $('#modalPic').modal('hide');
-            }, 800)
-        });
+$.getMultiScripts(scripts).done(function () { init(); });
 
-        // modal关闭，内容清空
-        $('#modalPic').on('hide.bs.modal', function () {
-            $uploadFiles.fileinput('clear');
+// 初始化
+function init() {
+    UE.delEditor('container');
+    UE.getEditor('container', { initialFrameHeight: 300 });
+    $uploadFiles.fileinput({
+        language: 'zh',
+        theme: 'explorer',
+        uploadUrl: page.siteRoot() + "/wsm_articles/create",
+        uploadAsync: false,
+        maxFileCount: 5,
+        minImageWidth: 50,      // 最小宽度
+        minImageHeight: 50,     // 最小高度
+        maxImageWidth: 1000,    // 最大宽度
+        maxImageHeight: 1000,   // 最大高度
+        allowedFileExtensions: ['jpg', 'gif', 'png'],//接收的文件后缀
+        fileActionSettings: {
+            showRemove: true,
+            showUpload: false,
+            showDrag: false
+        },
+        uploadExtraData: {
+            '_token': token
+        }
+    });
+    // 上传成功
+    $uploadFiles.on("filebatchuploadsuccess", function (event, data/*, previewId, index*/) {
+        // 填充数据
+        var files = data.response;
+        $.each(files, function (index, file) {
+            $preview.append(
+                '<div class="img-item">' +
+                    '<img src="../../' + file.path + '" id="' + file.id + '">' +
+                    '<div class="del-mask">' +
+                        '<i class="delete glyphicon glyphicon-trash text-red"></i>' +
+                    '</div>' +
+                '</div>'
+            );
+            $preview.append('<input type="hidden" name="media_ids[]" value="' + file.id + '">');
         });
-        // 点击删除按钮
-        $('body').on('click', '.delete', function () {
-            $(this).parent().parent().remove();
-            $preview.append('<input type="hidden" name="del_ids[]" value="' + $(this).parent().siblings().attr('id') + '">');
-        });
-    }
-);
+        // 成功后关闭弹窗
+        setTimeout(function () {
+            $('#modalPic').modal('hide');
+        }, 800)
+    });
+
+    // modal关闭，内容清空
+    $('#modalPic').on('hide.bs.modal', function () {
+        $uploadFiles.fileinput('clear');
+    });
+    // 点击删除按钮
+    $('body').on('click', '.delete', function () {
+        $(this).parent().parent().remove();
+        $preview.append('<input type="hidden" name="del_ids[]" value="' + $(this).parent().siblings().attr('id') + '">');
+    });
+}
