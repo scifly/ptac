8ba7a616226410445563861bd186fb9dd2d80d08
Hao Yuhang <haoyh@yahoo.com>
2018-06-05 10:11:00 +0800

    test

--- public/js/student/create.js
+++ public/js/student/create.js
@@ -3,7 +3,5 @@ page.create('formStudent', 'students');
 
 /** 初始化学籍创建页面功能 */
 $.getMultiScripts(['js/contact.js']).done(
-    function () {
-        $.contact().create('students');
-    }
+    function () { $.contact().create('students'); }
 );
