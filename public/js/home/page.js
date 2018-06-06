55993a9921b5b9fe4d5914b834778472162efbfb
Hao Yuhang <haoyh@yahoo.com>
2018-06-06 13:01:07 +0800

    test

--- public/js/home/page.js
+++ public/js/home/page.js
@@ -8,22 +8,24 @@ var page = {
     success: 'img/confirm.png',
     failure: 'img/error.png',
     info: 'img/info.png',
-    dateRangeLocale: {
-        format: "YYYY-MM-D",
-        separator: " 至 ",
-        applyLabel: "确定",
-        cancelLabel: "取消",
-        fromLabel: "从",
-        toLabel: "到",
-        todayRangeLabel: '今天',
-        customRangeLabel: "自定义",
-        weekLabel: "W",
-        daysOfWeek: ["日", "一", "二", "三", "四", "五", "六"],
-        monthNames: [
-            "一月", "二月", "三月", "四月", "五月", "六月",
-            "七月", "八月", "九月", "十月", "十一月", "十二月"
-        ],
-        firstDay: 1
+    dateRangeLocale: function() {
+       return {
+           format: "YYYY-MM-D",
+           separator: " 至 ",
+           applyLabel: "确定",
+           cancelLabel: "取消",
+           fromLabel: "从",
+           toLabel: "到",
+           todayRangeLabel: '今天',
+           customRangeLabel: "自定义",
+           weekLabel: "W",
+           daysOfWeek: ["日", "一", "二", "三", "四", "五", "六"],
+           monthNames: [
+               "一月", "二月", "三月", "四月", "五月", "六月",
+               "七月", "八月", "九月", "十月", "十一月", "十二月"
+           ],
+           firstDay: 1
+       };
     },
     dateRangeRanges: {
         '今天': [
