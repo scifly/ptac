b3dcf3dbb0899ff7cd2f01aa123247772a4e1c05
Hao Yuhang <haoyh@yahoo.com>
2018-06-06 14:26:54 +0800

    test

--- public/js/student_attendance_setting/create.js
+++ public/js/student_attendance_setting/create.js
@@ -1,25 +1,9 @@
+//# sourceURL=create.js
 page.create('formStudentAttendanceSetting', 'student_attendance_settings');
 page.initParsleyRules();
 page.loadCss(plugins.timepicker.css);
-$.getMultiScripts([plugins.timepicker.js, 'js/plugins/slimScroll/jquery.slimscroll.min.js']).done(
+$.getMultiScripts([plugins.timepicker.js]).done(
     function () {
         $('.timepicker').timepicker({ showInputs: false });
     }
 );
-// function datetime($initTime) {
-//     $initTime.timepicker({
-//         timeFormat: 'hh:mm:ss'
-//     })
-// }
-//
-// if (!($.fn.timepicker)) {
-//     page.loadCss(plugins.timepicker.css);
-//     $.getMultiScripts([plugins.timepicker.js, plugins.timepicker.jscn])
-//         .done(function () {
-//             datetime($(".start-time"));
-//             datetime($(".end-time"));
-//         });
-// } else {
-//     datetime($(".start-time"));
-//     datetime($(".end-time"));
-// }
