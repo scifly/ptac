2822029688ae0c960cbe85414e99da29ed6e39c7
Hao Yuhang <haoyh@yahoo.com>
2018-06-06 13:37:20 +0800

    test

--- resources/views/student_attendance_setting/create_edit.blade.php
+++ resources/views/student_attendance_setting/create_edit.blade.php
@@ -42,13 +42,13 @@
                 ]) !!}
                 <div class="col-sm-6">
                     <div class="input-group">
-                        @include('partials.icon_addon', ['class' => 'fa-clock-o'])
                         {!! Form::text('start', null, [
                             'class' => 'form-control start-time timepicker',
                             'required' => 'true',
                             'data-parsley-start' => '.end-time',
                             'placeholder' => '(不得大于等于结束时间)'
                         ]) !!}
+                        @include('partials.icon_addon', ['class' => 'fa-clock-o'])
                     </div>
                 </div>
             </div>
@@ -58,13 +58,13 @@
                 ]) !!}
                 <div class="col-sm-6">
                     <div class="input-group">
-                        @include('partials.icon_addon', ['class' => 'fa-clock-o'])
                         {!! Form::text('end', null, [
                             'class' => 'form-control end-time timepicker',
                             'required' => 'true',
                             'data-parsley-end' => '.start-time',
                             'placeholder' => '(不得小于等于开始时间)'
                         ]) !!}
+                        @include('partials.icon_addon', ['class' => 'fa-clock-o'])
                     </div>
                 </div>
             </div>
