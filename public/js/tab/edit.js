47aff9a56b92688898a704e0963a68b6cdd44e67
Hao Yuhang <haoyh@yahoo.com>
2018-06-11 11:33:35 +0800

    test

--- public/js/tab/edit.js
+++ public/js/tab/edit.js
@@ -1,6 +1,18 @@
-var options = {
-    templateResult: page.formatState,
-    templateSelection: page.formatState
-};
+var $groupId = $('#group_id'),
+    $actionId = $('#action_id'),
+    $menuIds = $('#menu_ids'),
+    options = [{
+    option: {
+        templateResult: page.formatState,
+        templateSelection: page.formatState,
+    },
+    id: 'icon_id'
+}];
 page.edit('formTab', 'tabs', options);
-page.initSelect2();
+$.getMultiScripts([plugins.select2.js]).done(function () {
+    $.getMultiScripts([plugins.select2.jscn]).done(function () {
+        $groupId.select2();
+        $actionId.select2();
+        $menuIds.select2();
+    });
+});
