28694fa33787ae35012ec55a65e1cfe307210a4a
Hao Yuhang <haoyh@yahoo.com>
2018-06-11 15:20:02 +0800

    test

--- public/js/score/stat.js
+++ public/js/score/stat.js
@@ -1,5 +1,5 @@
 var statType = 0; // 统计类型, 0 - 学生, 1 - 班级
-$('#students').hide();
+$('#student').hide();
 $('input[name="type"]').on('ifClicked', function () {
     statType = parseInt(this.value);
     if (statType === 1) {
