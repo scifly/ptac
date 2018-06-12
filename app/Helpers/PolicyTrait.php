12a08cf25277e2a833d662fce0718817375e2fa3
Hao Yuhang <haoyh@yahoo.com>
2018-06-12 14:32:23 +0800

    test

--- app/Helpers/PolicyTrait.php
+++ app/Helpers/PolicyTrait.php
@@ -4,15 +4,17 @@ namespace App\Helpers;
 use App\Models\Action;
 use App\Models\ActionGroup;
 use App\Models\User;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Request;
 
 trait PolicyTrait {
 
     function action(User $user) {
-        
+    
+        Log::debug(trim(Request::route()->uri()));
         $actionId = Action::whereRoute(trim(Request::route()->uri()))->first()->id;
         $ag = ActionGroup::whereGroupId($user->group_id)->where('action_id', $actionId)->first();
-    
+        
         return $ag ? true : false;
         
     }
