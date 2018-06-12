16c5c8b984e8bfa5b369bcbd1ff78c65b1a9fe2d
Hao Yuhang <haoyh@yahoo.com>
2018-06-12 11:42:01 +0800

    test

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -144,6 +144,7 @@ class HomeController extends Controller {
         }
         # 获取并返回wrapper-content层中的html内容
         if (Request::ajax()) {
+            Log::debug('tabs: ' . json_encode($tabArray));
             $this->result['html'] = view('partials.site_content', ['tabs' => $tabArray])->render();
             $this->result['department'] = $this->menu->department($id);
             $this->result['title'] = $this->menu->find(session('menuId'))->name;
