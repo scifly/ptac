12de70e8133e34e36593da72a9ffc559aa98718c
Hao Yuhang <haoyh@yahoo.com>
2018-06-21 17:27:39 +0800

    test

--- app/Http/ViewComposers/SchoolIndexComposer.php
+++ app/Http/ViewComposers/SchoolIndexComposer.php
@@ -12,6 +12,7 @@ class SchoolIndexComposer {
         
         $view->with([
             'titles' => ['#', '名称', '地址', '类型', '所属企业', '创建于', '更新于', '状态 . 操作'],
+            'batch' => true,
             'uris'   => $this->uris(),
         ]);
         
