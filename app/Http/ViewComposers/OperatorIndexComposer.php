68320a7289cc89cfe146167b9d714ef6ed6b252b
Hao Yuhang <haoyh@yahoo.com>
2018-06-25 14:18:12 +0800

    test

--- app/Http/ViewComposers/OperatorIndexComposer.php
+++ app/Http/ViewComposers/OperatorIndexComposer.php
@@ -1,12 +1,9 @@
 <?php
 namespace App\Http\ViewComposers;
 
-use App\Helpers\ModelTrait;
 use Illuminate\Contracts\View\View;
 
 class OperatorIndexComposer {
-    
-    use ModelTrait;
     
     public function compose(View $view) {
         
@@ -16,7 +13,6 @@ class OperatorIndexComposer {
                 '#', '用户名', '角色', '头像', '真实姓名',
                 '性别', '电子邮件', '创建于', '更新于', '状态 . 操作',
             ],
-            // 'uris'   => $this->uris(),
         ]);
         
     }
