96a90e57003fb56128f9f3e57fec45e9bd32cac7
Hao Yuhang <haoyh@yahoo.com>
2018-06-28 17:30:21 +0800

    test

--- app/Http/ViewComposers/UserMessageComposer.php
+++ app/Http/ViewComposers/UserMessageComposer.php
@@ -9,6 +9,7 @@ class UserMessageComposer {
         
         $view->with([
             'titles' => ['#', '通信方式', '应用', '消息批次', '接收者', '类型', '发送于', '状态(发送/阅读)'],
+            'batch' =>true
         ]);
         
     }
