dd72d338cdd58cc59193e4eb3eaf12d8b2d3eccf
Hao Yuhang <haoyh@yahoo.com>
2018-06-28 11:58:17 +0800

    test

--- app/Http/ViewComposers/UserEventComposer.php
+++ app/Http/ViewComposers/UserEventComposer.php
@@ -7,6 +7,13 @@ class UserEventComposer {
     
     public function compose(View $view) {
     
+        $view->with([
+            'titles' => [
+                '#', '名称', '备注', '地点', '开始时间', '结束时间',
+                '公共事件', '课程', '提醒', '创建者', '更新于'
+            ]
+        ]);
+        
     }
     
 }
