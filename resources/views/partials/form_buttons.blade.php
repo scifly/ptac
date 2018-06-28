f31aaa061b9119450e663adeef985d0e16e1fd84
Hao Yuhang <haoyh@yahoo.com>
2018-06-28 10:39:40 +0800

    test

--- resources/views/partials/form_buttons.blade.php
+++ resources/views/partials/form_buttons.blade.php
@@ -9,15 +9,18 @@
             {{--]) !!}--}}
             <button class="btn btn-primary btn-sm" id="{!! isset($id) ? $id : 'save' !!}" type="submit">
                 <i class="fa {!! isset($class) ? $class : 'fa-save' !!}">
-                    {!! isset($label) ? $label : '保存' !!}
+                    {!! isset($label) ? $label : ' 保存' !!}
                 </i>
             </button>
             @if (!isset($disabled))
                 @can('act', $uris['index'])
-                    {!! Form::reset('取消', [
-                        'class' => 'btn btn-sm btn-default pull-right',
-                        'id' => 'cancel'
-                    ]) !!}
+                    {{--{!! Form::reset('取消', [--}}
+                        {{--'class' => 'btn btn-sm btn-default pull-right',--}}
+                        {{--'id' => 'cancel'--}}
+                    {{--]) !!}--}}
+                    <button class="btn btn-default btn-sm pull-right" id="cancel" type="reset">
+                        <i class="fa fa-mail-reply"> 取消</i>
+                    </button>
                 @endcan
             @endif
         </div>
