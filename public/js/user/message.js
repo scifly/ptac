5316a9056699eae6074b6c0dafa73f286151539a
Hao Yuhang <haoyh@yahoo.com>
2018-06-28 18:10:52 +0800

    test

--- public/js/user/message.js
+++ public/js/user/message.js
@@ -1,22 +1,3 @@
-// $('#data-table').dataTable({
-//     processing: true,
-//     serverSide: true,
-//     ajax: {
-//         url: page.siteRoot() + 'users/message' + page.getQueryString(),
-//         error: function (e) {
-//             page.errorHandler(e);
-//         }
-//     },
-//     order: [[0, 'desc']],
-//     stateSave: true,
-//     autoWidth: true,
-//     columnDefs: [
-//         { className: 'text-center', targets: [0, 1, 2, 3, 4, 5, 6, 7, 8] },
-//     ],
-//     scrollX: true,
-//     language: {url: page.siteRoot() + '/files/ch.json'},
-//     lengthMenu: [[15, 25, 50, -1], [15, 25, 50, '所有']]
-// });
 //# sourceURL=message.js
 var options = [
     { className: 'text-center', targets: [0, 1, 2, 3, 4, 5, 6, 7] },
