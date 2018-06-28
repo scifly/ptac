a7b8bdb5c15705c4fbe9c6af6541b9cb506762d4
Hao Yuhang <haoyh@yahoo.com>
2018-06-28 10:32:07 +0800

    test

--- resources/views/partials/form_buttons.blade.php
+++ resources/views/partials/form_buttons.blade.php
@@ -3,14 +3,19 @@
     {{--button--}}
     <div class="form-group">
         <div class="col-sm-3 col-sm-offset-3">
-            {!! Form::submit(isset($label) ? $label : '保存', [
-                'class' => 'btn btn-primary pull-left fa-save',
-                'id' => isset($id) ? $id : 'save'
-            ]) !!}
+            {{--{!! Form::submit(isset($label) ? $label : '保存', [--}}
+                {{--'class' => 'btn btn-primary pull-left fa-save',--}}
+                {{--'id' => isset($id) ? $id : 'save'--}}
+            {{--]) !!}--}}
+            <button class="btn btn-primary btn-sm" id="{!! isset($id) ? $id : 'save' !!}" type="submit">
+                <i class="fa {!! isset($class) ? $class : 'fa-save' !!}">
+                    {!! isset($label) ? $label : '保存' !!}
+                </i>
+            </button>
             @if (!isset($disabled))
                 @can('act', $uris['index'])
                     {!! Form::reset('取消', [
-                        'class' => 'btn btn-default pull-right',
+                        'class' => 'btn btn-sm btn-default pull-right',
                         'id' => 'cancel'
                     ]) !!}
                 @endcan
