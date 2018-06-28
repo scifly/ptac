b3e465acd2a6d749acf3e0214493041a2fbecd03
Hao Yuhang <haoyh@yahoo.com>
2018-06-28 18:08:38 +0800

    test

--- public/js/user/message.js
+++ public/js/user/message.js
@@ -19,6 +19,6 @@
 // });
 //# sourceURL=message.js
 var options = [
-    { className: 'text-center', targets: [0, 1, 2, 3, 4, 5, 6, 7, 8] },
+    { className: 'text-center', targets: [0, 1, 2, 3, 4, 5, 6, 7] },
 ];
 page.initDatatable('users', options, 'message');
