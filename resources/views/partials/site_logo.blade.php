2c2d95550af5b8bf7c3cd27337e30e899b83c5c1
Hao Yuhang <haoyh@yahoo.com>
2018-06-29 11:40:22 +0800

    test

--- resources/views/partials/site_logo.blade.php
+++ resources/views/partials/site_logo.blade.php
@@ -1,4 +1,4 @@
 <a href="#" class="logo">
-    <span class="logo-mini"><b>PT</b></span>
+    <span class="logo-mini"><b>PTAC</b></span>
     <span class="logo-lg"><b>{!! config('app.name') !!}</b></span>
 </a>
