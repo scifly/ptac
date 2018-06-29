b065d39a10afea7ba9e99b4b95ddc4fd1fc5fd54
Hao Yuhang <haoyh@yahoo.com>
2018-06-29 15:21:45 +0800

    test

--- public/js/exam/create.js
+++ public/js/exam/create.js
@@ -1,1 +1,14 @@
 page.create('formExam', 'exams');
+
+$.getScript(
+    page.siteRoot() + plugins.daterangepicker.moment,
+    function () {
+        $.getScript(
+            page.siteRoot() + plugins.daterangepicker.js,
+            function () {
+                page.loadCss(plugins.daterangepicker.css);
+                $('#daterange').daterangepicker({locale: page.dateRangeLocale()});
+            }
+        )
+    }
+);
