87638c268b324b485b29b4f6726dbd2209e37367
Hao Yuhang <haoyh@yahoo.com>
2018-06-29 10:32:50 +0800

    test

--- resources/views/home/profile.blade.php
+++ resources/views/home/profile.blade.php
@@ -1,11 +1,10 @@
 <div class="modal fade" id="user-profile">
     <div class="modal-dialog">
-        <div class="modal-content modal-lg">
-            <div class="modal-header">
-                <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
-                {{--<h4 class="modal-title">个人信息</h4>--}}
-            </div>
+        <div class="modal-content modal">
+            {{--<div class="modal-header">--}}
+            {{--</div>--}}
             <div class="modal-body with-border">
+                <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
                 @include('user.edit', [
                     'user' => Auth::user(),
                     'breadcrumb' => '个人信息'
