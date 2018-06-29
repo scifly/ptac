c107b8678815403be6522c4666ff59d98a23417f
Hao Yuhang <haoyh@yahoo.com>
2018-06-29 10:26:08 +0800

    test

--- resources/views/home/profile.blade.php
+++ resources/views/home/profile.blade.php
@@ -3,12 +3,13 @@
         <div class="modal-content modal-lg">
             <div class="modal-header">
                 <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
-                <h4 class="modal-title">个人信息</h4>
+                {{--<h4 class="modal-title">个人信息</h4>--}}
             </div>
             <div class="modal-body with-border">
                 @include('user.edit', [
                     'user' => Auth::user(),
-                    'mobile' => '12345678901'
+                    'mobile' => '12345678901',
+                    'breadcrumb' => '个人信息'
                 ])
             </div>
             <div class="modal-footer"></div>
