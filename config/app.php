8c2b503a121ffc739f035d1cdeb8b2338254ac7d
Hao Yuhang <haoyh@yahoo.com>
2018-06-29 10:53:06 +0800

    test

--- config/app.php
+++ config/app.php
@@ -9,7 +9,7 @@ return [
     | framework needs to place the application's name in a notification or
     | any other location as required by the application or its packages.
     */
-    'name'            => env('APP_NAME', 'Laravel'),
+    'name'            => env('APP_NAME', 'PTAC'),
     /*
     |--------------------------------------------------------------------------
     | Application Environment
