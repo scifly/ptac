db1b89b3cf8cd1d55fa75da1501b5fecc831a5fc
Hao Yuhang <haoyh@yahoo.com>
2018-07-02 10:31:49 +0800

    test

--- public/js/custodian/index.js
+++ public/js/custodian/index.js
@@ -1,11 +1,9 @@
 //# sourceURL=index.js
 page.index('custodians', [
-    {className: 'text-center', targets: [1, 2, 4, 5, 6, 7]}
+    {className: 'text-center', targets: [1, 2, 3, 4, 5, 6, 7, 8]}
 ]);
 
 /** 初始化监护人首页功能 */
 $.getMultiScripts(['js/contact.js']).done(
-    function () {
-        $.contact().index('custodians');
-    }
+    function () { $.contact().index('custodians'); }
 );
