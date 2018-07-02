30329f4812343c27a575fd0e20bb2a2b4ea1c051
Hao Yuhang <haoyh@yahoo.com>
2018-07-02 11:13:30 +0800

    test

--- public/js/educator/index.js
+++ public/js/educator/index.js
@@ -1,6 +1,6 @@
 //# sourceURL=index.js
 page.index('educators', [
-    {className: 'text-center', targets: [1, 2, 3, 4, 5]}
+    {className: 'text-center', targets: [2, 3, 4, 5]}
 ]);
 
 /** 初始化教职员工首页功能 */
