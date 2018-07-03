d3001ec6b84494bf704950ed368f79818eef45c1
Hao Yuhang <haoyh@yahoo.com>
2018-07-03 17:05:49 +0800

    refactor

--- app/Policies/GradePolicy.php
+++ app/Policies/GradePolicy.php
@@ -54,7 +54,7 @@ class GradePolicy {
         if (in_array($action, ['store', 'update'])) {
             $departmentId = Request::input('department_id');
             $educatorIds = Request::input('educator_ids') ?? [];
-            $isDepartmentAllowed = in_array($departmentId, $this->departmentIds($user->id));
+            $isDepartmentAllowed = !$departmentId ? true : in_array($departmentId, $this->departmentIds($user->id));
             $isEducatorAllowed = empty(array_diff($educatorIds, $this->contactIds('educator')));
         }
         if (in_array($action, ['edit', 'update', 'destroy'])) {
