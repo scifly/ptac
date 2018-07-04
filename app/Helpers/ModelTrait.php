ac55fc1342c41c5593c44b34ef96dbfb714b1c5d
Hao Yuhang <haoyh@yahoo.com>
2018-07-04 15:02:17 +0800

    debug

--- app/Helpers/ModelTrait.php
+++ app/Helpers/ModelTrait.php
@@ -545,7 +545,7 @@ trait ModelTrait {
             return $writer->save('php://output');
         }
         $dir = 'public/uploads/' . date('Y/m/d/');
-        Log::debug($dir . ' exists: ' . !file_exists($dir));
+        Log::debug($dir . ' exists: ' . file_exists($dir));
         if (!file_exists($dir)) {
             mkdir($dir, 0777, true);
         }
