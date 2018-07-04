6e0a5db06cf7b08072856bcf93888b0f4cbf2457
Hao Yuhang <haoyh@yahoo.com>
2018-07-04 14:49:56 +0800

    debug

--- app/Helpers/ModelTrait.php
+++ app/Helpers/ModelTrait.php
@@ -544,7 +544,7 @@ trait ModelTrait {
             header('Pragma: public'); // HTTP/1.0
             return $writer->save('php://output');
         }
-        $dir = 'uploads/' . date('Y/m/d');
+        $dir = 'public/uploads/' . date('Y/m/d');
         if (!file_exists($dir)) {
             mkdir($dir, 0777, true);
         }
