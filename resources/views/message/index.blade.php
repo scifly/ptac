f1abebb149023ade9ac65307824d5358f44feb17
Hao Yuhang <haoyh@yahoo.com>
2018-07-12 16:25:20 +0800

    test

--- resources/views/message/index.blade.php
+++ resources/views/message/index.blade.php
@@ -53,7 +53,7 @@
                             <div id="checked-nodes"></div>
                             {!! Form::hidden('selected-node-ids', null, [
                                 'id' => 'selected-node-ids',
-                                'required'
+                                'required' => 'true'
                             ]) !!}
                             {!! Form::button('<i class="fa fa-user-plus text-blue">&nbsp;选择</i>', [
                                 'id' => 'choose',
