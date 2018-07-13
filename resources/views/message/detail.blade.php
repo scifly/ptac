272396904192e064b31ef145b0c3f4a328ff2d20
Hao Yuhang <haoyh@yahoo.com>
2018-07-13 15:23:53 +0800

    test

--- resources/views/message/detail.blade.php
+++ resources/views/message/detail.blade.php
@@ -12,23 +12,39 @@
             <div class="row">
                 <div class="col-xs-12" id="detail-body" style="border-bottom: 1px solid #f4f4f4 ">
                     <div>{!! $msgBody !!}</div>
-                </div>
-                <div>
-                    @if ($app)
-                        <img style="height: 16px; vertical-align: sub;" alt="" src="{!! $app['square_logo_url'] !!}"/>
-                        {!! $app['name'] !!}
-                    @else
-                        未知
-                    @endif
-                </div>
-                <div>
-                    <label class="control-label">发送时间:</label> {!! $sentAt !!}
-                </div>
-                <div>
-                    <label class="control-label">发送对象: </label>{!! $recipients !!}
-                </div>
-                <div>
-                    <label class="control-label">发送者: </label>{!! $sender !!}
+                    <div>
+                        @if ($app)
+                            <img style="height: 16px; vertical-align: sub;" alt="" src="{!! $app['square_logo_url'] !!}"/>
+                            {!! $app['name'] !!}
+                        @else
+                            未知
+                        @endif
+                    </div>
+                    <div>
+                        <label class="control-label">发送时间:</label> {!! $sentAt !!}
+                    </div>
+                    <div>
+                        <label class="control-label">发送对象: </label>{!! $recipients !!}
+                    </div>
+                    <div>
+                        <label class="control-label">发送者: </label>{!! $sender !!}
+                    </div>
+                    <div class="box-footer no-padding">
+                        <ul class="nav nav-stacked">
+                            <li>
+                                @if ($app)
+                                    <img style="height: 16px; vertical-align: sub;" alt="" src="{!! $app['square_logo_url'] !!}"/>
+                                    {!! $app['name'] !!}
+                                @else
+                                    未知
+                                @endif
+                            </li>
+                            <li><a href="#">Tasks <span class="pull-right badge bg-aqua">5</span></a></li>
+                            <li><a href="#">Completed Projects <span class="pull-right badge bg-green">12</span></a>
+                            </li>
+                            <li><a href="#">Followers <span class="pull-right badge bg-red">842</span></a></li>
+                        </ul>
+                    </div>
                 </div>
             </div>
         </div>
