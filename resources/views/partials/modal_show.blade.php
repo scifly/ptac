e6e7d3019b4a506bcdc9af11f44cd648cfc5323b
Hao Yuhang <haoyh@yahoo.com>
2018-07-13 13:43:14 +0800

    test

--- resources/views/partials/modal_show.blade.php
+++ resources/views/partials/modal_show.blade.php
@@ -6,37 +6,38 @@
                 <button type="button" class="close" data-dismiss="modal" aria-hidden="true">
                     &times;
                 </button>
-                <h4 class="modal-title" id="myModalLabel">
+                <h4 class="modal-title" id="detail-title">
                     企业名称
                 </h4>
             </div>
             <div class="modal-body">
                 <div class="row">
-                    <div class="col-xs-12">
-                        <dl class="dl-horizontal">
-                            <dt>地址：</dt>
-                            <dd>5039 Esteban Squares Apt. 714 Nadiamouth, LA 883765039 Esteban Squares Apt. 714
-                                Nadiamouth, LA 88376
-                            </dd>
-                            <dt>类型：</dt>
-                            <dd>Zachariah Kozey</dd>
-                            <dt>所属企业：</dt>
-                            <dd>成都凌凯通信技术</dd>
-                            <dt>创建于：</dt>
-                            <dd>2 days ago</dd>
-                            <dt>更新于：</dt>
-                            <dd>2 days ago</dd>
-                            <dt>状态：</dt>
-                            <dd><span class="badge bg-green">已启用</span></dd>
-                        </dl>
+                    <div class="col-xs-12" id="detail-body">
+                        {{--<dl class="dl-horizontal">--}}
+                            {{--<dt>地址：</dt>--}}
+                            {{--<dd>5039 Esteban Squares Apt. 714 Nadiamouth, LA 883765039 Esteban Squares Apt. 714--}}
+                                {{--Nadiamouth, LA 88376--}}
+                            {{--</dd>--}}
+                            {{--<dt>类型：</dt>--}}
+                            {{--<dd>Zachariah Kozey</dd>--}}
+                            {{--<dt>所属企业：</dt>--}}
+                            {{--<dd>成都凌凯通信技术</dd>--}}
+                            {{--<dt>创建于：</dt>--}}
+                            {{--<dd>2 days ago</dd>--}}
+                            {{--<dt>更新于：</dt>--}}
+                            {{--<dd>2 days ago</dd>--}}
+                            {{--<dt>状态：</dt>--}}
+                            {{--<dd><span class="badge bg-green">已启用</span></dd>--}}
+                        {{--</dl>--}}
                     </div>
                 </div>
             </div>
             <div class="modal-footer">
-                <button type="button" class="btn btn-default" data-dismiss="modal">关闭
+                <button type="button" class="btn btn-default" data-dismiss="modal">
+                    <i class="fa fa-sign-out"> 关闭</i>
                 </button>
                 <button type="button" class="btn btn-primary">
-                    编辑信息
+                    <i class="fa fa-edit"> 编辑</i>
                 </button>
             </div>
         </div>
