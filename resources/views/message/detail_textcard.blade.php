c541eb09ba0e805d6c1ba374ae6db2336a78a641
Hao Yuhang <haoyh@yahoo.com>
2018-07-16 09:41:21 +0800

    test

---
+++ resources/views/message/detail_textcard.blade.php
@@ -0,0 +1,13 @@
+<div class="small-box bg-green">
+    <div class="inner">
+        <h3>{!! $message->{'title'} !!}</h3>
+        <p>{!! $message->{'description'} !!}</p>
+    </div>
+    <div class="icon">
+        <i class="fa fa-link"></i>
+    </div>
+    <a href="#" class="small-box-footer">
+        {!! $message->{'btntxt'} !!}
+        <i class="fa fa-arrow-circle-right"></i>
+    </a>
+</div>
