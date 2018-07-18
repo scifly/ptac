72a01c85ab2e992462ecc9c8827bbc3fff6c8615
Hao Yuhang <haoyh@yahoo.com>
2018-07-18 18:02:46 +0800

    test

--- resources/views/wechat/attendance/educator.blade.php
+++ resources/views/wechat/attendance/educator.blade.php
@@ -22,7 +22,9 @@
             </div>
         </div>
         <div id="main" style="width: 100%; height: 300px;">
-            <span style="color: lightgray; margin: 5px auto; display:block;">(暂无图表，请点击右上角【+】按钮选择班级/规则/日期查看打卡详情)</span>
+            <span style="color: lightgray; margin: 5px auto; text-align: center;">
+                暂无图表，请点击右上角【+】按钮选择班级/规则/日期查看打卡详情
+            </span>
         </div>
         <table class="kaoqin-tongji">
             <tr>
@@ -60,7 +62,9 @@
                 </div>
             </div>
             <div class="modal-content">
-                <span style="color: lightgray; margin: 5px auto; ">(暂无数据)</span>
+                <span style="color: lightgray; margin: 5px; text-align: center;">
+                    (暂无数据)
+                </span>
             </div>
         </div>
     </div>
