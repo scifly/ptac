76689d6e41e6f958b0d17831133715ee85aab7ef
Hao Yuhang <haoyh@yahoo.com>
2018-07-19 16:27:59 +0800

    test

--- resources/views/wechat/attendance/list.blade.php
+++ resources/views/wechat/attendance/list.blade.php
@@ -2,14 +2,14 @@
 	@foreach($normals as $normal)
 	<div class="list-item">
 		<div class="list-item-info">
-			<div class="username">姓名 : <span>{{ $normal['student'] }}</span></div>
+			<div class="username">姓名 : <span>{!! $normal['student'] !!}</span></div>
 			@foreach($normal['custodians'] as $custodian)
-				<div class="parent">监护人 : <span>{{ $custodian }}</span></div>
+				<div class="parent">监护人 : <span>{!! $custodian !!}</span></div>
 			@endforeach
 			@foreach($normal['mobiles'] as $mobile)
-				<div class="mobile">手机 : <span>{{ $mobile }}</span></div>
+				<div class="mobile">手机 : <span>{!! $mobile !!}</span></div>
 			@endforeach
-			<div class="otherinfo">打卡时间: {{ $normal['punch_time'] }}</div>
+			<div class="otherinfo">打卡时间: {!! $normal['punch_time'] !!}</div>
 		</div>
 	</div>
 	@endforeach
@@ -18,14 +18,14 @@
 	@foreach($abnormals as $abnormal)
 		<div class="list-item">
 			<div class="list-item-info">
-				<div class="username">姓名 : <span>{{ $abnormal['student'] }}</span></div>
+				<div class="username">姓名 : <span>{!! $abnormal['student'] !!}</span></div>
 				@foreach($abnormal['custodians'] as $custodian)
-					<div class="parent">监护人 : <span>{{ $custodian }}</span></div>
+					<div class="parent">监护人 : <span>{!! $custodian !!}</span></div>
 				@endforeach
 				@foreach($abnormal['mobiles'] as $mobile)
-					<div class="mobile">手机 : <span>{{ $mobile }}</span></div>
+					<div class="mobile">手机 : <span>{!! $mobile !!}</span></div>
 				@endforeach
-				<div class="otherinfo">打卡时间: {{ $abnormal['punch_time'] }}</div>
+				<div class="otherinfo">打卡时间: {!! $abnormal['punch_time'] !!}</div>
 			</div>
 		</div>
 	@endforeach
@@ -34,12 +34,12 @@
 	@foreach($missed as $m)
 		<div class="list-item">
 			<div class="list-item-info">
-				<div class="username">姓名 : <span>{{ $m['student'] }}</span></div>
+				<div class="username">姓名 : <span>{!! $m['student'] !!}</span></div>
 				@foreach($m['custodians'] as $custodian)
-					<div class="parent">监护人 : <span>{{ $cusname }}</span></div>
+					<div class="parent">监护人 : <span>{!! $custodian !!}</span></div>
 				@endforeach
 				@foreach($m['mobiles'] as $mobile)
-					<div class="mobile">手机 : <span>{{ $mobile }}</span></div>
+					<div class="mobile">手机 : <span>{!! $mobile !!}</span></div>
 				@endforeach
 			</div>
 		</div>
