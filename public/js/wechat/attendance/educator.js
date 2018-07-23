19bab4818d6053143cc7ccf4a13ba20dd41c4a98
Hao Yuhang <haoyh@yahoo.com>
2018-07-23 09:12:39 +0800

    test

--- public/js/wechat/attendance/educator.js
+++ public/js/wechat/attendance/educator.js
@@ -95,7 +95,7 @@ function showPie(data, legend, title) {
                 fontSize: 14,
                 color: 'gray',
                 fontWeight: 'normal',
-                lineHeight: 25
+                rich: { a: { lineHeight: 16 }}
             }
         },
         legend: {
