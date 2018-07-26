9b7ba1748cf7aa6308ab804d46277b98a5e492e7
Hao Yuhang <haoyh@yahoo.com>
2018-07-26 11:32:57 +0800

    test

--- resources/views/wechat/message_center/create_edit.blade.php
+++ resources/views/wechat/message_center/create_edit.blade.php
@@ -1,157 +1,159 @@
 <div class="msg-send-wrap">
-@if (isset($message))
-    {!! Form::hidden('id', $message->id, ['id' => 'id']) !!}
-@endif
-<!-- 发送对象 -->
-    <div id="chosen-container" class="scui-chosen js-scui-chosen-container3 js-scui-chosen scui-form-group">
-        {!! Form::label(null, '发送对象', ['class' => 'scui-control-label mr4']) !!}
-        <div id="chosen-results">
-            @if (isset($message))
-                {!! $chosenTargetsHtml !!}
-            @endif
-        </div>
-        <span class="icons-chosen chosen-icon js-chosen-icon">
-            <a class="icon iconfont icon-add c-green open-popup" href="#" data-target="#targets"></a>
-        </span>
-    </div>
-    <!-- 信息类型 -->
-    <div class="weui-cell weui-cell_select weui-cell_select-after" style="background-color: #fff;">
-        <div class="weui-cell__hd">
-            {!! Form::label('msg-type', '信息类型', ['class' => 'weui-label']) !!}
-        </div>
-        <div class="weui-cell__bd">
-            {!! Form::select(
-                'msg-type',
-                $msgTypes,
-                $selectedMsgTypeId ?? null,
-                [
-                    'id' => 'msg-type',
-                    'class' => 'weui-input',
-                    'disabled' => sizeof($msgTypes) <= 1
-                ]
-           ) !!}
-        </div>
-    </div>
-    <!-- 消息类型 -->
-    <div class="weui-cell weui-cell_select weui-cell_select-after" style="background-color: #fff;">
-        <div class="weui-cell__hd">
-            {!! Form::label('message_type_id', '消息类型', ['class' => 'weui-label']) !!}
-        </div>
-        <div class="weui-cell__bd">
-            {!! Form::select(
-                'message_type_id',
-                $messageTypes,
-                isset($message) ? $message->message_type_id : null,
-                [
-                    'id' => 'message_type_id',
-                    'class' => 'weui-input',
-                    'disabled' => sizeof($messageTypes) <= 1
-                ]
-            ) !!}
-        </div>
-    </div>
-    <div style="height: 5px;"></div>
-    <!-- 标题(视频、卡片) -->
-    <div id="title-container" class="mt5px msg-send-bg b-bottom hw-title"
-         style="display: {!! $title ? 'block' : 'none';  !!};">
-        <div class="weui-cell">
-            <div class="weui-cell__bd js-title">
-                {!! Form::text(
-                    'title',
-                    $title ?? null,
-                    [
-                        'id' => 'title',
-                        'class' => 'weui-input fs18 one-line title',
-                        'placeholder' => '标题',
-                        'maxlength' => 30
-                    ]
-                ) !!}
-            </div>
-        </div>
-    </div>
-    <!-- 内容(文本、视频、卡片、短信) -->
-    <div id="content-container" class="weui-cells weui-cells_form"
-         style="display: {!! $selectedMsgTypeId ? (in_array($selectedMsgTypeId, ['text', 'video', 'textcard', 'sms']) ? 'block' : 'none') : 'block' !!}">
-        <div class="weui-cell">
-            <div class="weui-cell__bd">
-                {!! Form::textarea('content', $content ?? null, [
-                    'id' => 'content',
-                    'placeholder' => '',
-                    'class' => 'weui-textarea',
-                    'rows' => 3
-                ]) !!}
-            </div>
-        </div>
-    </div>
-    <!-- 点击后跳转的链接(卡片) -->
-    <div id="card-url-container" class="msg-send-bg b-bottom hw-title extra"
-         style="display: {!! isset($url) ? 'block' : 'none' !!};">
-        <div class="weui-cell">
-            <div class="weui-cell__bd">
-                {!! Form::text(
-                    'card-url',
-                    $url ?? null,
-                    [
-                        'id' => 'card-url',
-                        'class' => 'weui-input one-line title',
-                        'placeholder' => '点击后跳转的地址',
-                        'maxlength' => 30
-                ]) !!}
-            </div>
-        </div>
-    </div>
-    <!-- 按钮文字(卡片) -->
-    <div id="btn-txt-container" class="msg-send-bg b-bottom hw-title extra"
-         style="display: {!! isset($btntxt) ? 'block' : 'none' !!};">
-        <div class="weui-cell">
-            <div class="weui-cell__bd">
-                {!! Form::text(
-                    'btn-txt',
-                    $btntxt ?? null,
-                    [
-                        'id' => 'btn-txt',
-                        'class' => 'weui-input one-line title',
-                        'placeholder' => '按钮文字。默认为“详情”',
-                        'maxlength' => 30
-                    ]
-                ) !!}
-            </div>
-        </div>
-    </div>
-    <div style="height: 5px;"></div>
-    <!-- 上传素材(图片、语音、视频、文件) -->
-    <div id="upload-container" class="msg-send-conicon msg-send-bg b-top extra"
-         style="display: {!! isset($mediaId) ? 'block' : 'none' !!};">
-        <div class="weui-cell">
-            <div class="weui-uploader">
-                <div class="weui-uploader__hd">
-                    <p id="upload-title" class="weui-uploader__title">
-                        {!! $filename ?? '' !!}
-                    </p>
+    @if (isset($message))
+        {!! Form::hidden('id', $message->id, ['id' => 'id']) !!}
+    @endif
+    <!-- 发送对象 -->
+        <div class="weui-cells weui-cells_form">
+            <div id="chosen-container" class="scui-chosen js-scui-chosen-container3 js-scui-chosen scui-form-group">
+                {!! Form::label(null, '发送对象', ['class' => 'scui-control-label mr4']) !!}
+                <div id="chosen-results">
+                    @if (isset($message))
+                        {!! $chosenTargetsHtml !!}
+                    @endif
                 </div>
-                <div class="weui-uploader__bd">
-                    <ul class="weui-uploader__files" style="display: {!! $filepath ? 'block' : 'none' !!}" id="file-display">
-                        <li class="weui-uploader__file" style="background-image: url({!! '/' . ($filepath ?? '') !!})"></li>
-                    </ul>
-                    <div class="weui-uploader__input-box">
-                        {!! Form::hidden(
-                            'media_id',
-                            $mediaId ?? null,
+                <span class="icons-chosen chosen-icon js-chosen-icon">
+                    <a class="icon iconfont icon-add c-green open-popup" href="#" data-target="#targets"></a>
+                </span>
+            </div>
+            <!-- 信息类型 -->
+            <div class="weui-cell weui-cell_select weui-cell_select-after" style="background-color: #fff;">
+                <div class="weui-cell__hd">
+                    {!! Form::label('msg-type', '信息类型', ['class' => 'weui-label']) !!}
+                </div>
+                <div class="weui-cell__bd">
+                    {!! Form::select(
+                        'msg-type',
+                        $msgTypes,
+                        $selectedMsgTypeId ?? null,
+                        [
+                            'id' => 'msg-type',
+                            'class' => 'weui-input',
+                            'disabled' => sizeof($msgTypes) <= 1
+                        ]
+                   ) !!}
+                </div>
+            </div>
+            <!-- 消息类型 -->
+            <div class="weui-cell weui-cell_select weui-cell_select-after" style="background-color: #fff;">
+                <div class="weui-cell__hd">
+                    {!! Form::label('message_type_id', '消息类型', ['class' => 'weui-label']) !!}
+                </div>
+                <div class="weui-cell__bd">
+                    {!! Form::select(
+                        'message_type_id',
+                        $messageTypes,
+                        isset($message) ? $message->message_type_id : null,
+                        [
+                            'id' => 'message_type_id',
+                            'class' => 'weui-input',
+                            'disabled' => sizeof($messageTypes) <= 1
+                        ]
+                    ) !!}
+                </div>
+            </div>
+            <div style="height: 5px;"></div>
+            <!-- 标题(视频、卡片) -->
+            <div id="title-container" class="mt5px msg-send-bg b-bottom hw-title"
+                 style="display: {!! $title ? 'block' : 'none';  !!};">
+                <div class="weui-cell">
+                    <div class="weui-cell__bd js-title">
+                        {!! Form::text(
+                            'title',
+                            $title ?? null,
                             [
-                                'id' => 'media_id',
-                                'data-path' => $filepath ?? null
+                                'id' => 'title',
+                                'class' => 'weui-input fs18 one-line title',
+                                'placeholder' => '标题',
+                                'maxlength' => 30
                             ]
                         ) !!}
-                        {!! Form::file('upload', [
-                            'id' => 'upload',
-                            'accept' => $accept ?? '',
-                            'class' => 'weui-uploader__input'
+                    </div>
+                </div>
+            </div>
+            <!-- 内容(文本、视频、卡片、短信) -->
+            <div id="content-container" class="weui-cells weui-cells_form"
+                 style="display: {!! $selectedMsgTypeId ? (in_array($selectedMsgTypeId, ['text', 'video', 'textcard', 'sms']) ? 'block' : 'none') : 'block' !!}">
+                <div class="weui-cell">
+                    <div class="weui-cell__bd">
+                        {!! Form::textarea('content', $content ?? null, [
+                            'id' => 'content',
+                            'placeholder' => '',
+                            'class' => 'weui-textarea',
+                            'rows' => 3
                         ]) !!}
                     </div>
                 </div>
             </div>
+            <!-- 点击后跳转的链接(卡片) -->
+            <div id="card-url-container" class="msg-send-bg b-bottom hw-title extra"
+                 style="display: {!! isset($url) ? 'block' : 'none' !!};">
+                <div class="weui-cell">
+                    <div class="weui-cell__bd">
+                        {!! Form::text(
+                            'card-url',
+                            $url ?? null,
+                            [
+                                'id' => 'card-url',
+                                'class' => 'weui-input one-line title',
+                                'placeholder' => '点击后跳转的地址',
+                                'maxlength' => 30
+                        ]) !!}
+                    </div>
+                </div>
+            </div>
+            <!-- 按钮文字(卡片) -->
+            <div id="btn-txt-container" class="msg-send-bg b-bottom hw-title extra"
+                 style="display: {!! isset($btntxt) ? 'block' : 'none' !!};">
+                <div class="weui-cell">
+                    <div class="weui-cell__bd">
+                        {!! Form::text(
+                            'btn-txt',
+                            $btntxt ?? null,
+                            [
+                                'id' => 'btn-txt',
+                                'class' => 'weui-input one-line title',
+                                'placeholder' => '按钮文字。默认为“详情”',
+                                'maxlength' => 30
+                            ]
+                        ) !!}
+                    </div>
+                </div>
+            </div>
+            <div style="height: 5px;"></div>
+            <!-- 上传素材(图片、语音、视频、文件) -->
+            <div id="upload-container" class="msg-send-conicon msg-send-bg b-top extra"
+                 style="display: {!! isset($mediaId) ? 'block' : 'none' !!};">
+                <div class="weui-cell">
+                    <div class="weui-uploader">
+                        <div class="weui-uploader__hd">
+                            <p id="upload-title" class="weui-uploader__title">
+                                {!! $filename ?? '' !!}
+                            </p>
+                        </div>
+                        <div class="weui-uploader__bd">
+                            <ul class="weui-uploader__files" style="display: {!! $filepath ? 'block' : 'none' !!}" id="file-display">
+                                <li class="weui-uploader__file" style="background-image: url({!! '/' . ($filepath ?? '') !!})"></li>
+                            </ul>
+                            <div class="weui-uploader__input-box">
+                                {!! Form::hidden(
+                                    'media_id',
+                                    $mediaId ?? null,
+                                    [
+                                        'id' => 'media_id',
+                                        'data-path' => $filepath ?? null
+                                    ]
+                                ) !!}
+                                {!! Form::file('upload', [
+                                    'id' => 'upload',
+                                    'accept' => $accept ?? '',
+                                    'class' => 'weui-uploader__input'
+                                ]) !!}
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
-    </div>
     <!-- 图文消息 -->
     <div id="mpnews-container" class="msg-send-conicon msg-send-bg b-top"
          style="display: {!! isset($mpnewsList) ? 'block' : 'none' !!};">
