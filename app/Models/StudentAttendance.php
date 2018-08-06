37304fbfb87788d3b671ef6b9166b23a81bd5ab6
Hao Yuhang <haoyh@yahoo.com>
2018-08-06 10:56:10 +0800

    test

--- app/Models/StudentAttendance.php
+++ app/Models/StudentAttendance.php
@@ -19,7 +19,6 @@ use Illuminate\Database\Query\Builder;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Request;
 use Illuminate\Support\Facades\Session;
 use Illuminate\Validation\Rule;
@@ -193,8 +192,6 @@ class StudentAttendance extends Model {
         try {
             DB::transaction(function () {
                 $data = Request::input('data');
-                Log::debug(json_encode(Request::all()));
-                Log::debug(Request::input('school_id'));
                 $school = School::find(Request::input('school_id'));
                 abort_if(
                     !$school,
