8650cec6815bd2b7619b52ce8702b2cf10cec0b1
Hao Yuhang <haoyh@yahoo.com>
2018-08-08 11:08:30 +0800

    test

--- resources/views/wechat/message_center/show.blade.php
+++ resources/views/wechat/message_center/show.blade.php
@@ -32,54 +32,54 @@
                                     <div class="wwbw js-wwbw mce-item-table article-message">
                                         <?php
                                         $type = $content['type'];
-                                        // $msg = $type != 'other' ? $content[$type]->{$type} : $content[$type];
+                                        $msg = $type != 'other' ? $content[$type]->{$type} : $content[$type];
                                         ?>
-                                        {{--@switch ($type)--}}
-                                            {{--@case ('text')--}}
-                                                {{--<p>{!! $msg->{'content'} !!}</p>--}}
-                                                {{--@break--}}
-                                            {{--@case ('image')--}}
-                                                {{--<p><img alt="" src="/{!! $msg->{'path'} !!}" /></p>--}}
-                                                {{--@break--}}
-                                            {{--@case ('voice')--}}
-                                                {{--<p><a href="/{!! $msg->{'path'} !!}">点击下载此语音</a></p>--}}
-                                                {{--@break--}}
-                                            {{--@case ('video')--}}
-                                                {{--<p>标题: {!! $msg->{'title'} !!}</p>--}}
-                                                {{--<p>描述: {!! $msg->{'description'} !!}</p>--}}
-                                                {{--<video controls>--}}
-                                                    {{--<source src="/{!! $msg->{'path'} !!}" type="video/mp4">--}}
-                                                {{--</video>--}}
-                                                {{--@break--}}
-                                            {{--@case ('file')--}}
-                                                {{--<p><a href="/{!! $content['file']->{'path'} !!}">点击下载此文件</a></p>--}}
-                                                {{--@break--}}
-                                            {{--@case ('textcard')--}}
-                                                {{--<div class="card-content">--}}
-                                                    {{--<p class="card-title">{!! $msg->{'title'} !!}</p>--}}
-                                                    {{--<p class="card-detail">{!! $msg->{'description'} !!}</p>--}}
-                                                    {{--<a class="card-url" href="{!! $msg->{'url'} !!}">--}}
-                                                        {{--{!! $msg->{'btntxt'} ? $msg->{'btntxt'} : '详情' !!}--}}
-                                                    {{--</a>--}}
-                                                {{--</div>--}}
-                                                {{--@break--}}
-                                            {{--@case ('mpnews')--}}
-                                                {{--@foreach ($msg->{'articles'} as $article)--}}
-                                                    {{--<div class="mpnews-item">--}}
-                                                        {{--<p class="mpnews-title">{!! $article->{'title'} !!}</p>--}}
-                                                        {{--<img alt="" src="/{!! $article->{'image_url'} !!}" />--}}
-                                                        {{--<p class="mpnews-digest">{!! $article->{'digest'} !!}</p>--}}
-                                                        {{--<a class="mpnews-url" href="{!! $article->{'content_source_url'} !!}">阅读全文</a>--}}
-                                                    {{--</div>--}}
-                                                {{--@endforeach--}}
-                                                {{--@break--}}
-                                            {{--@case ('sms')--}}
-                                                {{--<p>{!! $msg->{'sms'} !!}</p>--}}
-                                                {{--@break--}}
-                                            {{--@default--}}
-                                                {{--<p>{!! $msg !!}</p>--}}
-                                                {{--@break--}}
-                                        {{--@endswitch--}}
+                                        @switch ($type)
+                                            @case ('text')
+                                                <p>{!! $msg->{'content'} !!}</p>
+                                                @break
+                                            @case ('image')
+                                                <p><img alt="" src="/{!! $msg->{'path'} !!}" /></p>
+                                                @break
+                                            @case ('voice')
+                                                <p><a href="/{!! $msg->{'path'} !!}">点击下载此语音</a></p>
+                                                @break
+                                            @case ('video')
+                                                <p>标题: {!! $msg->{'title'} !!}</p>
+                                                <p>描述: {!! $msg->{'description'} !!}</p>
+                                                <video controls>
+                                                    <source src="/{!! $msg->{'path'} !!}" type="video/mp4">
+                                                </video>
+                                                @break
+                                            @case ('file')
+                                                <p><a href="/{!! $content['file']->{'path'} !!}">点击下载此文件</a></p>
+                                                @break
+                                            @case ('textcard')
+                                                <div class="card-content">
+                                                    <p class="card-title">{!! $msg->{'title'} !!}</p>
+                                                    <p class="card-detail">{!! $msg->{'description'} !!}</p>
+                                                    <a class="card-url" href="{!! $msg->{'url'} !!}">
+                                                        {!! $msg->{'btntxt'} ? $msg->{'btntxt'} : '详情' !!}
+                                                    </a>
+                                                </div>
+                                                @break
+                                            @case ('mpnews')
+                                                @foreach ($msg->{'articles'} as $article)
+                                                    <div class="mpnews-item">
+                                                        <p class="mpnews-title">{!! $article->{'title'} !!}</p>
+                                                        <img alt="" src="/{!! $article->{'image_url'} !!}" />
+                                                        <p class="mpnews-digest">{!! $article->{'digest'} !!}</p>
+                                                        <a class="mpnews-url" href="{!! $article->{'content_source_url'} !!}">阅读全文</a>
+                                                    </div>
+                                                @endforeach
+                                                @break
+                                            @case ('sms')
+                                                <p>{!! $msg->{'sms'} !!}</p>
+                                                @break
+                                            @default
+                                                <p>{!! $msg !!}</p>
+                                                @break
+                                        @endswitch
                                     </div>
                                 </div>
                             </div>
