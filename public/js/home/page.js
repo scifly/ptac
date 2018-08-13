35ba306cb22ad90b63aadadfa37a253867c2b760
Hao Yuhang <haoyh@yahoo.com>
2018-08-13 16:31:44 +0800

    test

--- public/js/home/page.js
+++ public/js/home/page.js
@@ -414,7 +414,7 @@ var page = {
                     });
                 });
                 dt.search('').columns().search('').draw();
-                $(document).on('keyup click', '#data-table tfoot .form-control', function (e) {
+                $(document).on('click', '#data-table tfoot .form-control', function (e) {
                     console.log($(this).parent().index());
                 });
                 dt.columns().every(function () {
