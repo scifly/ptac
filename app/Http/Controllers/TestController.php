b4eb4f852e417ac0fb6561d35774ab7b4068dc86
Hao Yuhang <haoyh@yahoo.com>
2018-08-14 16:41:49 +0800

    test

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -32,7 +32,7 @@ class TestController extends Controller {
         
         Carbon::setLocale('zh');
         
-        dd(Carbon::parse('2018-09-30')->diffForHumans());
+        dd(Carbon::createFromTimeString('1天前'));
         
         $appid = '5100000025';
         $appsecret = 'B4C6F3A34F5936CEBA92C008F12B0396';
