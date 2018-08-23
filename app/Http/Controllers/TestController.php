49da527872ce29970a1c20c57609393bfa0a7f21
Hao Yuhang <haoyh@yahoo.com>
2018-08-23 16:33:59 +0800

    test

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -54,8 +54,7 @@ class TestController extends Controller {
         ]);
         $request->headers->add(['content-type' => 'application/json']);
         $response = Route::dispatch($request);
-        dd($response);
-        exit;
+        dd(json_decode($response->getContent()));
         $result = Validator::make($request->all(), $request->rules());
         $failed = $result->fails();
         if ($failed) {
