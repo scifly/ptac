39de6e0723445c900a142309bda6e347e0f016bf
Hao Yuhang <haoyh@yahoo.com>
2018-08-29 14:25:53 +0800

    test

--- app/Http/Controllers/Wechat/SyncController.php
+++ app/Http/Controllers/Wechat/SyncController.php
@@ -80,30 +80,7 @@ class SyncController extends Controller {
                         break;
                     case 'change_contact':  # 通讯录变更
                         $changeType = $this->event->{'ChangeType'};
-                        $this->{Inflector::camelize($changeType)};
-                        switch ($changeType) {
-                            case 'create_user':     # 创建会员
-                                $this->createUser();
-                                break;
-                            case 'update_user':     # 更新会员
-                                $this->updateUser();
-                                break;
-                            case 'delete_user':     # 删除会员
-                                $this->deleteUser();
-                                break;
-                            case 'create_party':    # 创建部门
-                                $this->createParty();
-                                break;
-                            case 'update_party':    # 更新部门
-                                $this->updateParty();
-                                break;
-                            case 'delete_party':    # 删除部门
-                                $this->deleteParty();
-                                break;
-                            case 'update_tag':      # 更新标签
-                            default:
-                                break;
-                        }
+                        $this->{Inflector::camelize($changeType)}();
                         break;
                     default:
                         break;
@@ -113,7 +90,207 @@ class SyncController extends Controller {
             throw $e;
         }
         
-        return true;
+        return '';
+        
+    }
+    
+    /**
+     * 创建会员
+     *
+     * @throws Throwable
+     */
+    protected function createUser() {
+    
+        # 创建用户(教职员工)
+        $data = $this->data();
+        $data['username'] = $data['userid'];
+        $data['password'] = bcrypt('12345678');
+        $data['synced'] = 1;
+        $data['enabled'] = 1;
+        $role = $this->role();
+        $school = null;
+        switch ($role) {
+            case 'educator':
+                $school = $this->school();
+                $data['group_id'] = Group::where([
+                    'name' => '教职员工',
+                    'school_id' => $school->id
+                ])->first()->id;
+                break;
+            case 'school':
+                $data['group_id'] = Group::whereName('学校')->first()->id;
+                break;
+            case 'corp':
+                $data['group_id'] = Group::whereName('企业')->first()->id;
+                break;
+            default:
+                break;
+        }
+        $user = User::create($data);
+        if ($role != 'corp') {
+            # 创建教职员工
+            Educator::create([
+                'user_id'   => $user->id,
+                'school_id' => $school->id,
+                'sms_quote' => 0,
+                'enabled'   => 1
+            ]);
+            $departmentIds = (array) $this->event->{'Department'};
+        } else {
+            $departmentIds = [$this->corp->department_id];
+        }
+        # 创建部门&用户绑定关系
+        (new DepartmentUser)->storeByUserId($user->id, $departmentIds);
+        # 保存用户手机号码
+        Mobile::create([
+            'user_id' => $user->id,
+            'mobile' => $this->event->{'Mobile'},
+            'isdefault' => 1,
+            'enabled' => 1
+        ]);
+        
+    }
+    
+    /**
+     * 更新会员
+     *
+     * @throws Throwable
+     */
+    protected function updateUser() {
+    
+        # 更新用户
+        $user = User::whereUserid($this->event->{'UserID'})->first();
+        if (!$user) {
+            $this->createUser();
+        } else {
+            $user->update($this->data());
+            # 更新用户手机号码
+            if (property_exists($this->event, 'Mobile')) {
+                Mobile::where(['user_id' => $user->id, 'isdefault' => 1])
+                    ->first()->update(['mobile' => $this->event->{'Mobile'}]);
+            }
+            # 更新用户所属部门
+            if (property_exists($this->event, 'Department')) {
+                $du = new DepartmentUser;
+                $du->where('user_id', $user->id)->delete();
+                $role = $this->role();
+                $departmentIds = (array) $this->event->{'Department'};
+                switch ($role) {
+                    case 'educator':
+                        $user->update([
+                            'group_id' => Group::where([
+                                'name' => '教职员工',
+                                'school_id' => $this->school()->id
+                            ])
+                        ]);
+                        break;
+                    case 'school':
+                        $user->update(['group_id' => Group::whereName('学校')->first()->id]);
+                        break;
+                    case 'corp':
+                        $user->update(['group_id' => Group::whereName('企业')->first()->id]);
+                        $departmentIds = [$this->corp->department_id];
+                        break;
+                    default:
+                        break;
+                }
+                $du->storeByUserId($user->id, $departmentIds);
+            }
+        }
+        
+    }
+    
+    /**
+     * 删除会员
+     *
+     * @throws Throwable
+     */
+    protected function deleteUser() {
+    
+        $userId = User::whereUserid($this->event->{'UserID'})->first()->id;
+        (new User)->remove($userId, false);
+        
+    }
+    
+    /**
+     * 创建部门
+     */
+    protected function createParty() {
+    
+        $data = [
+            'id' => $this->event->{'Id'},
+            'name' => $this->event->{'Name'},
+            'parent_id' => $this->event->{'ParentId'},
+            'department_type_id' => DepartmentType::whereName('其他')->first()->id,
+            'enabled' => 1
+        ];
+        Department::create($data);
+        
+    }
+    
+    /**
+     * 更新部门
+     */
+    protected function updateParty() {
+    
+        $department = Department::find($this->event->{'Id'});
+        if (!$department) {
+            $this->createParty();
+        } else {
+            $department->update([
+                'name' => $this->event->{'Name'},
+                'parent_id' => $this->event->{'ParentId'}
+            ]);
+        }
+        
+    }
+    
+    /**
+     * 删除部门
+     *
+     * @throws Throwable
+     */
+    protected function deleteParty() {
+    
+        (new Department)->remove($this->event->{'Id'});
+        
+    }
+    
+    /**
+     * 更新标签
+     */
+    protected function updateTag() {
+    
+    
+    
+    }
+    
+    /**
+     * 验证回调Url
+     */
+    protected function verifyUrl() {
+        
+        $paths = explode('/', Request::path());
+        $corp = Corp::whereAcronym($paths[0])->first();
+        // 假设企业号在公众平台上设置的参数如下
+        $encodingAesKey = $corp->encoding_aes_key;
+        $token = $corp->token;
+        $corpId = $corp->corpid;
+        $sVerifyMsgSig = Request::query('msg_signature');
+        $sVerifyTimeStamp = Request::query('timestamp');
+        $sVerifyNonce = Request::query('nonce');
+        $sVerifyEchoStr = rawurldecode(Request::query('echostr'));
+        // 需要返回的明文
+        $sEchoStr = "";
+        $wxcpt = new WXBizMsgCrypt($token, $encodingAesKey, $corpId);
+        $errCode = $wxcpt->VerifyURL(
+            $sVerifyMsgSig,
+            $sVerifyTimeStamp,
+            $sVerifyNonce,
+            $sVerifyEchoStr,
+            $sEchoStr
+        );
+        echo !$errCode ? $sEchoStr : "ERR: " . $errCode . "\n\n";
         
     }
     
@@ -174,7 +351,7 @@ class SyncController extends Controller {
      *
      * @return array
      */
-    private function memberData() {
+    private function data() {
         
         $data = [];
         foreach (self::MEMBER_PROPERTIES as $property => $field) {
@@ -236,197 +413,6 @@ class SyncController extends Controller {
         $schoolDepartmentId = (new Department)->departmentId($departmentId);
         
         return School::whereDepartmentId($schoolDepartmentId)->first();
-        
-    }
-    
-    /**
-     * 创建会员
-     *
-     * @throws Throwable
-     */
-    private function createUser() {
-    
-        # 创建用户(教职员工)
-        $data = $this->memberData();
-        $data['username'] = $data['userid'];
-        $data['password'] = bcrypt('12345678');
-        $data['synced'] = 1;
-        $data['enabled'] = 1;
-        $role = $this->role();
-        $school = null;
-        switch ($role) {
-            case 'educator':
-                $school = $this->school();
-                $data['group_id'] = Group::where([
-                    'name' => '教职员工',
-                    'school_id' => $school->id
-                ])->first()->id;
-                break;
-            case 'school':
-                $data['group_id'] = Group::whereName('学校')->first()->id;
-                break;
-            case 'corp':
-                $data['group_id'] = Group::whereName('企业')->first()->id;
-                break;
-            default:
-                break;
-        }
-        $user = User::create($data);
-        if ($role != 'corp') {
-            # 创建教职员工
-            Educator::create([
-                'user_id'   => $user->id,
-                'school_id' => $school->id,
-                'sms_quote' => 0,
-                'enabled'   => 1
-            ]);
-            $departmentIds = (array) $this->event->{'Department'};
-        } else {
-            $departmentIds = [$this->corp->department_id];
-        }
-        # 创建部门&用户绑定关系
-        (new DepartmentUser)->storeByUserId($user->id, $departmentIds);
-        # 保存用户手机号码
-        Mobile::create([
-            'user_id' => $user->id,
-            'mobile' => $this->event->{'Mobile'},
-            'isdefault' => 1,
-            'enabled' => 1
-        ]);
-        
-    }
-    
-    /**
-     * 更新会员
-     *
-     * @throws Throwable
-     */
-    private function updateUser() {
-    
-        # 更新用户
-        $user = User::whereUserid($this->event->{'UserID'})->first();
-        if (!$user) {
-            $this->createUser();
-        } else {
-            $user->update($this->memberData());
-            # 更新用户手机号码
-            if (property_exists($this->event, 'Mobile')) {
-                Mobile::where(['user_id' => $user->id, 'isdefault' => 1])
-                    ->first()->update(['mobile' => $this->event->{'Mobile'}]);
-            }
-            # 更新用户所属部门
-            if (property_exists($this->event, 'Department')) {
-                $du = new DepartmentUser;
-                $du->where('user_id', $user->id)->delete();
-                $role = $this->role();
-                $departmentIds = (array) $this->event->{'Department'};
-                switch ($role) {
-                    case 'educator':
-                        $user->update([
-                            'group_id' => Group::where([
-                                'name' => '教职员工',
-                                'school_id' => $this->school()->id
-                            ])
-                        ]);
-                        break;
-                    case 'school':
-                        $user->update(['group_id' => Group::whereName('学校')->first()->id]);
-                        break;
-                    case 'corp':
-                        $user->update(['group_id' => Group::whereName('企业')->first()->id]);
-                        $departmentIds = [$this->corp->department_id];
-                        break;
-                    default:
-                        break;
-                }
-                $du->storeByUserId($user->id, $departmentIds);
-            }
-        }
-        
-    }
-    
-    /**
-     * 删除会员
-     *
-     * @throws Throwable
-     */
-    private function deleteUser() {
-    
-        $userId = User::whereUserid($this->event->{'UserID'})->first()->id;
-        (new User)->remove($userId, false);
-        
-    }
-    
-    /**
-     * 创建部门
-     */
-    private function createParty() {
-    
-        $data = [
-            'id' => $this->event->{'Id'},
-            'name' => $this->event->{'Name'},
-            'parent_id' => $this->event->{'ParentId'},
-            'department_type_id' => DepartmentType::whereName('其他')->first()->id,
-            'enabled' => 1
-        ];
-        Department::create($data);
-        
-    }
-    
-    /**
-     * 更新部门
-     */
-    private function updateParty() {
-    
-        $department = Department::find($this->event->{'Id'});
-        if (!$department) {
-            $this->createParty();
-        } else {
-            $department->update([
-                'name' => $this->event->{'Name'},
-                'parent_id' => $this->event->{'ParentId'}
-            ]);
-        }
-        
-    }
-    
-    /**
-     * 删除部门
-     *
-     * @throws Throwable
-     */
-    private function deleteParty() {
-    
-        (new Department)->remove($this->event->{'Id'});
-        
-    }
-    
-    /**
-     * 验证回调Url
-     */
-    protected function verifyUrl() {
-        
-        $paths = explode('/', Request::path());
-        $corp = Corp::whereAcronym($paths[0])->first();
-        // 假设企业号在公众平台上设置的参数如下
-        $encodingAesKey = $corp->encoding_aes_key;
-        $token = $corp->token;
-        $corpId = $corp->corpid;
-        $sVerifyMsgSig = Request::query('msg_signature');
-        $sVerifyTimeStamp = Request::query('timestamp');
-        $sVerifyNonce = Request::query('nonce');
-        $sVerifyEchoStr = rawurldecode(Request::query('echostr'));
-        // 需要返回的明文
-        $sEchoStr = "";
-        $wxcpt = new WXBizMsgCrypt($token, $encodingAesKey, $corpId);
-        $errCode = $wxcpt->VerifyURL(
-            $sVerifyMsgSig,
-            $sVerifyTimeStamp,
-            $sVerifyNonce,
-            $sVerifyEchoStr,
-            $sEchoStr
-        );
-        echo !$errCode ? $sEchoStr : "ERR: " . $errCode . "\n\n";
         
     }
     
