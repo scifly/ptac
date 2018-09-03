0266600278b70dee091e265ab97becbd5b12c58e
Hao Yuhang <haoyh@yahoo.com>
2018-09-03 14:17:33 +0800

    refactor

--- app/Http/ViewComposers/CustodianIndexComposer.php
+++ app/Http/ViewComposers/CustodianIndexComposer.php
@@ -38,7 +38,9 @@ class CustodianIndexComposer {
             ],
             'batch'   => true,
             'filter'  => true,
-            'titles'  => ['#', '姓名', '头像', '性别', '学生', '邮箱', '手机号码', '创建于', '更新于', '状态 . 操作'],
+            'titles'  => [
+                '#', '姓名', '头像', '性别', '学生', '邮箱', '手机号码',
+                '创建于', '更新于', '同步状态', '关注状态', '状态 . 操作'],
             'grades'  => $grades,
             'classes' => $classes,
             'title'   => '导出监护人',
--- public/js/custodian/index.js
+++ public/js/custodian/index.js
@@ -1,6 +1,6 @@
 //# sourceURL=index.js
 page.index('custodians', [
-    {className: 'text-center', targets: [1, 2, 3, 4, 5, 6, 7, 8]}
+    {className: 'text-center', targets: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]}
 ]);
 
 /** 初始化监护人首页功能 */
