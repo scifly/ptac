4cfc936e67e2b8cc528390543137c299a08bb39c
Hao Yuhang <haoyh@yahoo.com>
2018-09-03 14:25:45 +0800

    refactor

--- public/js/custodian/index.js
+++ public/js/custodian/index.js
@@ -1,6 +1,9 @@
 //# sourceURL=index.js
 page.index('custodians', [
-    {className: 'text-center', targets: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]}
+    {className: 'text-center', targets: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]},
+    {className: 'searching_disabled', targets: [2]},
+    {searchable: false, targets: [2]},
+    {orderable: false, targets: [2]}
 ]);
 
 /** 初始化监护人首页功能 */
--- public/js/student/index.js
+++ public/js/student/index.js
@@ -1,8 +1,10 @@
 //# sourceURL=index.js
-page.index('students', [{
-    className: 'text-center',
-    targets: [1, 2, 3, 5, 6, 7, 8, 9, 10]
-}]);
+page.index('students', [
+    {className: 'text-center', targets: [1, 2, 3, 5, 6, 7, 8, 9, 10]},
+    {className: 'searching_disabled', targets: [2]},
+    {searchable: false, targets: [2]},
+    {orderable: false, targets: [2]}
+]);
 
 /** 初始化学籍首页功能 */
 $.getMultiScripts(['js/contact.js']).done(function () {
