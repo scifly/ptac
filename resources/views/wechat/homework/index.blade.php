5416ec2431e49c902c6f673adf32146e692ec893
Hao Yuhang <haoyh@yahoo.com>
2018-09-06 14:04:28 +0800

    test

--- resources/views/wechat/homework/index.blade.php
+++ resources/views/wechat/homework/index.blade.php
@@ -4,6 +4,6 @@
 @endsection
 @section('content')
     {!! Form::open(['method' => 'post', 'id' => 'formTest']) !!}
-    {!! Form::button('pay') !!}
+    {!! Form::button('pay', ['type' => 'submit']) !!}
     {!! Form::close() !!}
 @endsection
