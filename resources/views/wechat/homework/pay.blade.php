d4577632b075a2244a30a72d275b6a7c586b78ba
Hao Yuhang <haoyh@yahoo.com>
2018-09-07 10:45:13 +0800

    test

--- resources/views/wechat/homework/pay.blade.php
+++ resources/views/wechat/homework/pay.blade.php
@@ -4,51 +4,51 @@
  * 作者：沈唁
  * 博客：https://qq52o.me
  */
-$money= 1;                     //充值金额 微信支付单位为分
+$money = 1;                     //充值金额 微信支付单位为分
 $userip = Request::ip(); # get_client_ip();     //获得用户设备IP
-$appid  = "wwefd1c6553e218347";                  //应用APPID
+$appid = "wwefd1c6553e218347";                  //应用APPID
 $mch_id = "1226652702";                  //微信支付商户号
-$key    = "4372983891jkfdl43u2okjdkkdkfjkkk";                 //微信商户API密钥
-$out_trade_no = date('YmdHis').rand(1000,9999);//平台内部订单号
+$key = "4372983891jkfdl43u2okjdkkdkfjkkk";                 //微信商户API密钥
+$out_trade_no = date('YmdHis') . rand(1000, 9999);//平台内部订单号
 $nonce_str = createNoncestr();//随机字符串
 $body = "H5充值";//内容
 $total_fee = $money; //金额
 $spbill_create_ip = $userip; //IP
 $notify_url = "http://qq52o.me/wxpay/notify.php"; //回调地址
 $trade_type = 'MWEB';//交易类型 具体看API 里面有详细介绍
-$scene_info ='{"h5_info":{"type":"Wap","wap_url":"http://qq52o.me","wap_name":"支付"}}';//场景信息 必要参数
-$signA ="appid=$appid&attach=$out_trade_no&body=$body&mch_id=$mch_id&nonce_str=$nonce_str&notify_url=$notify_url&out_trade_no=$out_trade_no&scene_info=$scene_info&spbill_create_ip=$spbill_create_ip&total_fee=$total_fee&trade_type=$trade_type";
-$strSignTmp = $signA."&key=$key"; //拼接字符串  注意顺序微信有个测试网址 顺序按照他的来 直接点下面的校正测试 包括下面XML  是否正确
+$scene_info = '{"h5_info":{"type":"Wap","wap_url":"http://qq52o.me","wap_name":"支付"}}';//场景信息 必要参数
+$signA = "appid=$appid&attach=$out_trade_no&body=$body&mch_id=$mch_id&nonce_str=$nonce_str&notify_url=$notify_url&out_trade_no=$out_trade_no&scene_info=$scene_info&spbill_create_ip=$spbill_create_ip&total_fee=$total_fee&trade_type=$trade_type";
+$strSignTmp = $signA . "&key=$key"; //拼接字符串  注意顺序微信有个测试网址 顺序按照他的来 直接点下面的校正测试 包括下面XML  是否正确
 $sign = strtoupper(MD5($strSignTmp)); // MD5 后转换成大写
 $post_data = "<xml>
-                    <appid>$appid</appid>
-                    <mch_id>$mch_id</mch_id>
-                    <body>$body</body>
-                    <out_trade_no>$out_trade_no</out_trade_no>
-                    <total_fee>$total_fee</total_fee>
-                    <spbill_create_ip>$spbill_create_ip</spbill_create_ip>
-                    <notify_url>$notify_url</notify_url>
-                    <trade_type>$trade_type</trade_type>
-                    <scene_info>$scene_info</scene_info>
-                    <attach>$out_trade_no</attach>
-                    <nonce_str>$nonce_str</nonce_str>
-                    <sign>$sign</sign>
-            </xml>";//拼接成XML 格式
+    <appid>$appid</appid>
+    <mch_id>$mch_id</mch_id>
+    <body>$body</body>
+    <out_trade_no>$out_trade_no</out_trade_no>
+    <total_fee>$total_fee</total_fee>
+    <spbill_create_ip>$spbill_create_ip</spbill_create_ip>
+    <notify_url>$notify_url</notify_url>
+    <trade_type>$trade_type</trade_type>
+    <scene_info>$scene_info</scene_info>
+    <attach>$out_trade_no</attach>
+    <nonce_str>$nonce_str</nonce_str>
+    <sign>$sign</sign>
+</xml>";//拼接成XML 格式
 $url = "https://api.mch.weixin.qq.com/pay/unifiedorder";//微信传参地址
-$dataxml = postXmlCurl($post_data,$url); //后台POST微信传参地址  同时取得微信返回的参数
-        dd($dataxml);
+$dataxml = postXmlCurl($post_data, $url); //后台POST微信传参地址  同时取得微信返回的参数
 $objectxml = (array)simplexml_load_string($dataxml, 'SimpleXMLElement', LIBXML_NOCDATA); //将微信返回的XML 转换成数组
-        dd($objectxml);
+dd($objectxml);
 /**
  * @param int $length
  * @return string
  */
-function createNoncestr( $length = 32 ){
+function createNoncestr($length = 32) {
     $chars = "abcdefghijklmnopqrstuvwxyz0123456789";
-    $str ="";
-    for ( $i = 0; $i < $length; $i++ )  {
-        $str.= substr($chars, mt_rand(0, strlen($chars)-1), 1);
+    $str = "";
+    for ($i = 0; $i < $length; $i++) {
+        $str .= substr($chars, mt_rand(0, strlen($chars) - 1), 1);
     }
+
     return $str;
 }
 
@@ -58,13 +58,13 @@ function createNoncestr( $length = 32 ){
  * @param int $second
  * @return mixed
  */
-function postXmlCurl($xml,$url,$second = 30){
+function postXmlCurl($xml, $url, $second = 30) {
     $ch = curl_init();
     //设置超时
     curl_setopt($ch, CURLOPT_TIMEOUT, $second);
-    curl_setopt($ch,CURLOPT_URL, $url);
-    curl_setopt($ch,CURLOPT_SSL_VERIFYPEER,FALSE);
-    curl_setopt($ch,CURLOPT_SSL_VERIFYHOST,FALSE);
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, FALSE);
     //设置header
     curl_setopt($ch, CURLOPT_HEADER, FALSE);
     //要求结果为字符串且输出到屏幕上
@@ -72,17 +72,20 @@ function postXmlCurl($xml,$url,$second = 30){
     //post提交方式
     curl_setopt($ch, CURLOPT_POST, TRUE);
     curl_setopt($ch, CURLOPT_POSTFIELDS, $xml);
+    curl_setopt($ch, CURLOPT_ENCODING, 'UTF-8');
     //运行curl
     $data = curl_exec($ch);
     //返回结果
-    if($data){
+    if ($data) {
         curl_close($ch);
+
         return $data;
-    }else{
+    } else {
         $error = curl_errno($ch);
         curl_close($ch);
-        echo "curl出错，错误码:$error"."<br>";
+        echo "curl出错，错误码:$error" . "<br>";
     }
+
     return '';
 }
 
@@ -91,46 +94,49 @@ function postXmlCurl($xml,$url,$second = 30){
  * @return mixed
  */
 function get_client_ip($type = 0) {
-    $type       =  $type ? 1 : 0;
-    $ip         =   'unknown';
+    $type = $type ? 1 : 0;
+    $ip = 'unknown';
     if ($ip !== 'unknown') return $ip[$type];
-    if($_SERVER['HTTP_X_REAL_IP']){//nginx 代理模式下，获取客户端真实IP
-        $ip=$_SERVER['HTTP_X_REAL_IP'];
-    }elseif (isset($_SERVER['HTTP_CLIENT_IP'])) {//客户端的ip
-        $ip     =   $_SERVER['HTTP_CLIENT_IP'];
-    }elseif (isset($_SERVER['HTTP_X_FORWARDED_FOR'])) {//浏览当前页面的用户计算机的网关
-        $arr    =   explode(',', $_SERVER['HTTP_X_FORWARDED_FOR']);
-        $pos    =   array_search('unknown',$arr);
-        if(false !== $pos) unset($arr[$pos]);
-        $ip     =   trim($arr[0]);
-    }elseif (isset($_SERVER['REMOTE_ADDR'])) {
-        $ip     =   $_SERVER['REMOTE_ADDR'];//浏览当前页面的用户计算机的ip地址
-    }else{
-        $ip=$_SERVER['REMOTE_ADDR'];
+    if ($_SERVER['HTTP_X_REAL_IP']) {//nginx 代理模式下，获取客户端真实IP
+        $ip = $_SERVER['HTTP_X_REAL_IP'];
+    } elseif (isset($_SERVER['HTTP_CLIENT_IP'])) {//客户端的ip
+        $ip = $_SERVER['HTTP_CLIENT_IP'];
+    } elseif (isset($_SERVER['HTTP_X_FORWARDED_FOR'])) {//浏览当前页面的用户计算机的网关
+        $arr = explode(',', $_SERVER['HTTP_X_FORWARDED_FOR']);
+        $pos = array_search('unknown', $arr);
+        if (false !== $pos) unset($arr[$pos]);
+        $ip = trim($arr[0]);
+    } elseif (isset($_SERVER['REMOTE_ADDR'])) {
+        $ip = $_SERVER['REMOTE_ADDR'];//浏览当前页面的用户计算机的ip地址
+    } else {
+        $ip = $_SERVER['REMOTE_ADDR'];
     }
     // IP地址合法验证
-    $long = sprintf("%u",ip2long($ip));
-    $ip   = $long ? array($ip, $long) : array('0.0.0.0', 0);
+    $long = sprintf("%u", ip2long($ip));
+    $ip = $long ? array($ip, $long) : array('0.0.0.0', 0);
+
     return $ip[$type];
 }
 ?>
-<!DOCTYPE html>
+        <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <title>微信支付</title>
     <style type="text/css">
-        body{
+        body {
             font-family: "Microsoft YaHei", serif;
         }
-        .pay-box{
+
+        .pay-box {
             position: absolute;
             top: 50%;
             margin-top: -516px;
             left: 50%;
             margin-left: -320px;
         }
-        .ico{
+
+        .ico {
             width: 240px;
             height: 240px;
             border-radius: 120px;
@@ -140,28 +146,32 @@ function get_client_ip($type = 0) {
             font-size: 160px;
             line-height: 240px;
         }
-        .txt{
+
+        .txt {
             font-size: 42px;
             padding-top: 30px;
             color: #333;
         }
-        .val{
+
+        .val {
             font-size: 80px;
             font-weight: bold;
         }
-        .pay{
+
+        .pay {
             width: 640px;
             height: 100px;
             margin-top: 100px;
             padding: 20px;
             border-radius: 10px;
-            font-size:42px;
+            font-size: 42px;
             color: #fff;
             background: #07BF05;
             border: 0px;
             text-align: center;
         }
-        a{
+
+        a {
             color: #fff;
             background: transparent !important;
         }
@@ -176,10 +186,12 @@ function get_client_ip($type = 0) {
         支付金额
     </div>
     <div class="val">
-        ￥<span><?php echo $total_fee/100 ?></span>
+        ￥<span><?php echo $total_fee / 100 ?></span>
         <!-- 这里使用原生PHP echo输出需要支付的价格 -->
     </div>
-    <a class="pay" href="<?php echo $objectxml['mweb_url'] ?>"><button class="pay">确认支付</button></a>
+    <a class="pay" href="<?php echo $objectxml['mweb_url'] ?>">
+        <button class="pay">确认支付</button>
+    </a>
     <!-- 这里点击调起微信支付页面 mweb_url  -->
 </div>
 </body>
