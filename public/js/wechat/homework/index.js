e86b5e9a8b0275f47f043cfd3aa9561287293373
Hao Yuhang <haoyh@yahoo.com>
2018-09-10 13:32:34 +0800

    test

--- public/js/wechat/homework/index.js
+++ public/js/wechat/homework/index.js
@@ -1,61 +1,61 @@
-alert('wtf');
-// $('#pay').on('click', function () { callpay(); });
-//
-// function callpay() {
-//     if (typeof WeixinJSBridge === "undefined") {
-//         if (document.addEventListener) {
-//             document.addEventListener('WeixinJSBridgeReady', jsApiCall, false);
-//         } else if (document.attachEvent) {
-//             document.attachEvent('WeixinJSBridgeReady', jsApiCall);
-//             document.attachEvent('onWeixinJSBridgeReady', jsApiCall);
-//         }
-//     } else {
-//         jsApiCall();
-//     }
-// }
-// //调用微信JS api 支付
-// function jsApiCall() {
-//     WeixinJSBridge.invoke(
-//         'getBrandWCPayRequest',
-//         $('#params').val(),
-//         function (res) {
-//             WeixinJSBridge.log(res['err_msg']);
-//             alert(res['err_code'] + res['err_desc'] + res['err_msg']);
-//         }
-//     );
-// }
-// //获取共享地址
-// function editAddress() {
-//     alert($('#url').val());
-//     WeixinJSBridge.invoke(
-//         'editAddress',
-//         $('#url').val(),
-//         function (res) {
-//
-//             var value1 = res['proviceFirstStageName'],
-//                 value2 = res['addressCitySecondStageName'],
-//                 value3 = res['addressCountiesThirdStageName'],
-//                 value4 = res['addressDetailInfo'],
-//                 tel = res['telNumber'];
-//
-//             alert(value1);
-//             alert(value2);
-//             alert(value3);
-//             alert(value4);
-//             alert(tel);
-//             alert(value1 + value2 + value3 + value4 + ":" + tel);
-//         }
-//     );
-// }
-// window.onload = function () {
-//     if (typeof WeixinJSBridge === "undefined") {
-//         if (document.addEventListener) {
-//             document.addEventListener('WeixinJSBridgeReady', editAddress, false);
-//         } else if (document.attachEvent) {
-//             document.attachEvent('WeixinJSBridgeReady', editAddress);
-//             document.attachEvent('onWeixinJSBridgeReady', editAddress);
-//         }
-//     } else {
-//         editAddress();
-//     }
-// };
+$('#pay').on('click', function () { callpay(); });
+
+function callpay() {
+    if (typeof WeixinJSBridge === "undefined") {
+        if (document.addEventListener) {
+            document.addEventListener('WeixinJSBridgeReady', jsApiCall, false);
+        } else if (document.attachEvent) {
+            document.attachEvent('WeixinJSBridgeReady', jsApiCall);
+            document.attachEvent('onWeixinJSBridgeReady', jsApiCall);
+        }
+    } else {
+        jsApiCall();
+    }
+}
+//调用微信JS api 支付
+function jsApiCall() {
+    WeixinJSBridge.invoke(
+        'getBrandWCPayRequest',
+        $('#params').val(),
+        function (res) {
+            WeixinJSBridge.log(res['err_msg']);
+            alert(res['err_code'] + res['err_desc'] + res['err_msg']);
+        }
+    );
+}
+//获取共享地址
+function editAddress() {
+    alert($('#url').val());
+    WeixinJSBridge.invoke(
+        'editAddress',
+        $('#url').val(),
+        function (res) {
+
+            var value1 = res['proviceFirstStageName'],
+                value2 = res['addressCitySecondStageName'],
+                value3 = res['addressCountiesThirdStageName'],
+                value4 = res['addressDetailInfo'],
+                tel = res['telNumber'];
+
+            alert(value1);
+            alert(value2);
+            alert(value3);
+            alert(value4);
+            alert(tel);
+            alert(value1 + value2 + value3 + value4 + ":" + tel);
+        }
+    );
+}
+window.onload = function () {
+    alert('wtf');
+    if (typeof WeixinJSBridge === "undefined") {
+        if (document.addEventListener) {
+            document.addEventListener('WeixinJSBridgeReady', editAddress, false);
+        } else if (document.attachEvent) {
+            document.attachEvent('WeixinJSBridgeReady', editAddress);
+            document.attachEvent('onWeixinJSBridgeReady', editAddress);
+        }
+    } else {
+        editAddress();
+    }
+};
