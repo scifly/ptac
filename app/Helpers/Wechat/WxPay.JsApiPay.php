1133484cad8050d2ef2b80f818ee19ec16bad43c
Hao Yuhang <haoyh@yahoo.com>
2018-09-10 10:09:41 +0800

    test

--- app/Helpers/Wechat/WxPay.JsApiPay.php
+++ app/Helpers/Wechat/WxPay.JsApiPay.php
@@ -135,6 +135,7 @@ class JsApiPay {
         }
         //运行curl，结果以jason形式返回
         $res = curl_exec($ch);
+        Log::debug(json_encode($res));
         curl_close($ch);
         //取出openid
         $data = json_decode($res, true);
