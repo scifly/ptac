b4174b225418123dc275e745c10240d972d1bb08
Hao Yuhang <haoyh@yahoo.com>
2018-09-10 13:40:19 +0800

    test

--- public/js/wechat/homework/index.js
+++ public/js/wechat/homework/index.js
@@ -1,57 +1,57 @@
-$('#pay').on('click', function () { callpay(); });
-
-function callpay() {
-    if (typeof WeixinJSBridge === "undefined") {
-        if (document.addEventListener) {
-            document.addEventListener('WeixinJSBridgeReady', jsApiCall, false);
-        } else if (document.attachEvent) {
-            document.attachEvent('WeixinJSBridgeReady', jsApiCall);
-            document.attachEvent('onWeixinJSBridgeReady', jsApiCall);
-        }
-    } else {
-        jsApiCall();
-    }
-}
-//调用微信JS api 支付
-function jsApiCall() {
-    WeixinJSBridge.invoke(
-        'getBrandWCPayRequest',
-        $('#params').val(),
-        function (res) {
-            WeixinJSBridge.log(res['err_msg']);
-            alert(res['err_code'] + res['err_desc'] + res['err_msg']);
-        }
-    );
-}
-//获取共享地址
-function editAddress() {
-    WeixinJSBridge.invoke(
-        'editAddress',
-        $('#url').val(),
-        function (res) {
-
-            var value1 = res['proviceFirstStageName'],
-                value2 = res['addressCitySecondStageName'],
-                value3 = res['addressCountiesThirdStageName'],
-                value4 = res['addressDetailInfo'],
-                tel = res['telNumber'];
-
-            alert(value1 + value2 + value3 + value4 + ":" + tel);
-        }
-    );
-}
-$(document).on('WeixinJSBridgeReady',function(){
-    alert('wtf');
-    if (typeof WeixinJSBridge === "undefined") {
-        if (document.addEventListener) {
-            document.addEventListener('WeixinJSBridgeReady', editAddress, false);
-        } else if (document.attachEvent) {
-            document.attachEvent('WeixinJSBridgeReady', editAddress);
-            document.attachEvent('onWeixinJSBridgeReady', editAddress);
-        }
-    } else {
-        editAddress();
-    }
-});
-// window.onload = function () {
-// };
+// $('#pay').on('click', function () { callpay(); });
+//
+// function callpay() {
+//     if (typeof WeixinJSBridge === "undefined") {
+//         if (document.addEventListener) {
+//             document.addEventListener('WeixinJSBridgeReady', jsApiCall, false);
+//         } else if (document.attachEvent) {
+//             document.attachEvent('WeixinJSBridgeReady', jsApiCall);
+//             document.attachEvent('onWeixinJSBridgeReady', jsApiCall);
+//         }
+//     } else {
+//         jsApiCall();
+//     }
+// }
+// //调用微信JS api 支付
+// function jsApiCall() {
+//     WeixinJSBridge.invoke(
+//         'getBrandWCPayRequest',
+//         $('#params').val(),
+//         function (res) {
+//             WeixinJSBridge.log(res['err_msg']);
+//             alert(res['err_code'] + res['err_desc'] + res['err_msg']);
+//         }
+//     );
+// }
+// //获取共享地址
+// function editAddress() {
+//     WeixinJSBridge.invoke(
+//         'editAddress',
+//         $('#url').val(),
+//         function (res) {
+//
+//             var value1 = res['proviceFirstStageName'],
+//                 value2 = res['addressCitySecondStageName'],
+//                 value3 = res['addressCountiesThirdStageName'],
+//                 value4 = res['addressDetailInfo'],
+//                 tel = res['telNumber'];
+//
+//             alert(value1 + value2 + value3 + value4 + ":" + tel);
+//         }
+//     );
+// }
+// $(document).on('WeixinJSBridgeReady',function(){
+//     alert('wtf');
+//     if (typeof WeixinJSBridge === "undefined") {
+//         if (document.addEventListener) {
+//             document.addEventListener('WeixinJSBridgeReady', editAddress, false);
+//         } else if (document.attachEvent) {
+//             document.attachEvent('WeixinJSBridgeReady', editAddress);
+//             document.attachEvent('onWeixinJSBridgeReady', editAddress);
+//         }
+//     } else {
+//         editAddress();
+//     }
+// });
+// // window.onload = function () {
+// // };
