5f0ab52959a78bb61ec18cbdb41fc125edd0fd1a
haoyh <haoyh@yahoo.com>
2018-09-14 17:35:54 +0800

    test

--- public/js/home/page.js
+++ public/js/home/page.js
@@ -402,6 +402,9 @@ var page = {
                                 $this.html(title);
                             } else {
                                 $this.find('input').addClass($this.attr('class'));
+                                if (!$this.find('input').hasClass('input-sm')) {
+                                    $this.find('input').addClass('input-sm');
+                                }
                             }
                         }
                     });
