0b4c393f5256fa5b7c28e9aab0495d517e1b1bbc
haoyh <haoyh@yahoo.com>
2018-09-18 16:10:34 +0800

    test

--- public/js/dtrange.js
+++ public/js/dtrange.js
@@ -10,43 +10,34 @@
                         $.getScript(
                             page.siteRoot() + plugins.daterangepicker.js,
                             function () {
-                                if (typeof tp !== 'undefined') {
-                                    page.loadCss(plugins.timepicker.css);
-                                    $.getMultiScripts([plugins.timepicker.js]).done(
-                                        function () { dtrange.load(selector, tp); });
-                                } else {
-                                    dtrange.load(selector, tp);
-                                }
+                                var format = typeof tp === 'undefined'
+                                    ? "YYYY-MM-DD"
+                                    : "YYYY-MM-DD hh:mm:ss";
+                                page.loadCss(plugins.daterangepicker.css);
+                                $(typeof selector === 'undefined' ? '#daterange' : selector).daterangepicker({
+                                    autoUpdateInput: false,
+                                    locale: {
+                                        format: format,
+                                        timePicker: tp !== 'undefined',
+                                        separator: ' ~ ',
+                                        applyLabel: "确定",
+                                        cancelLabel: "取消",
+                                        fromLabel: "从",
+                                        toLabel: "到",
+                                        weekLabel: "W",
+                                        daysOfWeek: ["日", "一", "二", "三", "四", "五", "六"],
+                                        monthNames: [
+                                            "一月", "二月", "三月", "四月", "五月", "六月",
+                                            "七月", "八月", "九月", "十月", "十一月", "十二月"
+                                        ],
+                                        firstDay: 1
+                                    }
+                                });
                             }
                         );
                     }
                 );
             },
-            load: function (selector, tp) {
-                var format = typeof tp === 'undefined'
-                    ? "YYYY-MM-DD"
-                    : "YYYY-MM-DD hh:mm:ss";
-                page.loadCss(plugins.daterangepicker.css);
-                $(typeof selector === 'undefined' ? '#daterange' : selector).daterangepicker({
-                    autoUpdateInput: false,
-                    locale: {
-                        format: format,
-                        timepicker: tp !== 'undefined',
-                        separator: ' ~ ',
-                        applyLabel: "确定",
-                        cancelLabel: "取消",
-                        fromLabel: "从",
-                        toLabel: "到",
-                        weekLabel: "W",
-                        daysOfWeek: ["日", "一", "二", "三", "四", "五", "六"],
-                        monthNames: [
-                            "一月", "二月", "三月", "四月", "五月", "六月",
-                            "七月", "八月", "九月", "十月", "十一月", "十二月"
-                        ],
-                        firstDay: 1
-                    }
-                });
-            }
         };
 
         return { init: dtrange.init };
