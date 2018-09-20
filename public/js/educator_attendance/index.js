568e3cd18cec88a3a901f749814be54547b313ed
haoyh <haoyh@yahoo.com>
2018-09-20 11:24:40 +0800

    test

--- public/js/educator_attendance/index.js
+++ public/js/educator_attendance/index.js
@@ -1,5 +1,5 @@
 page.initDatatable('educator_attendances', [
-    {className: 'text-center', targets: [2, 3, 4, 5]}
+    {className: 'text-center', targets: [2, 3, 4]}
 ]);
 $.getMultiScripts(['js/dtrange.js']).done(
     function () {
