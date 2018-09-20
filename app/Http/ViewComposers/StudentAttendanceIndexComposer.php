81431d2e945a5289d8c96435d2ec781a973690fa
haoyh <haoyh@yahoo.com>
2018-09-20 11:06:13 +0800

    test

--- app/Http/ViewComposers/StudentAttendanceIndexComposer.php
+++ app/Http/ViewComposers/StudentAttendanceIndexComposer.php
@@ -1,6 +1,7 @@
 <?php
 namespace App\Http\ViewComposers;
 
+use App\Helpers\ModelTrait;
 use Illuminate\Contracts\View\View;
 
 /**
@@ -9,13 +10,34 @@ use Illuminate\Contracts\View\View;
  */
 class StudentAttendanceIndexComposer {
     
+    use ModelTrait;
+    
     /**
      * @param View $view
      */
     public function compose(View $view) {
         
         $view->with([
-            'titles'  => ['#', '姓名', '卡号', '打卡时间', '考勤时段', '考勤机', '进/出', '状态 . 操作'],
+            'titles'  => [
+                '#', '姓名', '卡号',
+                [
+                    'title' => '打卡时间',
+                    'html' => $this->inputDateTimeRange('打卡时间')
+                ],
+                '考勤时段', '考勤机',
+                [
+                    'title' => '进/出',
+                    'html' => $this->singleSelectList(
+                        [null => '全部', 0 => '进', 1 => '出'], 'filter_inorout'
+                    )
+                ],
+                [
+                    'title' => '状态 . 操作',
+                    'html' => $this->singleSelectList(
+                        [null => '全部', 0 => '异常', 1 => '正常'], 'filter_status'
+                    )
+                ],
+            ],
             'buttons' => [
                 'stat' => [
                     'id'    => 'stat',
--- public/js/student_attendance/index.js
+++ public/js/student_attendance/index.js
@@ -1,5 +1,10 @@
 page.initDatatable('student_attendances');
-
+$.getMultiScripts(['js/dtrange.js']).done(
+    function () {
+        $.dtrange().init('.dtrange');
+        page.initSelect2();
+    }
+);
 var $stat = $('#stat');
 $stat.on('click', function() {
     var $activeTabPane = $('#tab_' + page.getActiveTabId());
