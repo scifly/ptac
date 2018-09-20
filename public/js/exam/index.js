5b155759d54e63ba36e20b32f8452a02c2fd0fd3
haoyh <haoyh@yahoo.com>
2018-09-20 15:06:50 +0800

    test

--- public/js/exam/index.js
+++ public/js/exam/index.js
@@ -2,3 +2,10 @@ page.index('exams', [
     {className: 'text-center', targets: [1, 2, 5, 6, 7, 8]},
     {className: 'text-right', targets: [3, 4]},
 ]);
+$.getMultiScripts(['js/dtrange.js']).done(
+    function () {
+        $.dtrange().init('.dtrange');
+        $.dtrange().init('.drange');
+        page.initSelect2();
+    }
+);
