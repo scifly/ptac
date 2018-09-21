fecb60a00a9d3f8f5707133e8873c5f9523e752c
haoyh <haoyh@yahoo.com>
2018-09-21 09:51:36 +0800

    test

--- public/js/app/index.js
+++ public/js/app/index.js
@@ -104,8 +104,8 @@ $(document).off('click', '.fa-pencil').on(
 // 删除应用
 $(document).off('click', '.fa-remove').on(
     'click', '.fa-remove',
-    function () {
-        alert('wtf');
+    function (e) {
+        e.stopPropagation();
         id = $(this).parentsUntil('tbody').eq(2).children(0).first().html();
         $('#modal-dialog').modal({backdrop: true});
     }
