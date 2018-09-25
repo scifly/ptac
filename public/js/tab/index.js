43fcf9f57b4b1dc71a2f44d4a8a9d62b961b8b7b
haoyh <haoyh@yahoo.com>
2018-09-25 14:36:56 +0800

    test

--- public/js/tab/index.js
+++ public/js/tab/index.js
@@ -1,3 +1,7 @@
 page.index('tabs', [
     {className: 'text-center', targets: [2, 3, 5, 6, 7]}
 ]);
+page.initSelect2();
+$.getMultiScripts(['js/dtrange.js']).done(
+    function () { $.dtrange().init('.dtrange'); }
+);
