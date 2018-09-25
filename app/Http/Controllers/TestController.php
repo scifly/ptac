81b0d006c82d91b1e8c4f5fa0c39ee5c3aac11c4
haoyh <haoyh@yahoo.com>
2018-09-25 09:20:13 +0800

    test

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -57,7 +57,7 @@ class TestController extends Controller {
      */
     public function index() {
     
-        dd(Inflector::pluralize(Inflector::tableize('MessageCenter')));
+        dd(Inflector::pluralize(Inflector::tableize('WechatSms')));
         if (Request::method() == 'POST') {
             // $department = new Department;
             // $subs = $department->whereIn('id', $department->subDepartmentIds(33))->get()->toArray();
