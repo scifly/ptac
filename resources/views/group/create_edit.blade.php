1181347f102fe71e89308712ad7cf528fa6d993a
Hao Yuhang <haoyh@yahoo.com>
2018-10-08 13:52:25 +0800

    test

--- resources/views/group/create_edit.blade.php
+++ resources/views/group/create_edit.blade.php
@@ -5,7 +5,7 @@
     <div class="box-body">
         <div class="nav-tabs-custom">
             <ul class="nav nav-tabs pull-right">
-                <li><a href="#tab03" data-toggle="tab">卡片/功能权限</a></li>
+                <li><a href="#tab03" data-toggle="tab">控制器/功能权限</a></li>
                 <li><a href="#tab02" data-toggle="tab">菜单权限</a></li>
                 <li class="active"><a href="#tab01" data-toggle="tab">基本信息</a></li>
                 <li class="pull-left header"><i class="fa fa-th"></i>角色</li>
@@ -15,25 +15,21 @@
                 <div class="tab-pane active" id="tab01">
                     <div class="form-horizontal">
                         <!-- 角色ID -->
-                    @if (!empty($group['id']))
-                        {{ Form::hidden('id', $group['id'], ['id' => 'id']) }}
-                    @endif
-                    {{ Form::hidden(
-                        'menu_ids',
-                        $selectedMenuIds ?? null,
-                        ['id' => 'menu_ids']
-                    ) }}
-                    {{ Form::hidden(
-                        'tab_ids',
-                        !empty($selectedTabIds) ? implode(',', $selectedTabIds) : null,
-                        ['id' => 'tab_ids']
-                    ) }}
-                    {{ Form::hidden(
-                        'action_ids',
-                        !empty($selectedActionIds) ? implode(',', $selectedActionIds) : null,
-                        ['id' => 'action_ids']
-                    ) }}
-                    <!-- 角色名称 -->
+                        @if (!empty($group['id']))
+                            {{ Form::hidden('id', $group['id'], ['id' => 'id']) }}
+                        @endif
+                        {{ Form::hidden( 'menu_ids', $selectedMenuIds ?? null, ['id' => 'menu_ids'] ) }}
+                        {{ Form::hidden(
+                            'tab_ids',
+                            !empty($selectedTabIds) ? implode(',', $selectedTabIds) : null,
+                            ['id' => 'tab_ids']
+                        ) }}
+                        {{ Form::hidden(
+                            'action_ids',
+                            !empty($selectedActionIds) ? implode(',', $selectedActionIds) : null,
+                            ['id' => 'action_ids']
+                        ) }}
+                        <!-- 角色名称 -->
                         <div class="form-group">
                             {!! Form::label('name', '名称', [
                                 'class' => 'col-sm-3 control-label'
@@ -51,15 +47,15 @@
                             </div>
                         </div>
                         <!-- 角色所属学校 -->
-                    @include('partials.single_select', [
-                        'id' => 'school_id',
-                        'label' => '所属学校',
-                        'icon' => 'fa fa-university text-purple',
-                        'items' => $schools
-                    ])
-                    <!-- 角色备注 -->
-                    @include('partials.remark')
-                    <!-- 状态 -->
+                        @include('partials.single_select', [
+                            'id' => 'school_id',
+                            'label' => '所属学校',
+                            'icon' => 'fa fa-university text-purple',
+                            'items' => $schools
+                        ])
+                        <!-- 角色备注 -->
+                        @include('partials.remark')
+                        <!-- 状态 -->
                         @include('partials.enabled', [
                             'id' => 'enabled',
                             'value' => $group['enabled'] ?? null
@@ -70,7 +66,7 @@
                 <div class="tab-pane" id="tab02">
                     <div id="menu_tree" class="form-inline"></div>
                 </div>
-                <!-- 角色卡片/功能权限 -->
+                <!-- 角色控制器/功能权限 -->
                 <div class="tab-pane" id="tab03">
                     <div class="row">
                         @foreach ($tabActions as $ta)
@@ -84,7 +80,7 @@
                                                    class="minimal tabs"
                                                    @if (isset($selectedTabIds) && in_array($ta['tab']['id'], $selectedTabIds))
                                                    checked
-                                                   @endif
+                                                    @endif
                                             >&nbsp;
                                             <span style="margin-left: 5px; vertical-align: middle;">{{ $ta['tab']['name'] }}</span>
                                         </label>
@@ -108,7 +104,7 @@
                                                                    data-method="{{ $action['method'] }}"
                                                                    @if (isset($selectedActionIds) && in_array($action['id'], $selectedActionIds))
                                                                    checked
-                                                                   @endif
+                                                                    @endif
                                                             >&nbsp;
                                                             <span style="vertical-align: middle;">{{ $action['name'] }}</span>
                                                         </label>
