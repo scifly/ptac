9b0e7a51ec36e65266bd6cfba4e650ec14259e20
Hao Yuhang <haoyh@yahoo.com>
2018-10-08 10:54:41 +0800

    test

--- public/js/dtrange.js
+++ public/js/dtrange.js
@@ -11,31 +11,32 @@
                             page.siteRoot() + plugins.daterangepicker.js,
                             function () {
                                 var format = (selector === '.dtrange' ? "YYYY-MM-DD hh:mm:ss" : "YYYY-MM-DD"),
-                                    $picker = $(selector);
+                                    $picker = $(selector),
+                                    options = {
+                                        autoUpdateInput: false,
+                                        timePicker: selector === '.dtrange',
+                                        timePicker24Hour: true,
+                                        timePickerSeconds: true,
+                                        opens: 'left',
+                                        locale: {
+                                            format: format,
+                                            separator: ' ~ ',
+                                            applyLabel: "确定",
+                                            cancelLabel: "清除",
+                                            fromLabel: "从",
+                                            toLabel: "到",
+                                            weekLabel: "W",
+                                            daysOfWeek: ["日", "一", "二", "三", "四", "五", "六"],
+                                            monthNames: [
+                                                "一月", "二月", "三月", "四月", "五月", "六月",
+                                                "七月", "八月", "九月", "十月", "十一月", "十二月"
+                                            ],
+                                            firstDay: 1
+                                        }
+                                    };
 
                                 page.loadCss(plugins.daterangepicker.css);
-                                $picker.daterangepicker({
-                                    autoUpdateInput: false,
-                                    timePicker: selector === '.dtrange',
-                                    timePicker24Hour: true,
-                                    timePickerSeconds: true,
-                                    opens: 'left',
-                                    locale: {
-                                        format: format,
-                                        separator: ' ~ ',
-                                        applyLabel: "确定",
-                                        cancelLabel: "清除",
-                                        fromLabel: "从",
-                                        toLabel: "到",
-                                        weekLabel: "W",
-                                        daysOfWeek: ["日", "一", "二", "三", "四", "五", "六"],
-                                        monthNames: [
-                                            "一月", "二月", "三月", "四月", "五月", "六月",
-                                            "七月", "八月", "九月", "十月", "十一月", "十二月"
-                                        ],
-                                        firstDay: 1
-                                    }
-                                });
+                                $picker.daterangepicker(options);
                                 $picker.on('apply.daterangepicker', function(ev, picker) {
                                     var value = picker.startDate.format(format) + ' ~ ' + picker.endDate.format(format);
                                     $(this).val(value).trigger('change');
@@ -46,7 +47,8 @@
                                 });
 
                                 $picker.on('show.daterangepicker', function() {
-                                    $picker.daterangepicker('option', 'drops', 'up');
+                                    options['drops'] = 'up';
+                                    $picker.daterangepicker(options);
                                 });
                             }
                         );
