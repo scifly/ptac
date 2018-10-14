9fd04f6701acf0ccabe280a393608f38fb0ce89a
Hao Yuhang <haoyh@yahoo.com>
2018-10-14 10:03:23 +0800

    test

--- app/Http/ViewComposers/MenuSortComposer.php
+++ app/Http/ViewComposers/MenuSortComposer.php
@@ -4,6 +4,7 @@ namespace App\Http\ViewComposers;
 use App\Models\MenuTab;
 use App\Models\Tab;
 use Illuminate\Contracts\View\View;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Request;
 
 /**
@@ -21,9 +22,12 @@ class MenuSortComposer {
         $tabIds = MenuTab::whereMenuId($menuId)
             ->get()->sortBy('tab_order')
             ->pluck('tab_id')->toArray();
-        
+        $tabIds_ordered = implode(',', $tabIds);
+    
         $view->with([
-            'tabs'   => Tab::whereIn('id', $tabIds)->get(),
+            'tabs'   => Tab::whereIn('id', $tabIds)
+                ->orderByRaw(DB::raw("FIELD(id, $tabIds_ordered)"))
+                ->get(),
             'menuId' => $menuId,
         ]);
         
