a9caa857ec2d9c621c1e61d8fd9884af58b5fed7
Hao Yuhang <haoyh@yahoo.com>
2018-10-17 16:05:00 +0800

    test

--- app/Http/Controllers/Wechat/WechatController.php
+++ app/Http/Controllers/Wechat/WechatController.php
@@ -1,5 +1,7 @@
 <?php
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Wechat;
+
+use App\Http\Controllers\Controller;
 
 /**
  * 企业微信应用入口
@@ -9,11 +11,16 @@ namespace App\Http\Controllers;
  */
 class WechatController extends Controller {
     
+    static $category = 2;
+    
     /**
      * WechatController constructor.
      */
     function __construct() { }
     
+    /**
+     * 企业应用首页
+     */
     function index() {
     
     
