1727997f416c9d67d1990eb2696f9bee7cef34e0
Hao Yuhang <haoyh@yahoo.com>
2018-10-18 15:04:58 +0800

    test

--- public/js/module/index.js
+++ public/js/module/index.js
@@ -1,8 +1,5 @@
 var options = [
     {className: 'text-center', targets: [5, 6, 7, 8]},
-    {className: 'searching_disabled', targets: [5]},
-    {searchable: false, targets: [5]},
-    {orderable: false, targets: [5]}
 ];
 page.index('modules', options);
 page.initSelect2();
