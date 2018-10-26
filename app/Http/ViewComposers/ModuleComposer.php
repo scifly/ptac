4b328020f842f64ac546a7a2d01ea74f3bdbdec0
Hao Yuhang <haoyh@yahoo.com>
2018-10-26 11:36:38 +0800

    test

--- app/Http/ViewComposers/ModuleComposer.php
+++ app/Http/ViewComposers/ModuleComposer.php
@@ -42,7 +42,7 @@ class ModuleComposer {
                 break;
         }
         $groups = [null => '公用'] + Group::whereIn('name', ['监护人', '教职员工'])->pluck('name', 'id')->toArray();
-        $tabs = [null => '请选择'] + Tab::where(['enabled' => 1, 'category' => 1])->pluck('comment', 'id')->toArray();
+        $tabs = [null => ''] + Tab::where(['enabled' => 1, 'category' => 1])->pluck('comment', 'id')->toArray();
         if (Route::has('id')) {
             $media = Module::find(Request::route('id'))->media;
         }
