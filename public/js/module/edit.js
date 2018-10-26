ee7dc34ded2d5bb07cb5ba73790b241b3c9166af
Hao Yuhang <haoyh@yahoo.com>
2018-10-26 15:52:05 +0800

    test

--- public/js/module/edit.js
+++ public/js/module/edit.js
@@ -1,8 +1,9 @@
 var table = 'modules';
 page.loadCss('css/upload.css');
 page.edit('formModule', table);
-$.getMultiScripts(['js/shared/upload.js']).done(
+$.getMultiScripts(['js/shared/upload.js', 'js/module/module.js']).done(
     function () {
         $.upload().init('edit', table, '应用模块');
+        $.module().action(table, 'edit');
     }
 );
