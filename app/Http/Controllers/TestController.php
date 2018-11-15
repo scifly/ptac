7c0fcf7501f82411eb0958c3d29dcaf4bc401ae0
Hao Yuhang <haoyh@yahoo.com>
2018-11-15 10:17:44 +0800

    test

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -49,7 +49,7 @@ class TestController extends Controller {
      */
     public function index() {
     
-        $dir = public_path() . '/uploads/' . date('Y/m/d/13333');
+        $dir = public_path() . '/uploads/' . date('Y/m/d13333/');
         if (!file_exists($dir)) {
             mkdir($dir, 0777, true);
         }
