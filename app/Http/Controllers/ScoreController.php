7e66f288320cf8a21fe674d10a1b9e78832cf452
Hao Yuhang <haoyh@yahoo.com>
2018-12-29 11:28:57 +0800

    test

--- app/Http/Controllers/ScoreController.php
+++ app/Http/Controllers/ScoreController.php
@@ -2,8 +2,7 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\ScoreRequest;
-use App\Models\Exam;
-use App\Models\Score;
+use App\Models\{Exam, Score};
 use Exception;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Support\Facades\Request;
