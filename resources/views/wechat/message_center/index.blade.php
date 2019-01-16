53013901a54d94f52f49adee068e5f91f06c65d8
Hao Yuhang <haoyh@yahoo.com>
2019-01-16 14:41:22 +0800

    test

--- resources/views/wechat/message_center/index.blade.php
+++ resources/views/wechat/message_center/index.blade.php
@@ -37,14 +37,18 @@
             </div>
             <div class="modal-content">
                 <div class="weui-cells weui-cells_form">
-                    <div class="weui-cell weui-cell_select">
-                        <div class="weui-cell__hd">消息类型</div>
+                    <div class="weui-cell weui-cell_select weui-cell_select-after">
+                        <div class="weui-cell__hd">
+                            {!! Form::label('message_type', '消息类型', ['class' => 'weui-label']) !!}
+                        </div>
                         <div class="weui-cell__bd">
                             {!! Form::select('message_type', $messageTypes, ['class' => 'weui-select']) !!}
                         </div>
                     </div>
-                    <div class="weui-cell weui-cell_select">
-                        <div class="weui-cell__hd">消息格式</div>
+                    <div class="weui-cell weui-cell_select weui-cell_select-after">
+                        <div class="weui-cell__hd">
+                            {!! Form::label('message_type', '消息格式', ['class' => 'weui-label']) !!}
+                        </div>
                         <div class="weui-cell__bd">
                             {!! Form::select('media_type', $mediaTypes, ['class' => 'weui-select']) !!}
                         </div>
