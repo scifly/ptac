8134d7023ea471ebd7cf38c9f3210986004c7387
Hao Yuhang <haoyh@yahoo.com>
2019-01-16 13:32:21 +0800

    test

--- resources/views/wechat/message_center/index.blade.php
+++ resources/views/wechat/message_center/index.blade.php
@@ -4,31 +4,24 @@
     <link rel="stylesheet" href="{!! asset('/css/wechat/message_center/index.css') !!}">
 @endsection
 @section('content')
-    <div class="weui-navbar">
-        <div class="weui-navbar__item weui_bar__item_on">
-            <div class="weui-cells weui-cells_form">
-                <div class="weui-cell">
-                    <div class="weui-cell__hd" style="text-align: left;">
-                        <img alt="" src="{!! asset("img/nav.png") !!}" style="width: 16px;"/>
-                    </div>
-                    <div class="weui-cell__bd">
-                        {!! Form::text('search', null, [
-                            'id' => 'search',
-                            'placeholder' => '搜索消息',
-                            'class' => 'weui-input'
-                        ]) !!}
-                    </div>
-                    <div class="weui-cell__ft">
-                        <a class="icon iconfont icon-add c-green"
-                           href="{!! url($acronym . '/message_centers/create') !!}"
-                        ></a>
-                    </div>
-                </div>
+    <div class="weui-cells weui-cells_form">
+        <div class="weui-cell">
+            <div class="weui-cell__hd" style="text-align: left;">
+                <img alt="" src="{!! asset("img/nav.png") !!}" style="width: 16px;"/>
+            </div>
+            <div class="weui-cell__bd">
+                {!! Form::text('search', null, [
+                    'id' => 'search',
+                    'placeholder' => '搜索消息',
+                    'class' => 'weui-input'
+                ]) !!}
+            </div>
+            <div class="weui-cell__ft">
+                <a class="icon iconfont icon-add c-green"
+                   href="{!! url($acronym . '/message_centers/create') !!}"
+                ></a>
             </div>
         </div>
-    </div>
-    <div class="weui-tab">
-
     </div>
 @endsection
 @section('script')
