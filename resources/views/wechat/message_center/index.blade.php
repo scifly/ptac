37628f5da43e523cc81a9c34d45a5910a96412c0
Hao Yuhang <haoyh@yahoo.com>
2019-01-16 13:53:06 +0800

    test

--- resources/views/wechat/message_center/index.blade.php
+++ resources/views/wechat/message_center/index.blade.php
@@ -26,21 +26,6 @@
             </div>
         </div>
     </div>
-    <div class="weui-mask weui-actions_mask weui-mask--visible"></div>
-    <div class="weui-actionsheet  weui-actionsheet_toggle" id="weui-actionsheet">
-        <div class="weui-actionsheet__title">
-            <p class="weui-actionsheet__title-text">选择操作</p>
-        </div>
-        <div class="weui-actionsheet__menu">
-            <div class="weui-actionsheet__cell color-primary">收件箱</div>
-            <div class="weui-actionsheet__cell color-warning">发件箱</div>
-            <div class="weui-actionsheet__cell color-danger">按消息类型过滤</div>
-            <div class="weui-actionsheet__cell color-danger">按消息格式过滤</div>
-        </div>
-        <div class="weui-actionsheet__action">
-            <div class="weui-actionsheet__cell weui-actionsheet_cancel">取消</div>
-        </div>
-    </div>
 @endsection
 @section('script')
     <script src="{!! asset('/js/wechat/message_center/index.js') !!}"></script>
