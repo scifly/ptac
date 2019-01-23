47d36e8553ccd51712766928784de1220967b82d
Hao Yuhang <haoyh@yahoo.com>
2019-01-23 16:17:54 +0800

    test

--- app/Jobs/SendMessage.php
+++ app/Jobs/SendMessage.php
@@ -52,18 +52,13 @@ class SendMessage implements ShouldQueue {
         
         try {
             DB::transaction(function () {
-                $results = array_map(
-                    function (Message $message) {
-                        return $this->send($message, $this->response);
-                    }, $this->messages
+                $results = array_collapse(
+                    array_map(
+                        function (Message $message) {
+                            return $this->send($message, $this->response);
+                        }, $this->messages
+                    )
                 );
-                // $results = array_collapse(
-                //     array_map(
-                //         function (Message $message) {
-                //             return $this->send($message, $this->response);
-                //         }, $this->messages
-                //     )
-                // );
                 Log::debug(json_encode($results));
                 list($code, $msg) = $this->inform($results);
                 $this->response['statusCode'] = $code;
