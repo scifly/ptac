fe987ebe06986d3dea4c31235ecbdad457023ed7
Hao Yuhang <haoyh@yahoo.com>
2019-01-23 16:55:40 +0800

    test

--- app/Models/Score.php
+++ app/Models/Score.php
@@ -464,7 +464,7 @@ class Score extends Model {
                 $data[] = array_merge(
                     $basic, [
                         'subject_id' => $subject->id,
-                        'score'      => intval($record[$index]),
+                        'score'      => floatval($record[$index]),
                     ]
                 );
                 $index = chr(ord($index) + 1);
