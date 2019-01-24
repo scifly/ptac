ed5bcdf40588aeed26bb9ff10af59aa1e42c2815
Hao Yuhang <haoyh@yahoo.com>
2019-01-24 13:35:24 +0800

    test

---
+++ app/Models/Operator.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: scifly
+ * Date: 2019-1-24
+ * Time: 13:31
+ */
+namespace App\Helpers;
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * Class Operator
+ * @package App\Helpers
+ */
+class Operator extends Model {
+
+
+
+}
