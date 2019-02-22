4de37c01201e342fa4a548635ed01ef3e80de7b3
Hao Yuhang <haoyh@yahoo.com>
2019-02-22 09:49:04 +0800

    test

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -49,30 +49,70 @@ class TestController extends Controller {
      */
     public function index() {
     
-        $server = "120.78.55.152";
-        $port = 9933;
-        if (!($sock = socket_create(AF_INET, SOCK_DGRAM, 0))) {
-            $errcode = socket_last_error();
-            $errmsg = socket_strerror($errcode);
-            die ("Couldn't create socket: [$errcode] $errmsg\n");
+        // $server = "120.78.55.152";
+        // $port = 9933;
+        // if (!($sock = socket_create(AF_INET, SOCK_DGRAM, 0))) {
+        //     $errcode = socket_last_error();
+        //     $errmsg = socket_strerror($errcode);
+        //     die ("Couldn't create socket: [$errcode] $errmsg\n");
+        // }
+        // echo "Socket created \n";
+        // $input = 'abcdefg';
+        // if( ! socket_sendto($sock, $input , strlen($input) , 0 , $server , $port)) {
+        //     $errorcode = socket_last_error();
+        //     $errormsg = socket_strerror($errorcode);
+        //
+        //     die("Could not send data: [$errorcode] $errormsg \n");
+        // }
+        // // # Now receive reply from server and print it
+        // if(socket_recv($sock , $reply , 2045 , MSG_PEEK ) === FALSE) {
+        //     $errorcode = socket_last_error();
+        //     $errormsg = socket_strerror($errorcode);
+        //
+        //     die("Could not receive data: [$errorcode] $errormsg \n");
+        // }
+        //
+        // echo "Reply : $reply";
+        error_reporting(~E_WARNING);
+        $server = '127.0.0.1';
+        $port = 9999;
+    
+        if(!($sock = socket_create(AF_INET, SOCK_DGRAM, 0)))
+        {
+            $errorcode = socket_last_error();
+            $errormsg = socket_strerror($errorcode);
+        
+            die("Couldn't create socket: [$errorcode] $errormsg \n");
         }
+    
         echo "Socket created \n";
-        $input = 'abcdefg';
-        if( ! socket_sendto($sock, $input , strlen($input) , 0 , $server , $port)) {
-            $errorcode = socket_last_error();
-            $errormsg = socket_strerror($errorcode);
+    
+        while(1)
+        {
+//Take some input to send
+            echo 'Enter a message to send : ';
+            $input = fgets(STDIN);
+
+//Send the message to the server
+            if( ! socket_sendto($sock, $input , strlen($input) , 0 , $server , $port))
+            {
+                $errorcode = socket_last_error();
+                $errormsg = socket_strerror($errorcode);
+            
+                die("Could not send data: [$errorcode] $errormsg \n");
+            }
+
+//Now receive reply from server and print it
+            if(socket_recv ( $sock , $reply , 2045 , MSG_PEEK ) === FALSE)
+            {
+                $errorcode = socket_last_error();
+                $errormsg = socket_strerror($errorcode);
+            
+                die("Could not receive data: [$errorcode] $errormsg \n");
+            }
         
-            die("Could not send data: [$errorcode] $errormsg \n");
+            echo "Reply : $reply";
         }
-        // # Now receive reply from server and print it
-        if(socket_recv($sock , $reply , 2045 , MSG_PEEK ) === FALSE) {
-            $errorcode = socket_last_error();
-            $errormsg = socket_strerror($errorcode);
-        
-            die("Could not receive data: [$errorcode] $errormsg \n");
-        }
-        
-        echo "Reply : $reply";
         
     }
     
