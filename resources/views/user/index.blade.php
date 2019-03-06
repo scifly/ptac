6904b1644bbaa219b4ed3c251684094f2a81c83f
Hao Yuhang <haoyh@yahoo.com>
2019-03-06 14:05:08 +0800

    test

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -24,7 +24,7 @@
     var $inputs = $('.sn'),
     i = 0;
 
-    $('input').on('change', function() {
+    $('input').on('change keyup paste', function() {
         i++;
         $($inputs[i]).focus();
     });
