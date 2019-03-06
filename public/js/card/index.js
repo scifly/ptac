3e719df7ca634080bfe3b20d14466e496b49b816
Hao Yuhang <haoyh@yahoo.com>
2019-03-06 16:52:53 +0800

    test

--- public/js/card/index.js
+++ public/js/card/index.js
@@ -1,3 +1,9 @@
 page.index('cards', [
     {className: 'text-center', targets: [1, 2, 3, 4, 5, 6]},
 ]);
+$.getMultiScripts(['js/shared/dtrange.js']).done(
+    function () {
+        $.dtrange().init('.dtrange');
+        // $.dtrange().init('.drange');
+    }
+);
