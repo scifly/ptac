f58d924e70aacd11a4a65461fea72a84362a4d87
Hao Yuhang <haoyh@yahoo.com>
2019-03-08 14:11:37 +0800

    test

--- app/Models/Card.php
+++ app/Models/Card.php
@@ -94,9 +94,10 @@ class Card extends Model {
                         $colors[$d ?? 0][0], $colors[$d ?? 0][1]
                     );
                     
-                    return Datatable::status($status, $row, false);
+                    return $row['card_id'] ? Datatable::status($status, $row, false) : $status;
                 },
             ],
+            ['db' => 'User.card_id', 'dt' => 8]
         ];
         $joins = [
             [
