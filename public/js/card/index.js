aeb5ea846503a9dc45b3546628c7ff6a153ddc53
Hao Yuhang <haoyh@yahoo.com>
2019-03-08 14:43:58 +0800

    test

--- public/js/card/index.js
+++ public/js/card/index.js
@@ -1,6 +1,9 @@
 page.index('cards', [
     'create,edit',
-    {className: 'text-center', targets: [1, 2, 3, 4, 5, 6]}
+    {className: 'text-center', targets: [1, 2, 3, 4, 5, 6]},
+    {className: 'searching_disabled', targets: [3]},
+    {searchable: false, targets: [3]},
+    {orderable: false, targets: [3]}
 ]);
 $.getMultiScripts(['js/shared/dtrange.js']).done(
     function () { $.dtrange().init('.dtrange'); }
