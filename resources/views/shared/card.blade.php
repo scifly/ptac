843026e6c5749db4179f460659e907d14a9f6346
Hao Yuhang <haoyh@yahoo.com>
2019-03-08 16:04:29 +0800

    test

--- resources/views/shared/card.blade.php
+++ resources/views/shared/card.blade.php
@@ -4,10 +4,8 @@
         'class' => 'col-sm-3 control-label'
     ]) !!}
     <div class="col-sm-6">
-        <div class="input-group" style="width: 100%;">
-            <div class="input-group-addon" style="width: 45px;">
-                <strong>卡</strong>
-            </div>
+        <div class="input-group">
+            @include('shared.icon_addon', ['class' => 'fa-credit-card'])
             {!! Form::text('card[sn]', null, [
                 'class' => 'form-control text-blue',
                 'placeholder' => '(可选)',
