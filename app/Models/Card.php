6ee652cefddb3881d6fb1486e1bc15f118146d66
Hao Yuhang <haoyh@yahoo.com>
2019-03-11 16:50:24 +0800

    test

--- app/Models/Card.php
+++ app/Models/Card.php
@@ -282,19 +282,20 @@ class Card extends Model {
      */
     function issue() {
     
-        $sns = Request::input('sns');
-        $ns = array_count_values(array_map('strval', array_values($sns)));
-        foreach ($ns as $n => $count) {
-            if (!empty($n) && $count > 1) $ds[] = $n;
+        if ($sns = Request::input('sns')) {
+            $ns = array_count_values(array_map('strval', array_values($sns)));
+            foreach ($ns as $n => $count) {
+                if (!empty($n) && $count > 1) $ds[] = $n;
+            }
+            abort_if(
+                !empty($ds ?? []),
+                HttpStatusCode::NOT_ACCEPTABLE,
+                implode('', [
+                    (!empty($sns) ? ('卡号: ' . implode(',', $ds ?? [])) : ''),
+                    '有重复，请检查后重试'
+                ])
+            );
         }
-        abort_if(
-            !empty($ds ?? []),
-            HttpStatusCode::NOT_ACCEPTABLE,
-            implode('', [
-                (!empty($sns) ? ('卡号: ' . implode(',', $ds ?? [])) : ''),
-                '有重复，请检查后重试'
-            ])
-        );
         try {
             DB::transaction(function () use ($sns) {
                 foreach ($sns as $userId => $sn) {
