c3d8b1f1afe307ab26bfe3890958ed0d61b0c8b0
Hao Yuhang <haoyh@yahoo.com>
2019-03-20 16:50:53 +0800

    test

--- public/js/passage_rule/create.js
+++ public/js/passage_rule/create.js
@@ -1,5 +1,7 @@
 $.getMultiScripts(['js/shared/dtrange.js']).done(
     function () {
+        page.initICheck();
+        page.initSelect2();
         $.dtrange().tRange();
         $.dtrange().dRange('.drange');
     }
