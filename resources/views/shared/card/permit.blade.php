916c3766694c3bbdd1ace65edc2628c7eeae184d
Hao Yuhang <haoyh@yahoo.com>
2019-04-03 10:10:36 +0800

    test

--- resources/views/shared/card/permit.blade.php
+++ resources/views/shared/card/permit.blade.php
@@ -10,54 +10,57 @@
     <div class="box-body">
         <div class="row">
             <div class="col-md-3">
-                <div>
-                    <div class="form-group">
-                        {!! Form::label('section_id', '部门') !!}
-                        {!! Form::select('section_id', $sections, null, [
-                            'class' => 'form-control select2',
-                            'style' => 'width: 100%;',
-                            'disabled' => sizeof($sections) <= 1
-                        ]) !!}
-                    </div>
-                    <div class="form-group">
-                        {!! Form::label('user_ids', '一卡通列表') !!}
-                        <div>
-                            <table style="width: 100%"
-                                   class="display nowrap table table-striped table-bordered table-hover table-condensed">
-                                <thead>
-                                <tr>
-                                    @foreach (['姓名', '卡号', '授权'] as $title)
-                                        <th style="vertical-align: middle;" class="text-center">
-                                            {!! $title !!}
-                                        </th>
-                                    @endforeach
-                                </tr>
-                                </thead>
-                                <tbody>
-                                <tr>
-                                    <td colspan="3" class="text-center">
-                                        - 请选择一个部门进行一卡通批量授权 -
-                                    </td>
-                                </tr>
-                                </tbody>
-                            </table>
-                        </div>
+                <div class="form-group">
+                    {!! Form::label('section_id', '部门') !!}
+                    {!! Form::select('section_id', $sections, null, [
+                        'class' => 'form-control select2',
+                        'style' => 'width: 100%;',
+                        'disabled' => sizeof($sections) <= 1
+                    ]) !!}
+                </div>
+                <div class="form-group">
+                    {!! Form::label('user_ids', '一卡通列表') !!}
+                    <div>
+                        <table style="width: 100%"
+                               class="display nowrap table table-striped table-bordered table-hover table-condensed">
+                            <thead>
+                            <tr>
+                                @foreach (['姓名', '卡号', '授权'] as $title)
+                                    <th style="vertical-align: middle;" class="text-center">
+                                        {!! $title !!}
+                                    </th>
+                                @endforeach
+                            </tr>
+                            </thead>
+                            <tbody>
+                            <tr>
+                                <td colspan="3" class="text-center">
+                                    - 请选择一个部门进行一卡通批量授权 -
+                                </td>
+                            </tr>
+                            </tbody>
+                        </table>
                     </div>
                 </div>
             </div>
             <div class="col-md-9">
-                <table class="display nowrap table table-striped table-bordered table-hover table-condensed">
-                    <tbody>
-                    <tr>
-                        @foreach (['#', '门禁', 'No.1', 'No.2', 'No.3', 'No.4'] as $title)
-                            <th class="text-center" style="vertical-align: middle">
-                                {!! $title !!}
-                            </th>
-                        @endforeach
-                    </tr>
-                    {!! $turnstiles !!}
-                    </tbody>
-                </table>
+                <div class="form-group">
+                    {!! Form::label('user_ids', '门禁列表') !!}
+                    <div>
+                        <table class="display nowrap table table-striped table-bordered table-hover table-condensed">
+                            <tbody>
+                            <tr>
+                                @foreach (['#', '门禁', 'No.1', 'No.2', 'No.3', 'No.4'] as $title)
+                                    <th class="text-center" style="vertical-align: middle">
+                                        {!! $title !!}
+                                    </th>
+                                @endforeach
+                            </tr>
+                            {!! $turnstiles !!}
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
