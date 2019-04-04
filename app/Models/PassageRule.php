b2b72147bcdd9876096918a2bac3baf76d721b16
Han zhenzhen <1853010114@qq.com>
2019-04-04 16:10:20 +0800

    test

--- app/Models/PassageRule.php
+++ app/Models/PassageRule.php
@@ -296,6 +296,7 @@ class PassageRule extends Model {
                 }
             }
             Log::info('rules', $rules);
+            Log::info('deviceids',$devices->pluck('deviceid')->toArray());
             array_map(
                 function ($api, $data) { (new Turnstile)->invoke($api, ['data' => $data]); },
                 ['clrtimeframes', 'settimeframes'],
