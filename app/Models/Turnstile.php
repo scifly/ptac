ab9694306b3eb23d71af5bc0cd5e50a6eb7c5eec
Han zhenzhen <1853010114@qq.com>
2019-04-04 14:22:01 +0800

    test

--- app/Models/Turnstile.php
+++ app/Models/Turnstile.php
@@ -10,6 +10,7 @@ use Exception;
 use GuzzleHttp\Client;
 use Illuminate\Database\Eloquent\{Builder, Collection, Model, Relations\BelongsTo, Relations\BelongsToMany};
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
 use Throwable;
 
 /**
@@ -204,6 +205,7 @@ class Turnstile extends Model {
                     'form-params' => $params,
                 ]
             );
+            Log::debug(json_encode($response));
             $body = json_decode($response->getBody(), true);
             $status = $response->getHeader('status');
             throw_if(
