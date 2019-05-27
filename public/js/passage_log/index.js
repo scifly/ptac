04f52539a10a6b7e9fbb9db86271baad296369f4
Hao Yuhang <haoyh@yahoo.com>
2019-05-27 15:54:59 +0800

    test

--- public/js/passage_log/index.js
+++ public/js/passage_log/index.js
@@ -3,6 +3,9 @@ page.index('passage_logs', [
     { className: 'text-center', targets: [1, 2, 3, 4, 6, 7]}
 ]);
 page.initSelect2();
+$.getMultiScripts(['js/shared/dtrange.js']).done(
+    function () { $.dtrange().dRange('.dtrange'); }
+);
 // 采集门禁通行记录
 $('#store').on('click', function () {
     $('.overlay').show();
