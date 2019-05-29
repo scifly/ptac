79e31ca58b8941f8c115ad52e52c3d09168d7090
Hao Yuhang <haoyh@yahoo.com>
2019-05-29 11:18:22 +0800

    test

--- app/Http/ViewComposers/CardComposer.php
+++ app/Http/ViewComposers/CardComposer.php
@@ -66,10 +66,7 @@ class CardComposer {
                 $ids = session('ids');
             }
             $operator = $action == 'create' ? '=' : '<>';
-            $users = User::whereIn('id', $ids)
-                ->where('card_id', $operator, 0)
-                ->orWhere('card_id', $operator, null)
-                ->get();
+            $users = User::whereIn('id', $ids)->where('card_id', $operator, null)->get();
             $card = new Card;
             $sn = $card->input();
             $row = <<<HTML
