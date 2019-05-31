03f54b5b12c2cd61ccc61b95b7f3e788f68913fa
Hao Yuhang <haoyh@yahoo.com>
2019-05-31 16:48:28 +0800

    test

--- app/Models/Card.php
+++ app/Models/Card.php
@@ -429,7 +429,8 @@ class Card extends Model {
                     } else {
                         $user = User::find($userId);
                         if ($user->card) {
-                            $user->card->delete();
+                            Request::merge(['ids' => [$userId]]);
+                            $user->card->remove();
                             $user->update(['card_id' => 0]);
                         }
                     }
