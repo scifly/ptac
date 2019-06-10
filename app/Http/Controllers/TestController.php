7bfefd9581e681eaf3faa66e4721e5012c9ddd35
Hao Yuhang <haoyh@yahoo.com>
2019-06-10 15:22:15 +0800

    test

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -56,6 +56,7 @@ class TestController extends Controller {
      */
     public function index() {
     
+        echo 'abc';
         phpinfo();
         exit;
         dd(ucfirst(Inflector::camelize('passage_rule')));
