62a5fca1bb0f739d9505246f8a9694e1f4580d33
Hao Yuhang <haoyh@yahoo.com>
2019-07-09 13:44:03 +0800

    test

--- resources/views/layouts/web.blade.php
+++ resources/views/layouts/web.blade.php
@@ -9,7 +9,8 @@
     <title>{!! config('app.name') !!}</title>
     <!-- Tell the browser to be responsive to screen width -->
     <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
-    <link rel="stylesheet" href="{{ URL::asset('css/bootstrap.min.css') }}">
+    {!! Html::style('css/bootstrap.min.css')->toHtml() !!}
+{{--    <link rel="stylesheet" href="{{ URL::asset('css/bootstrap.min.css') }}">--}}
     {{--<link rel="stylesheet" href="{{ URL::asset('css/app.css') }}">--}}
     <link rel="stylesheet" href="{{ URL::asset('css/font-awesome.min.css') }}">
     <link rel="stylesheet" href="{{ URL::asset('css/ionicons.min.css') }}">
