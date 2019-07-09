94c7ac0c9958bf4f53f7e963d8029df5910fb3bd
Hao Yuhang <haoyh@yahoo.com>
2019-07-09 14:03:20 +0800

    test

--- resources/views/layouts/web.blade.php
+++ resources/views/layouts/web.blade.php
@@ -9,22 +9,23 @@
     <title>{!! config('app.name') !!}</title>
     <!-- Tell the browser to be responsive to screen width -->
     <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
-    {!! Html::style('css/bootstrap.min.css')->toHtml() !!}
-    {!! Html::style('css/font-awesome.min.css')->toHtml() !!}
-    {!! Html::style('css/ionicons.min.css')->toHtml() !!}
-    {!! Html::style('js/plugins/gritter/css/jquery.gritter.css', ['id' => 'cip'])->toHtml() !!}
-    {!! Html::style('js/plugins/parsley/parsley.css')->toHtml() !!}
-    {!! Html::style('css/AdminLTE.min.css')->toHtml() !!}
-    {!! Html::style('css/skins/_all-skins.min.css')->toHtml() !!}
+    {!! Html::style('css/bootstrap.min.css') !!}
+    {!! Html::style('css/font-awesome.min.css') !!}
+    {!! Html::style('css/ionicons.min.css') !!}
+    {!! Html::style('js/plugins/gritter/css/jquery.gritter.css', ['id' => 'cip']) !!}
+    {!! Html::style('js/plugins/parsley/parsley.css') !!}
+    {!! Html::style('css/AdminLTE.min.css') !!}
+    {!! Html::style('css/skins/_all-skins.min.css') !!}
     {!! Html::style('css/page.css') !!}
-<link rel="shortcut icon" href="{{ URL::asset('favicon.ico') }}">
-<!--[if lt IE 9]>
-<script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
-<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-<![endif]-->
-<!-- Google Font -->
-<link rel="stylesheet"
-  href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">
+    <link rel="shortcut icon" href="{{ URL::asset('favicon.ico') }}">
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+    <!-- Google Font -->
+    {!! Html::style('https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic') !!}
+{{--    <link rel="stylesheet"--}}
+{{--      href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">--}}
 </head>
 <body class="hold-transition skin-blue sidebar-mini">
 <div class="wrapper">
