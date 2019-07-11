5030317af5d48367e742ed5a158311aa5a223d7c
Hao Yuhang <haoyh@yahoo.com>
2019-07-11 14:03:50 +0800

    test

--- public/js/face/index.js
+++ public/js/face/index.js
@@ -1,9 +1,9 @@
 page.index('faces', [
     'create,edit',
     {className: 'text-center', targets: [1, 2, 3, 4, 5]},
-    {className: 'searching_disabled', targets: [4]},
-    {searchable: false, targets: [4]},
-    {orderable: false, targets: [4]}
+    {className: 'searching_disabled', targets: [1]},
+    {searchable: false, targets: [1]},
+    {orderable: false, targets: [1]}
 ]);
 $.getMultiScripts(['js/shared/dtrange.js']).done(
     function () { $.dtrange().dRange('.dtrange'); }
