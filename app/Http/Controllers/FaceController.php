8f0f04322725a585c5d283132dc5d9000abb51d7
Hao Yuhang <haoyh@yahoo.com>
2019-07-25 11:24:08 +0800

    test

--- app/Http/Controllers/FaceController.php
+++ app/Http/Controllers/FaceController.php
@@ -2,6 +2,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Face;
+use App\Models\User;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Support\Facades\Request;
 use Throwable;
@@ -24,7 +25,7 @@ class FaceController extends Controller {
         
         $this->middleware(['auth', 'checkrole']);
         $this->face = $face;
-        $this->approve($face);
+        $this->approve(new User);
         
     }
     
