43778fec1d6b5e03037e88771482e1f891c18988
Hao Yuhang <haoyh@yahoo.com>
2019-08-13 15:52:16 +0800

    test

--- public/js/shared/recharge.js
+++ public/js/shared/recharge.js
@@ -16,7 +16,7 @@
                 page.initBackBtn(table);
                 page.initDatatable(
                     table, [
-                        {className: 'text-center', targets: [1, 2, 3]},
+                        {className: 'text-center', targets: [1, 2]},
                         {className: 'text-left', targets: [3]}
                     ], 'recharge/' + id
                 );
