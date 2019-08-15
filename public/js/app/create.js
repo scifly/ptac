9ccb05e554f2b01d7bf83c758b6f6d768a83b552
Hao Yuhang <haoyh@yahoo.com>
2019-08-15 13:06:09 +0800

    test

--- public/js/app/create.js
+++ public/js/app/create.js
@@ -1,36 +1,24 @@
 page.create('formApp', 'apps');
-$('#category').on('change', function () {
-    var $name = $('.name'),
-        $appid = $('.appid'),
-        $appsecret = $('.appsecret'),
-        $url = $('.url'),
-        $token = $('.token'),
-        $eak = $('.eak');
+var $name = $('.name'),
+    $appid = $('.appid'),
+    $appsecret = $('.appsecret'),
+    $url = $('.url'),
+    $token = $('.token'),
+    $eak = $('.eak');
 
+$('#category').on('change', function () {
     switch($(this).val()) {
         case 1:
-            $name.hide();
-            $url.hide();
-            $token.hide();
-            $eak.hide();
-            $appid.show();
-            $appsecret.show();
+            $name.hide(); $url.hide(); $token.hide(); $eak.hide();
+            $appid.show(); $appsecret.show();
             break;
         case 2:
             $url.hide();
-            $name.show();
-            $appid.show();
-            $appsecret.show();
-            $token.show();
-            $eak.show();
+            $name.show(); $appid.show(); $appsecret.show(); $token.show(); $eak.show();
             break;
         default:
-            $name.hide();
-            $appid.hide();
-            $appsecret.show();
-            $url.show();
-            $token.show();
-            $eak.show();
+            $name.hide(); $appid.hide();
+            $appsecret.show(); $url.show(); $token.show(); $eak.show();
             break;
     }
 });
