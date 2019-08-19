419465fb8f334f79c4593e3f5fdf9f3279a54572
Hao Yuhang <haoyh@yahoo.com>
2019-08-19 08:31:31 +0800

    test

--- public/js/template/index.js
+++ public/js/template/index.js
@@ -1,6 +1,6 @@
 page.index('templates', [
     {className: 'text-center', targets: [1, 3, 4]},
-    {className: 'searching_disabled', targets: [5]},
-    {searchable: false, targets: [5]},
-    {orderable: false, targets: [5]}
+    {className: 'searching_disabled', targets: [3, 4, 5]},
+    {searchable: false, targets: [3, 4, 5]},
+    {orderable: false, targets: [3, 4, 5]}
 ]);
