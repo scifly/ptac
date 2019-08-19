da18ea25d09dc52522c0a4c96009ac8eccf51c06
Hao Yuhang <haoyh@yahoo.com>
2019-08-19 11:25:01 +0800

    test

--- public/js/template/config.js
+++ public/js/template/config.js
@@ -1,31 +1,33 @@
 var $appId = $('#app_id'),
     $primary = $('#primary'),
     $secondary = $('#secondary'),
-    $config = $('#config'),
     $form = $('#formTemplate');
 
 page.initSelect2();
 page.initBackBtn('templates');
 $primary.add($secondary).on('change', function() { validate(); });
-$form.on('submit', function () { return false; });
-$config.on('click', function () {
-    validate();
-    if ($appId.val() === null) {
-        page.inform('设置所属行业', '请选择一个公众号', page.failure);
-        return false;
-    }
-    $.ajax({
-        type: 'POST',
-        data: $form.serialize(),
-        url: 'templates/store',
-        dataType: 'json',
-        success: function (result) {
-            page.inform(result['title'], result['message'], page.success);
-        },
-        error: function (e) {
-            page.errorHandler(e);
+$form.parsley().on('form:validated', function () {
+    if ($('.parsley-error').length === 0) {
+        validate();
+        if ($appId.val() === null) {
+            page.inform('设置所属行业', '请选择一个公众号', page.failure);
+            return false;
         }
-    });
+        $.ajax({
+            type: 'POST',
+            data: $form.serialize(),
+            url: 'templates/store',
+            dataType: 'json',
+            success: function (result) {
+                page.inform(result['title'], result['message'], page.success);
+            },
+            error: function (e) {
+                page.errorHandler(e);
+            }
+        });
+    }
+}).on('form:submit', function () {
+    return false;
 });
 
 function validate() {
