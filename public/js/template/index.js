827ad9dc89a7aa6d7b49cdeb2c718a6465c97e5a
Hao Yuhang <haoyh@yahoo.com>
2019-08-19 08:29:49 +0800

    test

--- public/js/template/index.js
+++ public/js/template/index.js
@@ -1,3 +1,6 @@
 page.index('templates', [
-    {className: 'text-center', targets: [1, 3, 4]}
+    {className: 'text-center', targets: [1, 3, 4]},
+    {className: 'searching_disabled', targets: [5]},
+    {searchable, false, targets: [5]},
+    {orderable: false, targets: [5]}
 ]);
