9ab438a99bc31cdc7f4d963c94ccc0d8c1a0f29d
Hao Yuhang <haoyh@yahoo.com>
2019-08-27 16:15:45 +0800

    test

--- resources/views/shared/multiple_select.blade.php
+++ resources/views/shared/multiple_select.blade.php
@@ -1,7 +1,5 @@
 <div class="form-group">
-    {!! Form::label($id, $label, [
-        'class' => 'col-sm-3 control-label'
-    ]) !!}
+    {!! Form::label($id, $label, ['class' => 'col-sm-3 control-label']) !!}
     <div class="col-sm-6">
         <div class="input-group">
             <div class="input-group-addon">
@@ -18,24 +16,6 @@
                     isset($required) ? 'required' : '',
                 ]
             ) !!}
-{{--            <select multiple="multiple" name="{{ $id }}[]" id="{{ $id }}"--}}
-{{--                    class='form-control select2' style="width: 100%;"--}}
-{{--                    {!! isset($required) ? 'required' : '' !!}--}}
-{{--            >--}}
-{{--                @foreach ($items as $key => $value)--}}
-{{--                    @if (isset($selectedItems))--}}
-{{--                        <option value="{{ $key }}"--}}
-{{--                                @if(array_key_exists($key, $selectedItems))--}}
-{{--                                selected--}}
-{{--                                @endif--}}
-{{--                        >--}}
-{{--                            {{ $value }}--}}
-{{--                        </option>--}}
-{{--                    @else--}}
-{{--                        <option value="{{ $key }}">{{ $value }}</option>--}}
-{{--                    @endif--}}
-{{--                @endforeach--}}
-{{--            </select>--}}
         </div>
     </div>
 </div>
