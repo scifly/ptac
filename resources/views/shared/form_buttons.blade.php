262319a58eef8055f51aa145df5cfbe6830765ec
Hao Yuhang <haoyh@yahoo.com>
2019-08-29 09:39:05 +0800

    test

--- resources/views/shared/form_buttons.blade.php
+++ resources/views/shared/form_buttons.blade.php
@@ -2,19 +2,16 @@
 <div class="box-footer">
     <div class="form-group">
         <div class="col-sm-6 col-sm-offset-3">
-            <button class="btn btn-primary" @if (isset($id)) id="{!! $id !!}" @endif type="submit">
-                <i class="fa {!! $class ?? 'fa-save' !!}">
-                    {!! $label ?? ' 保存' !!}
-                </i>
-            </button>
+            {!! Form::button(
+                Html::tag('i', $label ?? ' 保存', ['class' => 'fa ' . ($class ?? 'fa-save')]), [
+                    'id' => $id ?? '', 'type' => 'submit',
+                    'class' => 'btn btn-primary'
+                ]
+            ) !!}
             @if (!isset($disabled))
                 @can('act', $uris['index'])
-{{--                    <button class="btn btn-default pull-right" id="cancel" type="reset">--}}
-{{--                        <i class="fa fa-mail-reply"> 取消</i>--}}
-{{--                    </button>--}}
                     {!! Form::button(
-                        Html::tag('i', ' 取消', ['class' => 'fa fa-mail-reply']),
-                        [
+                        Html::tag('i', ' 取消', ['class' => 'fa fa-mail-reply']), [
                             'id' => 'cancel', 'type' => 'reset',
                             'class' => 'btn btn-default pull-right'
                         ]
