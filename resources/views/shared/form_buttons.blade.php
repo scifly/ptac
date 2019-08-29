c64b71ecf9b570741972d6cbb824b7dfb022ee01
Hao Yuhang <haoyh@yahoo.com>
2019-08-29 09:49:58 +0800

    test

--- resources/views/shared/form_buttons.blade.php
+++ resources/views/shared/form_buttons.blade.php
@@ -4,8 +4,7 @@
         <div class="col-sm-6 col-sm-offset-3">
             {!! Form::button(
                 Html::tag('i', $label ?? ' 保存', ['class' => 'fa ' . ($class ?? 'fa-save')]), [
-                    'id' => $id ?? null, 'type' => 'submit',
-                    'class' => 'btn btn-primary'
+                    'id' => $id ?? null, 'type' => 'submit', 'class' => 'btn btn-primary'
                 ]
             ) !!}
             @if (!isset($disabled))
