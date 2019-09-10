76fdd82387c398e2e3b3100870ab326fe94bb9ab
Hao Yuhang <haoyh@yahoo.com>
2019-09-10 10:26:26 +0800

    test

--- resources/views/shared/multiple_select.blade.php
+++ resources/views/shared/multiple_select.blade.php
@@ -14,6 +14,7 @@
                 'class' => 'form-control',
                 'style' => 'width: 100%;',
                 isset($required) ? 'required' : '',
+                'disabled' => sizeof($items) <= 1
             ]) !!}
         </div>
     </div>
