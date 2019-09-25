676add782d6389bdb6f25611f321dbd2d67b4c7c
Hao Yuhang <haoyh@yahoo.com>
2019-09-25 12:46:59 +0800

    test

--- public/js/poll/index.js
+++ public/js/poll/index.js
@@ -1,3 +1,6 @@
 page.index('polls', [
     { className: 'text-center', targets: [1, 2, 3, 4, 5, 6] }
 ]);
+$.getMultiScripts(['js/shared/dtrange.js']).done(
+    function () { $.dtrange().dRange('.dtrange'); }
+);
--- public/js/poll_topic/index.js
+++ public/js/poll_topic/index.js
@@ -1,3 +1,6 @@
 page.index('poll_topics', [
     { className: 'text-center', targets: [1, 2, 3, 4, 5] }
 ]);
+$.getMultiScripts(['js/shared/dtrange.js']).done(
+    function () { $.dtrange().dRange('.dtrange'); }
+);
